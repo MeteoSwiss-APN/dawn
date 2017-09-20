f196ba1e0452ace125c1e09b54be89c464f24df6
Fabian <fabian_thuering@hotmail.com>
2017-09-20 15:15:23 +0200

    Add test/utils directory

---
+++ test/utils/gtclang-tester/gtclang_tester/config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+##===-----------------------------------------------------------------------------*- Python -*-===##
+##
+##                         _       _                   
+##                        | |     | |                  
+##                    __ _| |_ ___| | __ _ _ __   __ _ 
+##                   / _` | __/ __| |/ _` | '_ \ / _` |
+##                  | (_| | || (__| | (_| | | | | (_| |
+##                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL (2017)
+##                    __/ |                       __/ |
+##                   |___/                       |___/ 
+##
+##
+##  This file is distributed under the MIT License (MIT). 
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+class Config(object):
+    """ Global configuration options """
+
+    # Path to gtclang
+    gtclang = "gtclang"
+
+    # Verbosity
+    verbose = False
+
+    # GridTools flags
+    gridtools_flags = "-std=c++11"
+
+    # C++ compiler
+    cxx = "c++"
+
+    # Generate reference files
+    generate_reference = False
