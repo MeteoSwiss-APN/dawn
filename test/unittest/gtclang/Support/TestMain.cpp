87989ee9596c7c5e682706a941ed7ae39a64ee59
Fabian <fabian_thuering@hotmail.com>
2017-09-20 15:25:48 +0200

    Add unittest directory

---
+++ test/unittest/gtclang/Support/TestMain.cpp
@@ -0,0 +1,32 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _                   
+//                        | |     | |                  
+//                    __ _| |_ ___| | __ _ _ __   __ _ 
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/ 
+//
+//
+//  This file is distributed under the MIT License (MIT). 
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "gsl/Support/STLExtras.h"
+#include "gtclang/Support/Logger.h"
+#include "gtclang/Unittest/UnittestEnvironment.h"
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[]) {
+  // Initialize Logger
+  auto logger = gsl::make_unique<gtclang::Logger>();
+  gsl::Logger::getSingleton().registerLogger(logger.get());
+
+  // Initialize GTest
+  testing::InitGoogleTest(&argc, argv);
+  testing::AddGlobalTestEnvironment(&gtclang::UnittestEnvironment::getSingleton());
+
+  return RUN_ALL_TESTS();
+}
