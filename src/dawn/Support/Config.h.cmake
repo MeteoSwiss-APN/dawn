7efbc4c78dc8ca4eb25d58199642557280aa02e4
Fabian <fabian_thuering@hotmail.com>
2017-10-02 11:58:16 +0200

    Fix Config.h

--- src/dawn/Support/Config.h.cmake
+++ src/dawn/Support/Config.h.cmake
@@ -16,25 +16,22 @@
 #define DAWN_SUPPORT_CONFIG_H
 
 // Define if this is Unixish platform
-#cmakedefine DAWN_ON_UNIX ${DAWN_ON_UNIX }
+#cmakedefine DAWN_ON_UNIX ${DAWN_ON_UNIX}
 
 // Define if this is an Apple platform
-#cmakedefine DAWN_ON_APPLE ${DAWN_ON_APPLE }
+#cmakedefine DAWN_ON_APPLE ${DAWN_ON_APPLE}
 
 // Define if this is a Linux platform
-#cmakedefine DAWN_ON_LINUX ${DAWN_ON_LINUX }
+#cmakedefine DAWN_ON_LINUX ${DAWN_ON_LINUX}
 
 // Major version of DAWN
-#define DAWN_VERSION_MAJOR                                                                         \
-  $ { DAWN_VERSION_MAJOR }
+#define DAWN_VERSION_MAJOR ${DAWN_VERSION_MAJOR}
 
 // Minor version of DAWN
-#define DAWN_VERSION_MINOR                                                                         \
-  $ { DAWN_VERSION_MINOR }
+#define DAWN_VERSION_MINOR ${DAWN_VERSION_MINOR}
 
 // Patch version of DAWN
-#define DAWN_VERSION_PATCH                                                                         \
-  $ { DAWN_VERSION_PATCH }
+#define DAWN_VERSION_PATCH ${DAWN_VERSION_PATCH}
 
 // DAWN version string
 #define DAWN_VERSION_STR "${DAWN_VERSION_STR}"
