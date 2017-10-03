9ba9d37d13792c54b6d181896f1bc1a104494081
Fabian <fabian_thuering@hotmail.com>
2017-10-03 16:40:49 +0200

    [cmake]: Update the install package configuration

---
+++ src/dawn/Dawn.h
@@ -0,0 +1,21 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_DAWN_H
+#define DAWN_DAWN_H
+
+#include "dawn/Compiler/Compiler.h"
+#include "dawn/SIR/SIR.h"
+
+#endif
