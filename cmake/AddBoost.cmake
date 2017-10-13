14e529cb9286378f5d2a1c53a2006f32d5618b54
Fabian <fabian_thuering@hotmail.com>
2017-10-13 14:59:41 +0200

    Remove boost_system as a dependency (only headers required)

--- cmake/AddBoost.cmake
+++ cmake/AddBoost.cmake
@@ -14,26 +14,11 @@
 ##
 ##===------------------------------------------------------------------------------------------===##
 
-set(Boost_USE_MULTITHREADED ON)
-if(BUILD_SHARED_LIBS)
-  set(Boost_USE_STATIC_LIBS OFF)
-  set(Boost_USE_MULTITHREADED ON)
-  set(Boost_USE_STATIC_RUNTIME OFF)
-  add_definitions(-DBOOST_ALL_DYN_LINK)
-else()
-  set(Boost_USE_STATIC_LIBS ON)
-  set(Boost_USE_MULTITHREADED ON)
-  set(Boost_USE_STATIC_RUNTIME OFF)
-endif()
-
-set(GTCLANG_BOOST_COMPONENTS system)
-find_package(Boost 1.58 COMPONENTS ${GTCLANG_BOOST_COMPONENTS} REQUIRED)
+find_package(Boost 1.58 REQUIRED)
 
 dawn_export_package(
   NAME Boost
   FOUND ${Boost_FOUND} 
   VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}" 
-  LIBRARIES ${Boost_LIBRARIES}
   INCLUDE_DIRS ${Boost_INCLUDE_DIRS}
-  DEFINITIONS -DBOOST_ALL_NO_LIB
 )
