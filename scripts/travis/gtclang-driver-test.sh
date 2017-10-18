cb63c0899ebd4a157fc52ac3388815d561f7cfab
Fabian <fabian_thuering@hotmail.com>
2017-10-18 18:06:17 +0200

    Build gtclang in travis

--- scripts/travis/gtclang-driver-test.sh
+++ scripts/travis/gtclang-driver-test.sh
@@ -11,8 +11,42 @@
 ##
 ##
 ##  This file is distributed under the MIT License (MIT). 
-##  See LICENSE.txt for details.
+##  See LICENSE.txt for details
 ##
 ##===------------------------------------------------------------------------------------------===##
 
 this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Setup dependencies
+source "$this_script_dir/gtclang-install.sh"
+gtclang_install_dependencies
+
+if [ ! -z ${CLANG_VERSION+x} ]; then
+  install_driver -i "${CACHE_DIR}" -b clang
+fi
+
+export CXX=${CXX_COMPILER}
+export CC=${C_COMPILER}
+
+$CC --version
+$CXX --version
+
+# Build gtclang
+pushd "$(pwd)"
+
+mkdir build && cd build
+cmake .. -DCMAKE_CXX_COMPILER="$CXX"                                                               \
+         -DCMAKE_C_COMPILER="$CC"                                                                  \
+         -DCMAKE_BUILD_TYPE="$CONFIG"                                                              \
+         -DBOOST_ROOT="$BOOST_ROOT"                                                                \
+      || fatal_error "failed to configure"
+make -j2 || fatal_error "failed to build"
+
+# Run unittests
+ctest -C ${CONFIG} --output-on-failure --force-new-ctest-process                                   \
+     || fatal_error "failed to run tests"
+
+# Run regression tests
+bash  || fatal_error "failed to run tests"
+
+popd
