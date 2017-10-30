6f9fd6b86e67569e88fc6eeed1742609e4ab5620
Fabian <fabian_thuering@hotmail.com>
2017-10-30 13:52:44 +0100

    Parametrite the lib dir for RPATH

--- cmake/modules/DawnEnableFullRPATH.cmake
+++ cmake/modules/DawnEnableFullRPATH.cmake
@@ -37,7 +37,7 @@ macro(dawn_enable_full_rpath LIB_PREFIX)
   # When building, don't use the install RPATH already (but later on when installing)
   set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
 
-  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_PREFIX}")
   set(CMAKE_MACOSX_RPATH "${CMAKE_INSTALL_RPATH}")
 
   # Add the automatically determined parts of the RPATH which point to directories outside the 
@@ -45,8 +45,9 @@ macro(dawn_enable_full_rpath LIB_PREFIX)
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
   # The RPATH to be used when installing, but only if it's not a system directory
-  list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+  list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES 
+       "${CMAKE_INSTALL_PREFIX}/${LIB_PREFIX}" isSystemDir)
   if("${isSystemDir}" STREQUAL "-1")
-     SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+     SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_PREFIX}")
   endif("${isSystemDir}" STREQUAL "-1")
 endmacro()
