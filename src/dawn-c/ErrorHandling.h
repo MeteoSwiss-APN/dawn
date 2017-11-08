9b9f28bc525a26f64ae752a18907b2fd768c3950
Fabian <fabian_thuering@hotmail.com>
2017-11-08 14:45:41 +0100

    WIP of the dawn-c interface

---
+++ src/dawn-c/ErrorHandling.h
@@ -0,0 +1,111 @@
+/*===----------------------------------------------------------------------------------*- C -*-===*\
+ *                          _
+ *                         | |
+ *                       __| | __ ___      ___ ___
+ *                      / _` |/ _` \ \ /\ / / '_  |
+ *                     | (_| | (_| |\ V  V /| | | |
+ *                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+ *
+ *
+ *  This file is distributed under the MIT License (MIT).
+ *  See LICENSE.txt for details.
+ *
+\*===------------------------------------------------------------------------------------------===*/
+
+#ifndef DAWN_C_ERRORHANDLING_H
+#define DAWN_C_ERRORHANDLING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @ingroup dawn_c
+ * @{
+ */
+
+/**
+ * @brief Report a fatal error
+ *
+ * This function will invoke the installed FatalErrorHandler.
+ *
+ * @see dawnInstallFatalErrorHandler
+ */
+extern void dawnFatalError(const char* reason);
+
+/**
+ * @brief Error handler callback
+ */
+typedef void (*dawnFatalErrorHandler_t)(const char* reason);
+
+/**
+ * @brief Install a fatal error handler
+ *
+ * By default, if dawn detects a fatal error it will emit the last error string to stderr and
+ * call exit(1).
+ *
+ * This may not be appropriate in some contexts. For example, the Python module might want to
+ * translate the errror into an exception. This function allows you to install a callback that will
+ * be invoked after a fatal error occurred.
+ */
+extern void dawnInstallFatalErrorHandler(dawnFatalErrorHandler_t handler);
+
+/**
+ * @brief Reset the fatal error handler.
+ *
+ * This resets dawn's fatal error handling behavior to the default.
+ */
+extern void dawnResetFatalErrorHandler(void);
+
+/*===------------------------------------------------------------------------------------------===*\
+ *     Default Error Handler
+\*===------------------------------------------------------------------------------------------===*/
+
+/**
+ * @brief Emits the last error string to stderr and call exit(1)
+ */
+extern void dawnDefaultFatalErrorHandler(const char* reason);
+
+/*===------------------------------------------------------------------------------------------===*\
+ *     State Error Handler
+\*===------------------------------------------------------------------------------------------===*/
+
+/**
+ * @brief Store the the current state of the error which can be queried via
+ * @ref dawnStateErrorHandlerHasError as well as @ref dawnStateErrorHandlerGetErrorMessage
+ *
+ * This error handler is **not** thread-safe.
+ */
+extern void dawnStateErrorHandler(const char* reason);
+
+/**
+ * @brief Check the current error state
+ *
+ * This function requires to set the ErrorHandler to  @ref dawnStateErrorHandler. To obtain the
+ * associated error message, use @ref dawnStateErrorHandlerGetErrorMessage.
+ *
+ * @return 1 if there was an error, 0 otherwise
+ */
+extern int dawnStateErrorHandlerHasError(void);
+
+/**
+ * @brief Query the current error state
+ *
+ * This function requires to set the ErrorHandler to @ref dawnStateErrorHandler.
+ *
+ * @return newly allocated `char*` with the current error message
+ */
+extern char* dawnStateErrorHandlerGetErrorMessage(void);
+
+/**
+ * @brief Reset the current error state
+ */
+extern void dawnStateErrorHandlerResetState(void);
+
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/dawn-c/util/OptionsWrapper.h
@@ -0,0 +1,250 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_C_UTIL_OPTIONSWRAPPER_H
+#define DAWN_C_UTIL_OPTIONSWRAPPER_H
+
+#include "dawn-c/ErrorHandling.h"
+#include "dawn-c/Types.h"
+#include "dawn/Compiler/Options.h"
+#include "dawn/Support/Assert.h"
+#include <cstring>
+#include <string>
+#include <unordered_map>
+
+namespace dawn {
+
+namespace util {
+
+namespace internal {
+
+template <class T>
+struct OptionsInfo {};
+
+template <>
+struct OptionsInfo<bool> {
+  static constexpr DawnTypeKind TypeKind = DK_Integer;
+  using Type = int;
+
+  static std::size_t getSizeInBytes(const bool& value) { return sizeof(Type); }
+  static void* allocateCopy(const bool& value) {
+    auto ptr = (Type*)std::malloc(getSizeInBytes(value));
+    *ptr = value;
+    return ptr;
+  }
+};
+
+template <>
+struct OptionsInfo<int> {
+  static constexpr DawnTypeKind TypeKind = DK_Integer;
+  using Type = int;
+
+  static std::size_t getSizeInBytes(const int& value) { return sizeof(Type); }
+  static void* allocateCopy(const int& value) {
+    auto ptr = (Type*)std::malloc(getSizeInBytes(value));
+    *ptr = value;
+    return ptr;
+  }
+};
+
+template <>
+struct OptionsInfo<float> {
+  static constexpr DawnTypeKind TypeKind = DK_Double;
+  using Type = double;
+
+  static std::size_t getSizeInBytes(const float& value) { return sizeof(Type); }
+  static void* allocateCopy(const float& value) {
+    auto ptr = (Type*)std::malloc(getSizeInBytes(value));
+    *ptr = value;
+    return ptr;
+  }
+};
+
+template <>
+struct OptionsInfo<double> {
+  static constexpr DawnTypeKind TypeKind = DK_Double;
+  using Type = double;
+
+  static std::size_t getSizeInBytes(const double& value) { return sizeof(Type); }
+  static void* allocateCopy(const double& value) {
+    auto ptr = (Type*)std::malloc(getSizeInBytes(value));
+    *ptr = value;
+    return ptr;
+  }
+};
+
+template <>
+struct OptionsInfo<const char*> {
+  static constexpr DawnTypeKind TypeKind = DK_Char;
+  using Type = char;
+
+  static std::size_t getSizeInBytes(const char* value) { return std::strlen(value) + 1; }
+  static void* allocateCopy(const char* value) {
+    std::size_t size = getSizeInBytes(value);
+    auto ptr = (Type*)std::malloc(size);
+    std::memcpy(ptr, value, size);
+    return ptr;
+  }
+};
+
+template <>
+struct OptionsInfo<std::string> {
+  static constexpr DawnTypeKind TypeKind = DK_Char;
+  using Type = char;
+
+  static std::size_t getSizeInBytes(const std::string& value) { return value.size() + 1; }
+  static void* allocateCopy(const std::string& value) {
+    std::size_t size = getSizeInBytes(value);
+    auto ptr = (Type*)std::malloc(size);
+    std::memcpy(ptr, value.c_str(), size);
+    return ptr;
+  }
+};
+
+template <class T>
+struct ValueGetter {};
+
+template <>
+struct ValueGetter<bool> {
+  static bool get(const dawnOptionsEntry_t* entry) {
+    DAWN_ASSERT_MSG(entry->Type == DK_Integer, "invalid type, expected int");
+    return *(int*)entry->Value;
+  }
+};
+
+template <>
+struct ValueGetter<int> {
+  static int get(const dawnOptionsEntry_t* entry) {
+    DAWN_ASSERT_MSG(entry->Type == DK_Integer, "invalid type, expected int");
+    return *(int*)entry->Value;
+  }
+};
+
+template <>
+struct ValueGetter<double> {
+  static double get(const dawnOptionsEntry_t* entry) {
+    DAWN_ASSERT_MSG(entry->Type == DK_Double, "invalid type, expected double");
+    return *(double*)entry->Value;
+  }
+};
+
+template <>
+struct ValueGetter<std::string> {
+  static std::string get(const dawnOptionsEntry_t* entry) {
+    DAWN_ASSERT_MSG(entry->Type == DK_Char, "invalid type, expected string");
+    return std::string((const char*)entry->Value);
+  }
+};
+
+} // namespace internal
+
+/// @brief Warpper for dawnOptionsEntry_t`
+/// @ingroup dawn_c_util
+struct OptionsEntryWrapper {
+  OptionsEntryWrapper() = delete;
+
+  /// @brief Construct `dawnOptionsEntry_t` from `value`
+  template <class T>
+  static dawnOptionsEntry_t* construct(const T& value) {
+    dawnOptionsEntry_t* entry = (dawnOptionsEntry_t*)std::malloc(sizeof(dawnOptionsEntry_t));
+    using OptionInfoT = internal::OptionsInfo<T>;
+    entry->Type = OptionInfoT::TypeKind;
+    entry->SizeInBytes = OptionInfoT::getSizeInBytes(value);
+    entry->Value = OptionInfoT::allocateCopy(value);
+    return entry;
+  }
+
+  /// @brief Deallocate `dawnOptionsEntry_t`
+  static void destroy(dawnOptionsEntry_t* entry) {
+    std::free(entry->Value);
+    std::free(entry);
+  }
+
+  /// @brief Copy construct `dawnOptionsEntry_t`
+  static dawnOptionsEntry_t* copy(const dawnOptionsEntry_t* entry) {
+    dawnOptionsEntry_t* entryCopy = (dawnOptionsEntry_t*)std::malloc(sizeof(dawnOptionsEntry_t));
+    entryCopy->Type = entry->Type;
+    entryCopy->SizeInBytes = entry->SizeInBytes;
+    entryCopy->Value = std::malloc(entry->SizeInBytes);
+    std::memcpy(entryCopy->Value, entry->Value, entry->SizeInBytes);
+    return entryCopy;
+  }
+
+  /// @brief Extract the value from an entry of type `T`
+  template <class T>
+  static T getValue(const dawnOptionsEntry_t* entry) {
+    return internal::ValueGetter<T>::get(entry);
+  }
+};
+
+/// @brief Warpper for `dawn::Options` which provides types erased string based access
+/// @ingroup dawn_c_util
+class OptionsWrapper {
+public:
+  OptionsWrapper() {
+#define OPT(TYPE, NAME, DEFAULT_VALUE, OPTION, OPTION_SHORT, HELP, VALUE_NAME, HAS_VALUE, F_GROUP) \
+  this->setOption<TYPE>(#NAME, DEFAULT_VALUE);
+#include "dawn/Compiler/Options.inc"
+#undef OPT
+  }
+
+  ~OptionsWrapper();
+
+  /// @brief Set option `name` to `value`
+  template <class T>
+  void setOption(std::string name, const T& value) noexcept {
+    options_[name] = OptionsEntryWrapper::construct(value);
+  }
+
+  /// @brief Insert a copy of the options entry `name`
+  void setOptionEntry(std::string name, const dawnOptionsEntry_t* value) noexcept {
+    options_[name] = OptionsEntryWrapper::copy(value);
+  }
+
+  /// @brief Check if option `name` exists
+  bool hasOption(std::string name) const noexcept;
+
+  /// @brief Returns a copy of the option `name` or `NULL` if option does not exist
+  const dawnOptionsEntry_t* getOption(std::string name) const noexcept;
+
+  /// @brief Convert to `dawn::Options` (this copies the current options)
+  dawn::Options toOptions() const noexcept;
+
+  /// @brief Convert to string
+  char* toString() const;
+
+private:
+  std::unordered_map<std::string, dawnOptionsEntry_t*> options_;
+};
+
+/// @brief Convert `dawnOptions_t` to `OptionsWrapper`
+/// @{
+inline const OptionsWrapper* toConstOptionsWrapper(const dawnOptions_t* options) {
+  if(!options->Impl)
+    dawnFatalError("uninitialized Options");
+  return reinterpret_cast<const OptionsWrapper*>(options->Impl);
+}
+
+inline OptionsWrapper* toOptionsWrapper(dawnOptions_t* options) {
+  if(!options->Impl)
+    dawnFatalError("uninitialized Options");
+  return reinterpret_cast<OptionsWrapper*>(options->Impl);
+}
+/// @}
+
+} // namespace util
+
+} // namespace dawn
+
+#endif
