5eec07f7007dc6949cf8360820f1f5a137ab4e74
Tobias Wicky <twicki@ethz.ch>
2017-11-20 09:43:46 +0100

    Integration-tests for caching non temporary variables (#21)
    The integrationtest for MeteoSwiss-APN/dawn#32 (caching ij-planes of non temporary fields) added here

--- test/integration-test/PassSetNonTempCaches/MultipleCaches_1.cpp
+++ test/integration-test/PassSetNonTempCaches/MultipleCaches_1.cpp
@@ -14,50 +14,25 @@
 //
 //===------------------------------------------------------------------------------------------===//
 
-// RUN: %gtclang% %file% -fno-codegen -freport-pass-preprocessor
-
+// RUN: %gtclang% %file% -fno-codegen -fcache-non-temp-fields -freport-cache-non-temp-fields
+// EXPECTED: PASS: PassSetNonTempCaches: Test01 : Cached: field_b : Type: IJ:local Cached: field_c : Type: IJ:local
 #include "gridtools/clang_dsl.hpp"
 
 using namespace gridtools::clang;
 using namespace gridtools;
 
-// clang-format off
-stencil_function TestFun {
-  storage foo;
-
-  Do { return foo[i + 1]; }
-};
-
 stencil Test01 {
-  storage foo, bar;
+  storage field_a, field_b, field_c, field_d, field_e;
 
   void Do() {
     vertical_region(k_start, k_start) {
-      foo[i, j + 1, k] = 5;      // EXPECTED: %line%: foo(i, j + 1, k) = 5;
-      foo = bar[i];              // EXPECTED: %line%: foo = bar(i);
-      foo[i] = bar[i, j];        // EXPECTED: %line%: foo(i) = bar(i, j);
-      foo = TestFun(bar[i + 1]); // EXPECTED: %line%: foo = TestFun(bar(i + 1));
+      field_b[i + 1] = field_a;
+      field_a = field_b[j - 1];
+      field_c[i + 1] = field_d;
+      field_c[j + 1] = field_a;
+      field_c[j + 2] = field_b;
+      field_c[i + 2] = field_b;
+      field_c[i + 1] = field_b;
     }
   }
 };
-
-stencil Test02 {
-  gridtools::clang::storage foo, bar;
-
-  void Do() {
-    vertical_region(k_start, k_start) 
-      foo[i, j + 1, k] = bar; // EXPECTED: %line%: foo(i, j + 1, k) = bar;
-  }
-};
-
-stencil Test03 {
-  clang::storage foo, bar;
-
-  void Do() {
-    vertical_region(k_start, k_start) 
-      foo[i, j + 1, k] = bar; // EXPECTED: %line%: foo(i, j + 1, k) = bar;
-  }
-};
-
-// clang-format on
-
