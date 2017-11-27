81dd801f455a001df9e421d7f7758db33ab335cd
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-27 12:03:59 +0100

    add jenkins script

---
+++ scripts/jenkins/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+module load git/2.8.4
+module load CMake/3.8.1
+module load GCC/5.3.0-binutils-2.25
+#Avoid conflicts complains
+module load Python/3.5.0-gmvolf-15.11 >& /dev/null
+
+export BOOST_DIR=/scratch/cosuna/software/boost_1_59_0/
+
+base_dir=$(pwd)
+build_dir=${base_dir}/bundle/build
+
+mkdir -p $build_dir
+cd $build_dir
+
+export BOOST_DIR=
+
+cmake -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_CXX_COMPILER=`which g++` -DCMAKE_C_COMPILER=`which gcc` -DBOOST_ROOT=$(BOOST_DIR)  \
+        -DProtobuf_DIR=/scratch/cosuna/software/protobuf/3.4.0/lib/cmake/protobuf/ ../
+make -j2
+
+# Run unittests
+ctest -C ${build_type} --output-on-failure --force-new-ctest-process  
