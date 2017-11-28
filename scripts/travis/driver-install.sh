a5762b8ad5d4a5763406c535b1a75bf003c57d0f
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-28 08:49:31 +0100

    build protobuf in the install

--- scripts/travis/driver-install.sh
+++ scripts/travis/driver-install.sh
@@ -16,3 +16,25 @@
 this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$this_script_dir/install.sh"
 install_driver $*
+
+export CXX=${CXX_COMPILER}
+export CC=${C_COMPILER}
+
+$CC --version
+$CXX --version
+
+# Build dawn
+pushd "$(pwd)"
+
+export PYTHON_DIR=/opt/python/3.5.3
+
+cd bundle
+mkdir build
+cd build
+cmake .. -DCMAKE_CXX_COMPILER="$CXX"                                                               \
+         -DCMAKE_C_COMPILER="$CC"                                                                  \
+         -DCMAKE_BUILD_TYPE="$CONFIG"                                                              \
+         -DPYTHON_EXECUTABLE="$PYTHON_DIR/bin/python3"                                             \
+      || fatal_error "failed to configure"
+make -j2 protobuf || fatal_error "failed to build"
+
