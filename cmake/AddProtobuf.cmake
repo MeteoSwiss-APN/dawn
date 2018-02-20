92847f6468007e3c3ce0ebb27712862517227fe4
Tobias Wicky <twicki@ethz.ch>
2018-02-20 11:11:26 +0100

    fix typo

--- cmake/AddProtobuf.cmake
+++ cmake/AddProtobuf.cmake
@@ -30,7 +30,7 @@ get_filename_component(root_dir ${root_bin_dir} DIRECTORY)
 get_filename_component(root_dir ${root_dir} ABSOLUTE)
 set(DAWN_PROTOBUF_ROOT ${root_dir})
 
-# set the path to the install-direcotry of protobuf
+# set the path to the install-directory of protobuf
 set(DAWN_PROTOBUF_RPATH_DIR ${DAWN_PROTOBUF_ROOT}/lib)
 
 dawn_export_package(
