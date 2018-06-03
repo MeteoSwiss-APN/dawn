e4762ef57d56903f5d986802b7c8f4dd7481d0d3
cosunae <carlos.osuna@env.ethz.ch>
2018-06-03 13:13:24 +0200

    adding daint setup

--- scripts/jenkins/build.sh
+++ scripts/jenkins/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+BASEPATH_SCRIPT=$(dirname "${0}")
+source ${BASEPATH_SCRIPT}/machine_env.sh
+source ${BASEPATH_SCRIPT}/env_${myhost}.sh
+
 module load git
 module load cmake
 module load gcc/5.4.0-2.26
@@ -13,8 +17,18 @@ build_dir=${base_dir}/bundle/build
 mkdir -p $build_dir
 cd $build_dir
 
+if [ ${myhost} == "kesch" ]; then
+  PROTOBUFDIR="/scratch/jenkins/workspace/protobuf/slave/kesch/install/lib64/cmake/protobuf/"
+elif [ ${myhost} == "daint" ]; then
+  PROTOBUFDIR="/scratch/snx3000/jenkins/workspace/protobuf/slave/daint/install/lib64/cmake/protobuf/"
+else
+  echo" Error Machine not found: ${myhost}"
+  exit 1
+fi
+
+
 cmake -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_CXX_COMPILER=`which g++` -DCMAKE_C_COMPILER=`which gcc` -DBOOST_ROOT=${BOOST_DIR}  \
-        -DProtobuf_DIR=/scratch/cosuna/software/protobuf/3.4.0/lib/cmake/protobuf/ ../
+        -DProtobuf_DIR=${PROTOBUFDIR} ../
 make -j2
 
 # Run unittests
