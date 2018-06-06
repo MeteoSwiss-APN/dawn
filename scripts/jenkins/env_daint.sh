3531816e663252f410390d388617250dfc7167a8
cosunae <carlos.osuna@env.ethz.ch>
2018-06-06 09:32:37 +0200

    go back to gcc compilation

--- scripts/jenkins/env_daint.sh
+++ scripts/jenkins/env_daint.sh
@@ -5,7 +5,8 @@ module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc/5.3.0
 
 export BOOST_DIR=/project/c14/install/daint/boost/boost_1_67_0/
-module load /users/vogtha/modules/compilers/clang/3.8.1
-export CXX=${LLVM_DIR}/bin/clang++
-export CC=${LLVM_DIR}/bin/clang
-
+#module load /project/c14/install/daint/clang/module_3.8.1
+#export CXX=${LLVM_DIR}/bin/clang++
+#export CC=${LLVM_DIR}/bin/clang
+export CXX=`which g++`
+export CC=`which gcc`
