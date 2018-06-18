d27c47cb336a032db4c938e9a4c1927a69ae598b
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-06-18 16:01:53 +0200

    add missing file

---
+++ cmake/thirdparty/DawnAddBoost.cmake
@@ -0,0 +1,40 @@
+##===------------------------------------------------------------------------------*- CMake -*-===##
+##                         _       _
+##                        | |     | |
+##                    __ _| |_ ___| | __ _ _ __   __ _ 
+##                   / _` | __/ __| |/ _` | '_ \ / _` |
+##                  | (_| | || (__| | (_| | | | | (_| |
+##                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+##                    __/ |                       __/ |
+##                   |___/                       |___/
+##
+##
+##  This file is distributed under the MIT License (MIT). 
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+include(mchbuildSetDownloadDir)
+include(mchbuildFindPackage)
+
+mchbuild_set_download_dir()
+
+#
+# Boost
+#
+set(boost_min_version 1.58.0)
+set(_v 63)
+set(boost_download_version 1.${_v}.0)
+
+mchbuild_find_package(
+  PACKAGE Boost
+  PACKAGE_ARGS ${boost_min_version} 
+  COMPONENTS ${boost_components}
+  REQUIRED_VARS BOOST_ROOT
+  ADDITIONAL
+    DOWNLOAD_DIR ${MCHBUILD_DOWNLOAD_DIR}
+    URL "http://sourceforge.net/projects/boost/files/boost/1.${_v}.0/boost_1_${_v}_0.tar.gz/download"
+    URL_MD5 "7b493c08bc9557bbde7e29091f28b605" 
+    BUILD_VERSION ${boost_download_version}
+)
+
