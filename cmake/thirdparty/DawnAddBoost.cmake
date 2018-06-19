e016e5960144e5cf5379f6782954f06ee9f73426
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-06-19 08:58:09 +0200

    yoda

--- cmake/thirdparty/DawnAddBoost.cmake
+++ cmake/thirdparty/DawnAddBoost.cmake
@@ -14,10 +14,10 @@
 ##
 ##===------------------------------------------------------------------------------------------===##
 
-include(mchbuildSetDownloadDir)
-include(mchbuildFindPackage)
+include(yodaSetDownloadDir)
+include(yodaFindPackage)
 
-mchbuild_set_download_dir()
+yoda_set_download_dir()
 
 #
 # Boost
@@ -26,13 +26,13 @@ set(boost_min_version 1.58.0)
 set(_v 63)
 set(boost_download_version 1.${_v}.0)
 
-mchbuild_find_package(
+yoda_find_package(
   PACKAGE Boost
   PACKAGE_ARGS ${boost_min_version} 
   COMPONENTS ${boost_components}
   REQUIRED_VARS BOOST_ROOT
   ADDITIONAL
-    DOWNLOAD_DIR ${MCHBUILD_DOWNLOAD_DIR}
+    DOWNLOAD_DIR ${YODA_DOWNLOAD_DIR}
     URL "http://sourceforge.net/projects/boost/files/boost/1.${_v}.0/boost_1_${_v}_0.tar.gz/download"
     URL_MD5 "7b493c08bc9557bbde7e29091f28b605" 
     BUILD_VERSION ${boost_download_version}
