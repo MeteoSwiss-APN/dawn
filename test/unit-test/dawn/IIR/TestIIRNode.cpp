cdfb20bce9f2acf59c0728cf7e92914c9bfee7ea
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-08-04 00:36:47 +0200

    add missing unittest directory

---
+++ test/unit-test/dawn/IIR/TestIIRNode.cpp
@@ -0,0 +1,23 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "dawn/SIR/SIR.h"
+#include <gtest/gtest.h>
+#include <string>
+
+using namespace dawn;
+
+namespace {
+TEST(IIRNode, t) {}
+}
