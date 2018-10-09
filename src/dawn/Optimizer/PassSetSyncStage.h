61e049ff4c698933c795f287002e2ecef04983d9
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-10-09 12:01:53 +0200

    add pass

---
+++ src/dawn/Optimizer/PassSetSyncStage.h
@@ -0,0 +1,44 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_OPTIMIZER_PASSSETSYNCSTAGE_H
+#define DAWN_OPTIMIZER_PASSSETSYNCSTAGE_H
+
+#include "dawn/IIR/Cache.h"
+#include "dawn/IIR/Interval.h"
+#include "dawn/IIR/Stage.h"
+#include "dawn/IIR/Stencil.h"
+#include "dawn/Optimizer/Pass.h"
+
+namespace dawn {
+
+/// @brief Determine whether stages require a synchronization
+/// input: well formed and legal IIR
+/// output: IIR with sync property of derived info of stage
+/// @ingroup optimizer
+///
+class PassSetSyncStage : public Pass {
+public:
+  PassSetSyncStage();
+
+  /// @brief Pass implementation
+  bool run(const std::shared_ptr<iir::StencilInstantiation>& stencilInstantiation) override;
+
+private:
+  bool requiresSync(const iir::Stage& stage, const std::unique_ptr<iir::MultiStage>& ms) const;
+};
+
+} // namespace dawn
+
+#endif
