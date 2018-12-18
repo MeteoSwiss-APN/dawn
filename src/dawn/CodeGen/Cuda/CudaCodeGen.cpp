0735181ae37388ce849c2a280112e0750459f655
cosunae <carlos.osuna@meteoswiss.ch>
2018-12-18 11:35:06 +0100

    fix

--- src/dawn/CodeGen/Cuda/CudaCodeGen.cpp
+++ src/dawn/CodeGen/Cuda/CudaCodeGen.cpp
@@ -561,8 +561,8 @@ void CudaCodeGen::generateStencilRunMethod(
           stencilInstantiation->getNameFromAccessID((*field).second.getAccessID());
 
       args = args + (idx == 0 ? "" : ",") + "(" + fieldName + ".data()+" + "m_" + fieldName +
-             ".get_storage_info_ptr()->index(" + fieldName + ".template begin<0>(), " + fieldName +
-             ".template begin<1>(),0 ))";
+             ".get_storage_info_ptr()->index(" + fieldName + ".begin<0>(), " + fieldName +
+             ".begin<1>(),0 ))";
       ++idx;
     }
     DAWN_ASSERT(nonTempFields.size() > 0);
@@ -575,9 +575,8 @@ void CudaCodeGen::generateStencilRunMethod(
             stencilInstantiation->getNameFromAccessID((*field).second.getAccessID());
 
         args = args + ", (" + fieldName + ".data()+" + "m_" + fieldName +
-               ".get_storage_info_ptr()->index(" + fieldName + ".template begin<0>(), " +
-               fieldName + ".template begin<1>()," + fieldName + ".template begin<2>()," +
-               fieldName + ".template begin<3>(), 0))";
+               ".get_storage_info_ptr()->index(" + fieldName + ".begin<0>(), " + fieldName +
+               ".begin<1>()," + fieldName + ".begin<2>()," + fieldName + ".begin<3>(), 0))";
       } else {
         args =
             args + "," + stencilInstantiation->getNameFromAccessID((*field).second.getAccessID());
