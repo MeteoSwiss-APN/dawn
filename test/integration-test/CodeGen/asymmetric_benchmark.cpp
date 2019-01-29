a63e811e706f051db5c91e67d1fd7840cf4b3b5a
Tobias Wicky <twicki@ethz.ch>
2019-01-29 13:57:37 +0100

    add asymmetric test (#112)
    * add asymmetric test
    
    * typo

---
+++ test/integration-test/CodeGen/asymmetric_benchmark.cpp
@@ -0,0 +1,62 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+#define GRIDTOOLS_CLANG_GENERATED 1
+#define GRIDTOOLS_CLANG_HALO_EXTEND 3
+#define GT_VECTOR_LIMIT_SIZE 30
+
+#undef FUSION_MAX_VECTOR_SIZE
+#undef FUSION_MAX_MAP_SIZE
+#define FUSION_MAX_VECTOR_SIZE GT_VECTOR_LIMIT_SIZE
+#define FUSION_MAX_MAP_SIZE FUSION_MAX_VECTOR_SIZE
+#define BOOST_MPL_LIMIT_VECTOR_SIZE FUSION_MAX_VECTOR_SIZE
+#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
+
+#include "gridtools/clang/verify.hpp"
+#include "test/integration-test/CodeGen/Macros.hpp"
+#include "test/integration-test/CodeGen/Options.hpp"
+#include "test/integration-test/CodeGen/generated/asymmetric_c++-naive.cpp"
+#include <gtest/gtest.h>
+
+#ifndef OPTBACKEND
+#define OPTBACKEND gridtools
+#endif
+
+// clang-format off
+#include INCLUDE_FILE(test/integration-test/CodeGen/generated/asymmetric_,OPTBACKEND.cpp)
+// clang-format on
+
+using namespace dawn;
+TEST(asymmetric, test) {
+  domain dom(Options::getInstance().m_size[0], Options::getInstance().m_size[1],
+             Options::getInstance().m_size[2]);
+  dom.set_halos(halo::value, halo::value, halo::value, halo::value, 0, 0);
+
+  verifier verif(dom);
+
+  meta_data_t meta_data(dom.isize(), dom.jsize(), dom.ksize() + 1);
+  storage_t in(meta_data, "in"), out_opt(meta_data, "out-opt"), out_naive(meta_data, "out-naive");
+
+  verif.fillMath(8.0, 2.0, 1.5, 1.5, 2.0, 4.0, in);
+  verif.fill(-1.0, out_opt, out_naive);
+
+  OPTBACKEND::asymmetric_stencil stencil_opt(dom, in, out_opt);
+  cxxnaive::asymmetric_stencil stencil_naive(dom, in, out_naive);
+
+  stencil_opt.run();
+  stencil_naive.run();
+
+  ASSERT_TRUE(verif.verify(out_opt, out_naive));
+}
