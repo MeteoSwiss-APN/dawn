d92f939369289a35c6e74eec8dc1947784c39836
Tobias Wicky <twicki@ethz.ch>
2019-03-22 11:56:21 +0100

    fix positioning

--- src/dawn/Compiler/DawnCompiler.cpp
+++ src/dawn/Compiler/DawnCompiler.cpp
@@ -177,10 +177,10 @@ std::unique_ptr<OptimizerContext> DawnCompiler::runOptimizer(std::shared_ptr<SIR
   optimizer->checkAndPushBack<PassStageReordering>(reorderStrategy);
   optimizer->checkAndPushBack<PassStageMerger>();
   optimizer->checkAndPushBack<PassStencilSplitter>(maxFields);
-  optimizer->checkAndPushBack<PassFieldVersioning>(
-      PassFieldVersioning::FieldVersioningPassMode::FM_FixAccess);
   optimizer->checkAndPushBack<PassTemporaryType>();
   optimizer->checkAndPushBack<PassTemporaryMerger>();
+  optimizer->checkAndPushBack<PassFieldVersioning>(
+      PassFieldVersioning::FieldVersioningPassMode::FM_FixAccess);
   optimizer->checkAndPushBack<PassInlining>(
       (getOptions().InlineSF || getOptions().PassTmpToFunction),
       PassInlining::IK_ComputationsOnTheFly);
