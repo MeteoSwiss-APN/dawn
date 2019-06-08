ad9931d39e4b4aa8410e7be7de100b293e7692e2
cosunae <carlos.osuna@meteoswiss.ch>
2019-06-08 09:10:16 +0200

    fix pep8 in printer

--- python/dawn/sir_printer.py
+++ python/dawn/sir_printer.py
@@ -1,12 +1,13 @@
 import textwrap
 
+
 class SIRPrinter:
     def __init__(self):
         self.indent_ = 0
         self.T_ = textwrap.TextWrapper(initial_indent=' ' * self.indent_, width=120,
                                        subsequent_indent=' ' * self.indent_)
 
-    def visitBuiltinType(self, builtin_type):
+    def visit_builtin_type(self, builtin_type):
         if builtin_type.type_id == 0:
             raise ValueError('Builtin type not supported')
         elif builtin_type.type_id == 1:
@@ -19,65 +20,65 @@ class SIRPrinter:
             return "float"
         raise ValueError('Builtin type not supported')
 
-    def visitUnaryOperator(self, expr):
-        return expr.op + " " + self.visitExpr(expr.operand)
+    def visit_unary_operator(self, expr):
+        return expr.op + " " + self.visit_expr(expr.operand)
 
-    def visitBinaryOperator(self, expr):
-        return "("+ self.visitExpr(expr.left) + " " + expr.op + " " + self.visitExpr(expr.right)+")"
+    def visit_binary_operator(self, expr):
+        return "("+ self.visit_expr(expr.left) + " " + expr.op + " " + self.visit_expr(expr.right)+")"
 
-    def visitAssignmentExpr(self, expr):
-        return self.visitExpr(expr.left) + " " + expr.op + " " + self.visitExpr(expr.right)
+    def visit_assignment_expr(self, expr):
+        return self.visit_expr(expr.left) + " " + expr.op + " " + self.visit_expr(expr.right)
 
-    def visitTernaryOperator(self, expr):
-        return "(" + self.visitExpr(expr.cond) + " ? " + self.visitExpr(expr.left) + " : " + self.visitExpr(
+    def visit_ternary_operator(self, expr):
+        return "(" + self.visit_expr(expr.cond) + " ? " + self.visit_expr(expr.left) + " : " + self.visit_expr(
             expr.right) + ")"
 
-    def visitVarAccessExpr(self, expr):
-        return expr.name  # + self.visitExpr(expr.index)
+    def visit_var_access_expr(self, expr):
+        return expr.name  # + self.visit_expr(expr.index)
 
-    def visitFieldAccessExpr(self, expr):
+    def visit_field_access_expr(self, expr):
         str_ = expr.name + "["
         str_ += ",".join(str(x) for x in expr.offset)
         str_ += "]"
         return str_
 
-    def visitLiteralAccessExpr(self, expr):
+    def visit_literal_access_expr(self, expr):
         return expr.value
 
     # call to external function, like math::sqrt
-    def visitFunCallExpr(self, expr):
-        return expr.callee + "(" + ",".join(self.visitExpr(x) for x in expr.arguments) + ")"
+    def visit_fun_call_expr(self, expr):
+        return expr.callee + "(" + ",".join(self.visit_expr(x) for x in expr.arguments) + ")"
 
-    def visitExpr(self, expr):
+    def visit_expr(self, expr):
         if expr.WhichOneof("expr") == "unary_operator":
-            return self.visitUnaryOperator(expr.unary_operator)
+            return self.visit_unary_operator(expr.unary_operator)
         elif expr.WhichOneof("expr") == "binary_operator":
-            return self.visitBinaryOperator(expr.binary_operator)
+            return self.visit_binary_operator(expr.binary_operator)
         elif expr.WhichOneof("expr") == "assignment_expr":
-            return self.visitAssignmentExpr(expr.assignment_expr)
+            return self.visit_assignment_expr(expr.assignment_expr)
         elif expr.WhichOneof("expr") == "ternary_operator":
-            return self.visitTernaryOperator(expr.ternary_operator)
+            return self.visit_ternary_operator(expr.ternary_operator)
         elif expr.WhichOneof("expr") == "fun_call_expr":
-            return self.visitFunCallExpr(expr.fun_call_expr)
+            return self.visit_fun_call_expr(expr.fun_call_expr)
         elif expr.WhichOneof("expr") == "stencil_fun_call_expr":
             raise ValueError("non supported expression")
         elif expr.WhichOneof("expr") == "stencil_fun_arg_expr":
             raise ValueError("non supported expression")
         elif expr.WhichOneof("expr") == "var_access_expr":
-            return self.visitVarAccessExpr(expr.var_access_expr)
+            return self.visit_var_access_expr(expr.var_access_expr)
         elif expr.WhichOneof("expr") == "field_access_expr":
-            return self.visitFieldAccessExpr(expr.field_access_expr)
+            return self.visit_field_access_expr(expr.field_access_expr)
         elif expr.WhichOneof("expr") == "literal_access_expr":
-            return self.visitLiteralAccessExpr(expr.literal_access_expr)
+            return self.visit_literal_access_expr(expr.literal_access_expr)
         else:
             raise ValueError("Unknown expression")
 
-    def visitVarDeclStmt(self, var_decl):
+    def visit_var_decl_stmt(self, var_decl):
         str_ = ""
         if var_decl.type.WhichOneof("type") == "name":
             str_ += var_decl.type.name
         elif var_decl.type.WhichOneof("type") == "builtin_type":
-            str_ += self.visitBuiltinType(var_decl.type.builtin_type)
+            str_ += self.visit_builtin_type(var_decl.type.builtin_type)
         else:
             raise ValueError("Unknown type ", var_decl.type.WhichOneof("type"))
         str_ += " " + var_decl.name
@@ -88,47 +89,47 @@ class SIRPrinter:
         str_ += var_decl.op
 
         for expr in var_decl.init_list:
-            str_ += self.visitExpr(expr)
+            str_ += self.visit_expr(expr)
 
         print(self.T_.fill(str_))
 
-    def visitExprStmt(self, stmt):
-        print(self.T_.fill(self.visitExpr(stmt.expr)))
+    def visit_expr_stmt(self, stmt):
+        print(self.T_.fill(self.visit_expr(stmt.expr)))
 
-    def visitIfStmt(self, stmt):
+    def visit_if_stmt(self, stmt):
         cond = stmt.cond_part
         if cond.WhichOneof("stmt") != "expr_stmt":
             raise ValueError("Not expected stmt")
 
-        print(self.T_.fill("if(" + self.visitExpr(cond.expr_stmt.expr) + ")"))
-        self.visitBodyStmt(stmt.then_part)
-        self.visitBodyStmt(stmt.else_part)
+        print(self.T_.fill("if(" + self.visit_expr(cond.expr_stmt.expr) + ")"))
+        self.visit_body_stmt(stmt.then_part)
+        self.visit_body_stmt(stmt.else_part)
 
-    def visitBlockStmt(self, stmt):
+    def visit_block_stmt(self, stmt):
         print(self.T_.fill("{"))
         self.indent_ += 2
         self.T_.initial_indent = ' ' * self.indent_
 
         for each in stmt.statements:
-            self.visitBodyStmt(each)
+            self.visit_body_stmt(each)
         self.indent_ -= 2
         self.T_.initial_indent = ' ' * self.indent_
 
         print(self.T_.fill("}"))
 
-    def visitBodyStmt(self, stmt):
+    def visit_body_stmt(self, stmt):
         if stmt.WhichOneof("stmt") == "var_decl_stmt":
-            self.visitVarDeclStmt(stmt.var_decl_stmt)
+            self.visit_var_decl_stmt(stmt.var_decl_stmt)
         elif stmt.WhichOneof("stmt") == "expr_stmt":
-            self.visitExprStmt(stmt.expr_stmt)
+            self.visit_expr_stmt(stmt.expr_stmt)
         elif stmt.WhichOneof("stmt") == "if_stmt":
-            self.visitIfStmt(stmt.if_stmt)
+            self.visit_if_stmt(stmt.if_stmt)
         elif stmt.WhichOneof("stmt") == "block_stmt":
-            self.visitBlockStmt(stmt.block_stmt)
+            self.visit_block_stmt(stmt.block_stmt)
         else:
             raise ValueError("Stmt not supported :" + stmt.WhichOneof("stmt"))
 
-    def visitVerticalRegion(self, vertical_region):
+    def visit_vertical_region(self, vertical_region):
         str_ = "vertical_region("
         interval = vertical_region.interval
         if (interval.WhichOneof("LowerLevel") == 'special_lower_level'):
@@ -160,27 +161,27 @@ class SIRPrinter:
         self.T_.initial_indent = ' ' * self.indent_
 
         for stmt in vertical_region.ast.root.block_stmt.statements:
-            self.visitBodyStmt(stmt)
+            self.visit_body_stmt(stmt)
 
         self.indent_ -= 2
         self.T_.initial_indent = ' ' * self.indent_
 
-    def visitStencil(self, stencil):
+    def visit_stencil(self, stencil):
         print(self.T_.fill("stencil " + stencil.name))
         self.indent_ += 2
         self.T_.initial_indent = ' ' * self.indent_
 
-        self.visitFields(stencil.fields)
+        self.visit_fields(stencil.fields)
 
         block = stencil.ast.root.block_stmt
         for stmt in block.statements:
             if stmt.WhichOneof("stmt") == "vertical_region_decl_stmt":
-                self.visitVerticalRegion(stmt.vertical_region_decl_stmt.vertical_region)
+                self.visit_vertical_region(stmt.vertical_region_decl_stmt.vertical_region)
 
         self.indent_ -= 2
         self.T_.initial_indent = ' ' * self.indent_
 
-    def visitGlobalVariables(self, global_variables):
+    def visit_global_variables(self, global_variables):
         if not global_variables.IsInitialized():
             return
 
@@ -214,7 +215,7 @@ class SIRPrinter:
         self.indent_ -= 2
         self.T_.initial_indent = ' ' * self.indent_
 
-    def visitFields(self, fields):
+    def visit_fields(self, fields):
         str_ = "field "
         for field in fields:
             str_ += field.name
