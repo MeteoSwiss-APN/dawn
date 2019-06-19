227597f5d6cb9ffff0326fd36cc84487c357ceae
cosunae <carlos.osuna@env.ethz.ch>
2019-06-19 15:35:34 +0200

    fix for kesch upgrade

--- scripts/jenkins/env_kesch.sh
+++ scripts/jenkins/env_kesch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
+module load PE/17.06
 module load git
 module load /users/jenkins/easybuild/kesch/modules/all/cmake/3.12.4
-module load gcc/5.4.0-2.26
 module load python/3.6.2-gmvolf-17.02
+module unload gcc
+module load gcc/6.1.0
+
 
 export CXX=`which g++`
 export CC=`which gcc`
