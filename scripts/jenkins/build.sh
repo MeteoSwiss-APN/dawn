0d8119031d369ace5691aaff3234fd5a4e1b01d0
Tobias Wicky <twicki@ethz.ch>
2019-08-09 16:03:47 +0200

    update jenkins script

--- scripts/jenkins/build.sh
+++ scripts/jenkins/build.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+set -e
+
 BASEPATH_SCRIPT=$(dirname "${0}")
 source ${BASEPATH_SCRIPT}/machine_env.sh
 source ${BASEPATH_SCRIPT}/env_${myhost}.sh
+
+if [ -z ${myhost+x} ]; then
+  echo "myhost is unset"
+  exit 1
+fi
+
+SCRIPT=`basename $0`
+
+function help {
+  echo -e "Basic usage:$SCRIPT "\\n
+  echo -e "The following switches are recognized. $OFF "
+  echo -e "-i sets the installation directory"
+  echo -e "-h Shows this help"
+  exit 1
+}
+echo "####### executing: $0 $* (PID=$$ HOST=$HOSTNAME TIME=`date '+%D %H:%M:%S'`)"
+while getopts i: flag; do
+  case $flag in
+    i)
+      INSTALL_DIR=$OPTARG
+      ;;
+    h)
+      help
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${OFF} not allowed."
+      help
+      ;;
+  esac
+done
 
 base_dir=$(pwd)
 build_dir=${base_dir}/bundle/build
@@ -17,9 +49,14 @@ fi
 if [ -z ${PROTOBUFDIR+x} ]; then 
  echo "PROTOBUFDIF needs to be set in the machine env"
 fi
+CMAKE_ARGS="-DDAWN_BUNDLE_PYTHON=ON -DDAWN_BUNDLE_JAVA=ON -DDAWN_PYTHON_EXAMPLES=ON -DCMAKE_BUILD_TYPE=${build_type} -DBOOST_ROOT=${BOOST_DIR}  \
+        -DProtobuf_DIR=${PROTOBUFDIR}"
 
-cmake -DDAWN_BUNDLE_PYTHON=ON -DDAWN_BUNDLE_JAVA=ON -DDAWN_PYTHON_EXAMPLES=ON -DCMAKE_BUILD_TYPE=${build_type} -DBOOST_ROOT=${BOOST_DIR}  \
-        -DProtobuf_DIR=${PROTOBUFDIR} ../
+if [ -n ${INSTALL_DIR} ]; then
+  CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+fi
+
+cmake ${CMAKE_ARGS} ../
 make -j2 install
 
 # Run unittests
