d8a29b99358cc663f2ceb0d44f35481a6fbc0a0a
Tobias Wicky <twicki@ethz.ch>
2019-08-21 08:02:57 +0200

    fix typo

--- test/unit-test/dawn/IIR/TestIIRSerializer.cpp
+++ test/unit-test/dawn/IIR/TestIIRSerializer.cpp
@@ -173,10 +173,9 @@ class createEmptyOptimizerContext : public ::testing::Test {
 protected:
   virtual void SetUp() override {
     dawn::DiagnosticsEngine diag;
-    dawn::Options options;
     std::shared_ptr<SIR> sir = std::make_shared<SIR>();
-    dawn::OptimizerContext::OptimizerContextOptions optionss_;
-    context_ = new OptimizerContext(diag, optionss_, sir);
+    dawn::OptimizerContext::OptimizerContextOptions options;
+    context_ = new OptimizerContext(diag, options, sir);
   }
   virtual void TearDown() override {}
   OptimizerContext* context_;
