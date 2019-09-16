5d9531acea373bc645276d01bfbc5b0c7b24790c
Tobias Wicky <wicky.tob@gmail.com>
2019-09-16 13:30:19 +0200

    Update examples/cpp/SIRToIIR.cpp
    Co-Authored-By: Hannes Vogt <hannes@havogt.de>

--- examples/cpp/SIRToIIR.cpp
+++ examples/cpp/SIRToIIR.cpp
@@ -19,7 +19,7 @@
 
 int main(int argc, char* argv[]) {
   if(argc != 2) {
-    std::cerr << "Usage: iir-emitter <file>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
     return 1;
   }
 
