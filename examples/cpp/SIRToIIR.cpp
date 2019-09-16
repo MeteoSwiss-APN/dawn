5db06e69c5cd3cc9fe8afc38128d7c2e0b9d249c
Tobias Wicky <wicky.tob@gmail.com>
2019-09-16 13:31:06 +0200

    cpp example to run a copystencil (#267)
    Add an example on how to run dawn standalone if provided an SIR file in byte format

---
+++ examples/cpp/SIRToIIR.cpp
@@ -0,0 +1,41 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+#include <dawn-c/Compiler.h>
+#include <dawn-c/Options.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char* argv[]) {
+  if(argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
+    return 1;
+  }
+
+  std::ifstream inputFile(argv[1]);
+  if(!inputFile.is_open())
+    return 1;
+
+  std::stringstream ss;
+  ss << inputFile.rdbuf();
+
+  auto options = dawnOptionsCreate();
+  auto entry = dawnOptionsEntryCreateInteger(1);
+  dawnOptionsSet(options, "SerializeIIR", entry);
+
+  auto str = ss.str();
+  dawnCompile(str.c_str(), str.length(), options);
+
+  return 0;
+}
