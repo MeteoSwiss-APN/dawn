ac1c104e77134ebeceba47ec19c3f14aaa20d616
lukasm91 <lukasm91@users.noreply.github.com>
2019-10-09 12:45:37 +0200

    Add atlas interface (#310)
    - defines the current requirements of the interface (currently in `src/dawn/CodeGen/CXXNaive-ico/CXXNaiveCodeGen.cpp`)
    - Adapts the `mylib` interface to this interface
    - Implements the interface for `atlas` and adds a driver code
    - Generalize the unstructured "CodeGenPlayground" to  take into account that different nodes might have different number of neighbours
    - Fix initialization in the periodic unstructured example
    - (unrelated) add bundle-folder to ignorelist of `clang-format-all.sh`

--- dawn/prototype/mylibDriver.cpp
+++ dawn/prototype/mylibDriver.cpp
@@ -2,24 +2,26 @@
 #include <gridtools/clang_dsl.hpp>
 
 #include "generated.hpp"
+#include "mylib_interface.hpp"
 
 int main() {
-  int w = 20;
-  Mesh m{w, w, true};
-  Field<double> in(m), out(m);
+  int w = 32;
+  mylib::Grid m{w, w, true};
+  mylib::FaceData<double> in(m), out(m);
 
   for(auto& f : m.faces()) {
     auto center_x = w / 2.f - (1.f / 3) * (f.vertex(0).x() + f.vertex(1).x() + f.vertex(2).x());
     auto center_y = w / 2.f - (1.f / 3) * (f.vertex(0).y() + f.vertex(1).y() + f.vertex(2).y());
-    in[f] = (center_x * center_x + center_y * center_y > w / 3.) ? 1 : 0;
+    in(f) =
+        std::abs(center_x) < w / 5. && std::abs(center_y) < w / 5. && mylib::inner_face(f) ? 1 : 0;
   }
 
-  for(int i = 0; i < 1000; ++i) {
+  for(int i = 0; i < 500; ++i) {
     std::ofstream of("of_" + std::to_string(i) + ".vtk");
     toVtk(m, of);
     toVtk("temperature", in, m, of);
 
-    dawn_generated::cxxnaiveico::generated(m, in, out).run();
+    dawn_generated::cxxnaiveico::generated<mylibInterface::mylibTag>(m, in, out).run();
 
     using std::swap;
     swap(in, out);
