c84ef6455108e1d8da9d4218c57515a9c7575ab9
mroeth <matthias.roethlin@meteoswiss.ch>
2019-10-17 12:19:41 +0200

    new driver codes, adapted readme

---
+++ dawn/prototype/mylibCopyDriver.cpp
@@ -0,0 +1,22 @@
+#include "mylib_interface.hpp"
+
+#include <fstream>
+#include <gridtools/clang_dsl.hpp>
+
+using namespace mylibInterface;
+#include "generated_copyEdgeToCell.hpp"
+
+int main() {
+  int w = 20;
+  Mesh m{w, w, true};
+  CellField<double> out(m);
+  EdgeField<double> in(m);
+
+  for(int i = 0; i < 10; ++i) {
+    std::ofstream of("of_" + std::to_string(i) + ".vtk");
+    toVtk(m, of);
+    toVtk("temperature", out, m, of);
+
+    dawn_generated::cxxnaiveico::generated<mylibInterface::mylibTag>(m, in, out).run();
+  }
+}
