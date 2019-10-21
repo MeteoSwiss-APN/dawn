488bb6f96b5e662e6d02ee5410e15b5d0816b452
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-10-21 14:12:14 +0200

    remove redundand add

--- dawn/src/dawn/Unittest/IIRBuilder.cpp
+++ dawn/src/dawn/Unittest/IIRBuilder.cpp
@@ -175,9 +175,6 @@ IIRBuilder::Field IIRBuilder::field(std::string const& name, ast::Expr::Location
   DAWN_ASSERT(si_);
   int id = si_->getMetaData().addField(iir::FieldAccessType::FAT_APIField, name,
                                        asArray(fieldType::ijk), location);
-  int accessID = si_->getMetaData().getAccessIDFromName(name);
-  si_->getMetaData().addAccessIDLocationPair(accessID, location);
-
   return {id, name};
 }
 IIRBuilder::LocalVar IIRBuilder::localvar(std::string const& name, BuiltinTypeID type) {
