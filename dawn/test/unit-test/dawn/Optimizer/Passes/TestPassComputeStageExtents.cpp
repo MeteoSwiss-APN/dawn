42bcc2a95d290282922e53708ec987529aad1705
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-10-22 09:09:42 +0200

    final final change

--- dawn/test/unit-test/dawn/Optimizer/Passes/TestPassComputeStageExtents.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestPassComputeStageExtents.cpp
@@ -64,19 +64,6 @@ protected:
   }
 };
 
-static void compareExtents(iir::Extents extents, const std::array<int, 6>& ref) {
-  const auto& hExtents =
-      dawn::iir::extent_cast<dawn::iir::CartesianExtent const&>(extents.horizontalExtent());
-  const auto& vExtents = extents.verticalExtent();
-
-  EXPECT_EQ(hExtents.iMinus(), ref[0]);
-  EXPECT_EQ(hExtents.iPlus(), ref[1]);
-  EXPECT_EQ(hExtents.jMinus(), ref[2]);
-  EXPECT_EQ(hExtents.jPlus(), ref[3]);
-  EXPECT_EQ(vExtents.minus(), ref[4]);
-  EXPECT_EQ(vExtents.plus(), ref[5]);
-}
-
 TEST_F(ComputeStageExtents, test_stencil_01) {
   std::unique_ptr<iir::IIR> IIR = loadTest("compute_extent_test_stencil_01.sir");
   const auto& stencils = IIR->getChildren();
@@ -85,8 +72,8 @@ TEST_F(ComputeStageExtents, test_stencil_01) {
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
   EXPECT_EQ(stencil->getNumStages(), 2);
-  compareExtents(stencil->getStage(0)->getExtents(), {-1, 1, -1, 1, 0, 0});
-  compareExtents(stencil->getStage(1)->getExtents(), {0, 0, 0, 0, 0, 0});
+  EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian));
 }
 
 TEST_F(ComputeStageExtents, test_stencil_02) {
@@ -97,9 +84,9 @@ TEST_F(ComputeStageExtents, test_stencil_02) {
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
   EXPECT_EQ(stencil->getNumStages(), 3);
-  compareExtents(stencil->getStage(0)->getExtents(), {-1, 1, -1, 1, 0, 0});
-  compareExtents(stencil->getStage(1)->getExtents(), {-1, 0, -1, 0, 0, 0});
-  compareExtents(stencil->getStage(2)->getExtents(), {0, 0, 0, 0, 0, 0});
+  EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian, -1, 0, -1, 0, 0, 0));
+  EXPECT_EQ(stencil->getStage(2)->getExtents(), iir::Extents(ast::cartesian));
 }
 TEST_F(ComputeStageExtents, test_stencil_03) {
   std::unique_ptr<iir::IIR> IIR = loadTest("compute_extent_test_stencil_03.sir");
@@ -108,10 +95,10 @@ TEST_F(ComputeStageExtents, test_stencil_03) {
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
   EXPECT_EQ(stencil->getNumStages(), 4);
-  compareExtents(stencil->getStage(0)->getExtents(), {-1, 1, -1, 2, 0, 0});
-  compareExtents(stencil->getStage(1)->getExtents(), {-1, 0, -1, 1, 0, 0});
-  compareExtents(stencil->getStage(2)->getExtents(), {0, 0, 0, 1, 0, 0});
-  compareExtents(stencil->getStage(3)->getExtents(), {0, 0, 0, 0, 0, 0});
+  EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -1, 1, -1, 2, 0, 0));
+  EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian, -1, 0, -1, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(2)->getExtents(), iir::Extents(ast::cartesian, 0, 0, 0, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(3)->getExtents(), iir::Extents(ast::cartesian));
 }
 
 TEST_F(ComputeStageExtents, test_stencil_04) {
@@ -122,10 +109,10 @@ TEST_F(ComputeStageExtents, test_stencil_04) {
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
   EXPECT_EQ(stencil->getNumStages(), 4);
-  compareExtents(stencil->getStage(0)->getExtents(), {-2, 3, -2, 1, 0, 0});
-  compareExtents(stencil->getStage(1)->getExtents(), {-1, 1, -1, 0, 0, 0});
-  compareExtents(stencil->getStage(2)->getExtents(), {0, 0, -1, 0, 0, 0});
-  compareExtents(stencil->getStage(3)->getExtents(), {0, 0, 0, 0, 0, 0});
+  EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -2, 3, -2, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian, -1, 1, -1, 0, 0, 0));
+  EXPECT_EQ(stencil->getStage(2)->getExtents(), iir::Extents(ast::cartesian, 0, 0, -1, 0, 0, 0));
+  EXPECT_EQ(stencil->getStage(3)->getExtents(), iir::Extents(ast::cartesian));
 }
 
 TEST_F(ComputeStageExtents, test_stencil_05) {
@@ -135,10 +122,10 @@ TEST_F(ComputeStageExtents, test_stencil_05) {
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
   EXPECT_EQ(stencil->getNumStages(), 4);
-  compareExtents(stencil->getStage(0)->getExtents(), {-2, 3, -2, 1, 0, 0});
-  compareExtents(stencil->getStage(1)->getExtents(), {-1, 0, -1, 0, 0, 0});
-  compareExtents(stencil->getStage(2)->getExtents(), {0, 1, -1, 0, 0, 0});
-  compareExtents(stencil->getStage(3)->getExtents(), {0, 0, 0, 0, 0, 0});
+  EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -2, 3, -2, 1, 0, 0));
+  EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian, -1, 0, -1, 0, 0, 0));
+  EXPECT_EQ(stencil->getStage(2)->getExtents(), iir::Extents(ast::cartesian, 0, 1, -1, 0, 0, 0));
+  EXPECT_EQ(stencil->getStage(3)->getExtents(), iir::Extents(ast::cartesian));
 }
 
 } // anonymous namespace
