MeteoSwiss-APN/dawn

commit 6eab2a656655454f3406272dd40946c7f6666412

Support for clang 10 aka master (#344)
## Technical Description

Fixes some API changes in clang 10 (master branch as of Oct 14 2019).

gtclang/src/gtclang/Support/ClangCompat/CompilerInvocation.h
@@ -1,0 +1,40 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef GTCLANG_SUPPORT_CLANGCOMPAT_COMPILER_INVOCATION_H
+#define GTCLANG_SUPPORT_CLANGCOMPAT_COMPILER_INVOCATION_H
+
+#include "clang/Basic/Version.h"
+#include "clang/Frontend/CompilerInvocation.h"
+#include "llvm/Option/Option.h"
+
+namespace gtclang::clang_compat::CompilerInvocation {
+#if CLANG_VERSION_MAJOR < 9
+inline bool CreateFromArgs(clang::CompilerInvocation& Res, llvm::opt::ArgStringList& ccArgs,
+                           clang::DiagnosticsEngine& Diags) {
+  return clang::CompilerInvocation::CreateFromArgs(
+      Res, const_cast<const char**>(ccArgs.data()),
+      const_cast<const char**>(ccArgs.data()) + ccArgs.size(), Diags);
+}
+#else
+inline bool CreateFromArgs(clang::CompilerInvocation& Res, llvm::opt::ArgStringList& ccArgs,
+                           clang::DiagnosticsEngine& Diags) {
+  return clang::CompilerInvocation::CreateFromArgs(Res, ccArgs, Diags);
+}
+#endif
+} // namespace gtclang::clang_compat::CompilerInvocation
+
+#endif // GTCLANG_SUPPORT_CLANGCOMPAT_COMPILER_INVOCATION_H

gtclang/src/gtclang/Support/ClangCompat/FileUtil.h
@@ -14,22 +14,22 @@
 //
 //===------------------------------------------------------------------------------------------===//
 
-#ifndef GTCLANG_SUPPORT_CLANGCOMPAT_EVALRESULT_H
-#define GTCLANG_SUPPORT_CLANGCOMPAT_EVALRESULT_H
+#ifndef GTCLANG_SUPPORT_CLANGCOMPAT_FILE_UTIL_H
+#define GTCLANG_SUPPORT_CLANGCOMPAT_FILE_UTIL_H
 
-#include "clang/AST/Expr.h"
+#include "clang/Basic/FileManager.h"
 #include "clang/Basic/Version.h"
 
-namespace gtclang::clang_compat::Expr {
-#if CLANG_VERSION_MAJOR < 8
-using EvalResultInt = ::llvm::APSInt;
-inline int64_t getInt(EvalResultInt const& res) { return res.getExtValue(); }
-
+namespace gtclang::clang_compat::FileUtil {
+#if CLANG_VERSION_MAJOR < 9
+inline const clang::FileEntry* getFile(clang::FileManager& files, ::llvm::StringRef filename) {
+  return files.getFile(filename);
+}
 #else
-using EvalResultInt = ::clang::Expr::EvalResult;
-inline int64_t getInt(EvalResultInt const& res) { return res.Val.getInt().getExtValue(); }
-
+inline const clang::FileEntry* getFile(clang::FileManager& files, ::llvm::StringRef filename) {
+  return files.getFile(filename).get(); // maybe check for error
+}
 #endif
-} // namespace gtclang::clang_compat::Expr
+} // namespace gtclang::clang_compat::FileUtil
 
-#endif // GTCLANG_SUPPORT_CLANGCOMPAT_EVALRESULT_H
+#endif // GTCLANG_SUPPORT_CLANGCOMPAT_FILE_UTIL_H

gtclang/src/gtclang/Driver/CompilerInstance.cpp
@@ -16,6 +16,7 @@
 
 #include "gtclang/Driver/CompilerInstance.h"
 #include "dawn/Support/Compiler.h"
+#include "gtclang/Support/ClangCompat/CompilerInvocation.h"
 #include "gtclang/Support/Config.h"
 #include "gtclang/Support/Logger.h"
 #include "clang/Basic/DiagnosticOptions.h"
@@ -101,9 +102,7 @@ clang::CompilerInstance* createCompilerInstance(llvm::SmallVectorImpl<const char
 #endif
 
   std::shared_ptr<CompilerInvocation> CI(new CompilerInvocation);
-  CompilerInvocation::CreateFromArgs(*CI, const_cast<const char**>(ccArgs.data()),
-                                     const_cast<const char**>(ccArgs.data()) + ccArgs.size(),
-                                     diagnostics);
+  clang_compat::CompilerInvocation::CreateFromArgs(*CI, ccArgs, diagnostics);
   CI->getFrontendOpts().DisableFree = false;
 
   // Show the invocation, with -v

gtclang/src/gtclang/Support/ClangCompat/FileSystem.h
@@ -20,30 +20,16 @@
 #include "llvm/Config/llvm-config.h"
 #include "llvm/Support/FileSystem.h"
 
-namespace gtclang {
-namespace clang_compat {
+namespace gtclang::clang_compat {
 #if LLVM_VERSION_MAJOR < 7
-namespace llvm {
-namespace sys {
-namespace fs {
-namespace OpenFlags {
+namespace llvm::sys::fs::OpenFlags {
 static constexpr ::llvm::sys::fs::OpenFlags OF_Text = ::llvm::sys::fs::OpenFlags::F_Text;
 }
-} // namespace fs
-} // namespace sys
-} // namespace llvm
 #else
-namespace llvm {
-namespace sys {
-namespace fs {
-namespace OpenFlags {
+namespace llvm::sys::fs::OpenFlags {
 static constexpr ::llvm::sys::fs::OpenFlags OF_Text = ::llvm::sys::fs::OpenFlags::OF_Text;
 }
-} // namespace fs
-} // namespace sys
-} // namespace llvm
 #endif
-} // namespace clang_compat
-} // namespace gtclang
+} // namespace gtclang::clang_compat
 
 #endif // GTCLANG_SUPPORT_CLANGCOMPAT_FILESYSTEM_H

gtclang/src/gtclang/Support/ClangCompat/ImplicitNodes.h
@@ -23,8 +23,7 @@
 // TODO skipAllImplicitNodes should be refactored to be only called for Exprs (not Stmts)
 // to reflect the change in clang 9
 
-namespace gtclang {
-namespace clang_compat {
+namespace gtclang::clang_compat {
 
 #if CLANG_VERSION_MAJOR < 9
 template <typename StmtT>
@@ -46,7 +45,6 @@ StmtT* skipAllImplicitNodes(StmtT* s) {
   return s;
 }
 #endif
-} // namespace clang_compat
-} // namespace gtclang
+} // namespace gtclang::clang_compat
 
 #endif // GTCLANG_SUPPORT_CLANGCOMPAT_IMPLICIT_NODES_H

gtclang/src/gtclang/Support/ClangCompat/SourceLocation.h
@@ -24,8 +24,7 @@
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Basic/Version.h"
 
-namespace gtclang {
-namespace clang_compat {
+namespace gtclang::clang_compat {
 #if CLANG_VERSION_MAJOR < 8
 inline ::clang::SourceLocation getEndLoc(::clang::CXXBaseSpecifier const& base) {
   return base.getLocStart();
@@ -59,7 +58,6 @@ inline ::clang::SourceLocation getEndLoc(::clang::Expr const& expr) { return exp
 inline ::clang::SourceLocation getBeginLoc(::clang::Stmt const& stmt) { return stmt.getBeginLoc(); }
 inline ::clang::SourceLocation getEndLoc(::clang::Stmt const& stmt) { return stmt.getEndLoc(); }
 #endif
-} // namespace clang_compat
-} // namespace gtclang
+} // namespace gtclang::clang_compat
 
 #endif // GTCLANG_SUPPORT_CLANGCOMPAT_SOURCELOCATION_H

gtclang/src/gtclang/Support/ClangCompat/VirtualFileSystem.h
@@ -25,24 +25,18 @@
 #include "llvm/Support/VirtualFileSystem.h"
 #endif
 
-namespace gtclang {
-namespace clang_compat {
+namespace gtclang::clang_compat {
 #if CLANG_VERSION_MAJOR < 8
 
-namespace llvm {
-namespace vfs {
+namespace llvm::vfs {
 using InMemoryFileSystem = ::clang::vfs::InMemoryFileSystem;
 }
-} // namespace llvm
 #else
-namespace llvm {
-namespace vfs {
+namespace llvm::vfs {
 using InMemoryFileSystem = ::llvm::vfs::InMemoryFileSystem;
-}
-} // namespace llvm
+} // namespace llvm::vfs
 #endif
 
-} // namespace clang_compat
-} // namespace gtclang
+} // namespace gtclang::clang_compat
 
 #endif // GTCLANG_SUPPORT_CLANGCOMPAT_VIRTUALFILESYSTEM_H

gtclang/src/gtclang/Support/FileUtil.cpp
@@ -15,6 +15,7 @@
 //===------------------------------------------------------------------------------------------===//
 
 #include "gtclang/Support/FileUtil.h"
+#include "gtclang/Support/ClangCompat/FileUtil.h"
 #include "gtclang/Support/ClangCompat/VirtualFileSystem.h"
 #include "clang/Basic/SourceManager.h"
 
@@ -28,8 +29,8 @@ clang::FileID createInMemoryFile(llvm::StringRef filename, llvm::MemoryBuffer* s
                                  clang::SourceManager& sources, clang::FileManager& files,
                                  clang_compat::llvm::vfs::InMemoryFileSystem* memFS) {
   memFS->addFileNoOwn(filename, 0, source);
-  return sources.createFileID(files.getFile(filename), clang::SourceLocation(),
-                              clang::SrcMgr::C_User);
+  return sources.createFileID(clang_compat::FileUtil::getFile(files, filename),
+                              clang::SourceLocation(), clang::SrcMgr::C_User);
 }
 
 } // namespace gtclang

commit 08e2b98343c7720f9db895ebb85c80750e85b088

Fix compilation for GTClang with LLVM 9 (#380)
## Technical Description

Fix compilation with clang 9. I can still not use gtclang built with llvm 9, but at least it compiles.

### Resolves / Enhances

Regression from #344

gtclang/src/gtclang/Support/ClangCompat/CompilerInvocation.h
@@ -22,7 +22,7 @@
 #include "llvm/Option/Option.h"
 
 namespace gtclang::clang_compat::CompilerInvocation {
-#if CLANG_VERSION_MAJOR < 9
+#if CLANG_VERSION_MAJOR < 10
 inline bool CreateFromArgs(clang::CompilerInvocation& Res, llvm::opt::ArgStringList& ccArgs,
                            clang::DiagnosticsEngine& Diags) {
   return clang::CompilerInvocation::CreateFromArgs(

gtclang/src/gtclang/Support/ClangCompat/FileUtil.h
@@ -21,7 +21,7 @@
 #include "clang/Basic/Version.h"
 
 namespace gtclang::clang_compat::FileUtil {
-#if CLANG_VERSION_MAJOR < 9
+#if CLANG_VERSION_MAJOR < 10
 inline const clang::FileEntry* getFile(clang::FileManager& files, ::llvm::StringRef filename) {
   return files.getFile(filename);
 }