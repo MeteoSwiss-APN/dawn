e559a7974b6560555a90b840c0d36300c55619fb
Hannes Vogt <hannes@havogt.de>
2019-10-30 11:26:55 +0100

    add test

---
+++ gtclang/test/integration-test/Regression/globals_dependencies.cpp
@@ -0,0 +1,33 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+// RUN: %gtclang% %file% -fno-codegen --write-iir -o %filename%.cpp
+// EXPECTED_FILE: OUTPUT:%filename%.0.iir REFERENCE:%filename%_ref.0.iir IGNORE:filename
+
+#include "gridtools/clang_dsl.hpp"
+
+using namespace gridtools::clang;
+
+globals { int global = 0; };
+
+stencil copystencil {
+  storage in_field, out_field;
+  void Do() {
+    vertical_region(k_start, k_end) { out_field = global * in_field; }
+    global = 1;
+    vertical_region(k_start, k_end) { out_field = global * in_field; }
+  }
+};
