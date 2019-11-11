c02a4d57a6738f02cd9c1c151080c94c909f29ee
lukasm91 <lukasm91@users.noreply.github.com>
2019-11-11 11:10:57 +0100

    Extents/Offsets move definitions from header to cpp (#444)
    ## Technical Description
    
    Final task for extents/offsets is moving definitions from header to cpp to reduce dependencies when changing one of the definitions. Basically all files depend on `Offsets.h`.
    
    No code was changed in the implementations.
    
    ### Dependencies
    
    no dependencies

--- dawn/src/dawn/AST/Offsets.cpp
+++ dawn/src/dawn/AST/Offsets.cpp
@@ -17,9 +17,137 @@
 
 #include "Offsets.h"
 
+#include "dawn/Support/Unreachable.h"
 #include <iostream>
 
 namespace dawn::ast {
+
+// HorizontalOffsetImpl
+std::unique_ptr<HorizontalOffsetImpl> HorizontalOffsetImpl::clone() const { return cloneImpl(); }
+
+bool HorizontalOffsetImpl::operator==(HorizontalOffsetImpl const& other) const {
+  return equalsImpl(other);
+}
+
+HorizontalOffsetImpl& HorizontalOffsetImpl::operator+=(HorizontalOffsetImpl const& other) {
+  addImpl(other);
+  return *this;
+}
+
+bool HorizontalOffsetImpl::isZero() const { return isZeroImpl(); }
+
+// CartesianOffset
+CartesianOffset::CartesianOffset(int iOffset, int jOffset) : horizontalOffset_{iOffset, jOffset} {}
+CartesianOffset::CartesianOffset(std::array<int, 2> const& offsets) : horizontalOffset_(offsets) {}
+
+int CartesianOffset::offsetI() const { return horizontalOffset_[0]; }
+int CartesianOffset::offsetJ() const { return horizontalOffset_[1]; }
+
+std::unique_ptr<HorizontalOffsetImpl> CartesianOffset::cloneImpl() const {
+  return std::make_unique<CartesianOffset>(horizontalOffset_);
+}
+bool CartesianOffset::equalsImpl(HorizontalOffsetImpl const& other) const {
+  auto const& co_other = dynamic_cast<CartesianOffset const&>(other);
+  return co_other.horizontalOffset_ == horizontalOffset_;
+}
+
+void CartesianOffset::addImpl(HorizontalOffsetImpl const& other) {
+  auto const& co_other = dynamic_cast<CartesianOffset const&>(other);
+  horizontalOffset_[0] += co_other.horizontalOffset_[0];
+  horizontalOffset_[1] += co_other.horizontalOffset_[1];
+}
+
+bool CartesianOffset::isZeroImpl() const {
+  return horizontalOffset_[0] == 0 && horizontalOffset_[1] == 0;
+}
+
+// UnstructuredOffset
+UnstructuredOffset::UnstructuredOffset(bool hasOffset) : hasOffset_(hasOffset) {}
+bool UnstructuredOffset::hasOffset() const { return hasOffset_; }
+std::unique_ptr<HorizontalOffsetImpl> UnstructuredOffset::cloneImpl() const {
+  return std::make_unique<UnstructuredOffset>(hasOffset_);
+}
+
+bool UnstructuredOffset::equalsImpl(HorizontalOffsetImpl const& other) const {
+  auto const& uo_other = dynamic_cast<UnstructuredOffset const&>(other);
+  return uo_other.hasOffset_ == hasOffset_;
+}
+void UnstructuredOffset::addImpl(HorizontalOffsetImpl const& other) {
+  auto const& uo_other = dynamic_cast<UnstructuredOffset const&>(other);
+  hasOffset_ = hasOffset_ || uo_other.hasOffset_;
+}
+
+bool UnstructuredOffset::isZeroImpl() const { return !hasOffset_; }
+
+// HorizontalOffset
+HorizontalOffset::HorizontalOffset(cartesian_) : impl_(std::make_unique<CartesianOffset>()) {}
+HorizontalOffset::HorizontalOffset(cartesian_, int iOffset, int jOffset)
+    : impl_(std::make_unique<CartesianOffset>(iOffset, jOffset)) {}
+
+HorizontalOffset::HorizontalOffset(unstructured_) : impl_(std::make_unique<UnstructuredOffset>()) {}
+HorizontalOffset::HorizontalOffset(unstructured_, bool hasOffset)
+    : impl_(std::make_unique<UnstructuredOffset>(hasOffset)) {}
+
+HorizontalOffset::HorizontalOffset(HorizontalOffset const& other) { *this = other; }
+HorizontalOffset& HorizontalOffset::operator=(HorizontalOffset const& other) {
+  if(other.impl_)
+    impl_ = other.impl_->clone();
+  else
+    impl_ = nullptr;
+  return *this;
+}
+
+bool HorizontalOffset::operator==(HorizontalOffset const& other) const {
+  if(impl_ && other.impl_)
+    return *impl_ == *other.impl_;
+  else if(impl_)
+    return isZero();
+  else if(other.impl_)
+    return other.isZero();
+  else
+    return true;
+}
+bool HorizontalOffset::operator!=(HorizontalOffset const& other) const { return !(*this == other); }
+
+HorizontalOffset& HorizontalOffset::operator+=(HorizontalOffset const& other) {
+  if(impl_ && other.impl_)
+    *impl_ += *other.impl_;
+  else if(other.impl_)
+    *this = other;
+  return *this;
+}
+bool HorizontalOffset::isZero() const { return !impl_ || impl_->isZero(); }
+
+// Offsets
+
+Offsets::Offsets(HorizontalOffset const& hOffset, int vOffset)
+    : horizontalOffset_(hOffset), verticalOffset_(vOffset) {}
+
+Offsets::Offsets(cartesian_, int i, int j, int k)
+    : horizontalOffset_(cartesian, i, j), verticalOffset_(k) {}
+Offsets::Offsets(cartesian_, std::array<int, 3> const& structuredOffsets)
+    : Offsets(cartesian, structuredOffsets[0], structuredOffsets[1], structuredOffsets[2]) {}
+Offsets::Offsets(cartesian_) : horizontalOffset_(cartesian) {}
+
+Offsets::Offsets(unstructured_, bool hasOffset, int k)
+    : horizontalOffset_(unstructured, hasOffset), verticalOffset_(k) {}
+Offsets::Offsets(unstructured_) : horizontalOffset_(unstructured) {}
+
+int Offsets::verticalOffset() const { return verticalOffset_; }
+HorizontalOffset const& Offsets::horizontalOffset() const { return horizontalOffset_; }
+
+bool Offsets::operator==(Offsets const& other) const {
+  return horizontalOffset_ == other.horizontalOffset_ && verticalOffset_ == other.verticalOffset_;
+}
+bool Offsets::operator!=(Offsets const& other) const { return !(*this == other); }
+
+Offsets& Offsets::operator+=(Offsets const& other) {
+  horizontalOffset_ += other.horizontalOffset_;
+  verticalOffset_ += other.verticalOffset_;
+  return *this;
+}
+
+bool Offsets::isZero() const { return verticalOffset_ == 0 && horizontalOffset_.isZero(); }
 
 std::string to_string(unstructured_, Offsets const& offset) {
   auto const& hoffset = offset_cast<UnstructuredOffset const&>(offset.horizontalOffset());
@@ -44,25 +172,6 @@ std::string to_string(Offsets const& offset) {
                            return "<no_horizontal_offset>,"s +
                                   std::to_string(offset.verticalOffset());
                          });
-}
-
-bool CartesianOffset::equalsImpl(HorizontalOffsetImpl const& other) const {
-  auto const& co_other = dynamic_cast<CartesianOffset const&>(other);
-  return co_other.horizontalOffset_ == horizontalOffset_;
-}
-
-void CartesianOffset::addImpl(HorizontalOffsetImpl const& other) {
-  auto const& co_other = dynamic_cast<CartesianOffset const&>(other);
-  horizontalOffset_[0] += co_other.horizontalOffset_[0];
-  horizontalOffset_[1] += co_other.horizontalOffset_[1];
-}
-bool UnstructuredOffset::equalsImpl(HorizontalOffsetImpl const& other) const {
-  auto const& uo_other = dynamic_cast<UnstructuredOffset const&>(other);
-  return uo_other.hasOffset_ == hasOffset_;
-}
-void UnstructuredOffset::addImpl(HorizontalOffsetImpl const& other) {
-  auto const& uo_other = dynamic_cast<UnstructuredOffset const&>(other);
-  hasOffset_ = hasOffset_ || uo_other.hasOffset_;
 }
 
 Offsets operator+(Offsets o1, Offsets const& o2) { return o1 += o2; }
