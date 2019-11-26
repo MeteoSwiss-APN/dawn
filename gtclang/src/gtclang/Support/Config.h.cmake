29ef7684a12f43c2264caebff5a9b79224ecd33b
Hannes Vogt <hannes@havogt.de>
2019-11-26 12:03:48 +0100

    build the full version string

--- gtclang/src/gtclang/Support/Config.h.cmake
+++ gtclang/src/gtclang/Support/Config.h.cmake
@@ -1,15 +1,15 @@
 //===--------------------------------------------------------------------------------*- C++ -*-===//
-//                         _       _                   
-//                        | |     | |                  
-//                    __ _| |_ ___| | __ _ _ __   __ _ 
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
 //                   / _` | __/ __| |/ _` | '_ \ / _` |
 //                  | (_| | || (__| | (_| | | | | (_| |
 //                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
 //                    __/ |                       __/ |
-//                   |___/                       |___/ 
+//                   |___/                       |___/
 //
 //
-//  This file is distributed under the MIT License (MIT). 
+//  This file is distributed under the MIT License (MIT).
 //  See LICENSE.txt for details.
 //
 //===------------------------------------------------------------------------------------------===//
@@ -17,19 +17,19 @@
 #ifndef GTCLANG_SUPPORT_CONFIG_H
 #define GTCLANG_SUPPORT_CONFIG_H
 
-// Major version of GTClang 
+// Major version of GTClang
 #define GTCLANG_VERSION_MAJOR ${VERSION_MAJOR}
 
-// Minor version of GTClang 
+// Minor version of GTClang
 #define GTCLANG_VERSION_MINOR ${VERSION_MINOR}
 
-// Patch version of GTClang 
+// Patch version of GTClang
 #define GTCLANG_VERSION_PATCH ${VERSION_PATCH}
 
-// GTClang version string 
-#define GTCLANG_VERSION_STR "${VERSION}"
+// GTClang version string
+#define GTCLANG_VERSION_STR "${GTClang_VERSION}"
 
-// GTClang full version string 
+// GTClang full version string
 #define GTCLANG_FULL_VERSION_STR "${GTCLANG_FULL_VERSION}"
 
 // Path to gridtools clang DSL headers
@@ -39,4 +39,3 @@
 #define GTCLANG_CLANG_RESSOURCE_INCLUDE_PATH "${CLANG_RESSOURCE_INCLUDE_PATH}"
 
 #endif
-
