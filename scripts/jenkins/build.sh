0a9109440102b22260cf2425074627a3dd8a5111
Hannes Vogt <hannes@havogt.de>
2019-11-28 10:48:47 +0100

    add top-level build script

---
+++ scripts/jenkins/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+BASEPATH_SCRIPT=$(dirname "${0}")
+source ${BASEPATH_SCRIPT}/machine_env.sh
+source ${BASEPATH_SCRIPT}/env_${myhost}.sh
+
+if [ -z ${myhost+x} ]; then
+  echo "myhost is unset"
+  exit 1
+fi
+
+SCRIPT=`basename $0`
+
+function help {
+  echo -e "Basic usage:$SCRIPT "\\n
+  echo -e "The following switches are recognized. $OFF "
+  echo -e "-i sets the installation directory"
+  echo -e "-h Shows this help"
+  exit 1
+}
+echo "####### executing: $0 $* (PID=$$ HOST=$HOSTNAME TIME=`date '+%D %H:%M:%S'`)"
+while getopts i: flag; do
+  case $flag in
+    i)
+      INSTALL_DIR=$OPTARG
+      ;;
+    h)
+      help
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${OFF} not allowed."
+      help
+      ;;
+  esac
+done
+
+base_dir=${BASEPATH_SCRIPT}/../..
+build_dir=${base_dir}/build
+
+mkdir -p $build_dir
+cd $build_dir
+
+if [ -z ${PROTOBUFDIR+x} ]; then
+ echo "PROTOBUFDIF needs to be set in the machine env"
+fi
+
+#TODO -DDAWN_BUNDLE_JAVA=ON
+CMAKE_ARGS="-DCMAKE_BUILD_TYPE=${build_type} \
+            -DProtobuf_DIR=${PROTOBUFDIR} \
+            -DGTCLANG_REQUIRE_UNSTRUCTURED_TESTING=ON"
+
+if [ -n ${INSTALL_DIR} ]; then
+  rm -rf ${INSTALL_DIR}
+  CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+else
+  CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${base_dir}/install"
+fi
+
+cmake ${CMAKE_ARGS} ..
+make -j8 install
+
+# Run unittests
+ctest -VV -C ${build_type} --output-on-failure --force-new-ctest-process
