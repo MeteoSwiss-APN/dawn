2768c8d316a9bb2a7dbc23ed80520f1c960384e4
Stagno <giacomo.serafini@mail.polimi.it>
2019-12-09 12:39:08 +0100

    Point to new protobuf install location (#511)
    ## Technical Description
    
    Protobuf gets now installed in `/project/c14/install/daint/protobuf/`. Previously it was installed in `/scratch/snx3000`, which gets wiped once in a while.

--- dawn/scripts/jenkins/env_daint.sh
+++ dawn/scripts/jenkins/env_daint.sh
@@ -7,6 +7,6 @@ module load cray-python/3.6.5.7
 module swap PrgEnv-cray PrgEnv-gnu
 
 export BOOST_DIR=/project/c14/install/daint/boost/boost_1_67_0/
-export PROTOBUFDIR="/scratch/snx3000/jenkins/workspace/protobuf/slave/daint/install/lib64/cmake/protobuf/"
+export PROTOBUFDIR="/project/c14/install/daint/protobuf/lib64/cmake/protobuf/"
 export CXX=`which g++`
 export CC=`which gcc`
