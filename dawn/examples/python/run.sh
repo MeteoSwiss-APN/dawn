cf2e3d3a076a76afbeadbd1b283c2c37fdd3f1cb
Tobias Wicky <tobiasw@vulcan.com>
2019-12-16 14:34:17 +0100

    add unstructured to tests

--- dawn/examples/python/run.sh
+++ dawn/examples/python/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-examples=("copy_stencil" "hori_diff_stencil" "tridiagonal_solve_stencil" "unstructured_stencil")
+examples=("copy_stencil" "hori_diff_stencil" "tridiagonal_solve_stencil" "unstructured_stencil" "global_index_stencil")
 verification=("copy_stencil" "hori_diff_stencil" "tridiagonal_solve_stencil")
 
 for file in "${examples[@]}"
