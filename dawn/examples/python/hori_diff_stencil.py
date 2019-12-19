e7410cfafe3e96116bb3d644fe4089c8c3df94c5
Enrique G. Paredes <enriqueg@cscs.ch>
2019-12-19 12:39:37 +0100

    Add support new SIR.gridType element to serialization utils and fix examples

--- dawn/examples/python/hori_diff_stencil.py
+++ dawn/examples/python/hori_diff_stencil.py
@@ -108,11 +108,13 @@ def main(args: argparse.Namespace):
         ]
     )
 
-    vertical_region_stmt = sir_utils.make_vertical_region_decl_stmt(body_ast, interval, SIR.VerticalRegion.Forward)
+    vertical_region_stmt = sir_utils.make_vertical_region_decl_stmt(
+        body_ast, interval, SIR.VerticalRegion.Forward
+    )
 
     sir = sir_utils.make_sir(
-        sir_utils.GridType.Value("Cartesian"),
         OUTPUT_FILE,
+        SIR.GridType.Value("Cartesian"),
         [
             sir_utils.make_stencil(
                 OUTPUT_NAME,
@@ -141,8 +143,15 @@ def main(args: argparse.Namespace):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate a simple horizontal diffusion stencil using Dawn compiler")
+    parser = argparse.ArgumentParser(
+        description="Generate a simple horizontal diffusion stencil using Dawn compiler"
+    )
     parser.add_argument(
-        "-v", "--verbose", dest="verbose", action="store_true", default=False, help="Print the generated SIR",
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Print the generated SIR",
     )
     main(parser.parse_args())
