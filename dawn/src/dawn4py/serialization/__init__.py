da121b8db89d2d3436b6593e2a7a1fcb5e6d72c1
Tobias Wicky <wicky.tob@gmail.com>
2019-12-20 14:13:21 +0100

    Python bindings v2 (#544)
    Adds an installable Python package providing a Python interface to the Dawn compiler object and the SIR/IIR serialization utilities.
    
        Add src/dawn4py folder with the bindings sources
            Brief description and installation instructions: README.md
            installation script and configuration: setup.py, pyproject.toml, CMakelists.txt, _version.py
            C++ bindings using pybind11: _dawn4py.cpp
            higher level convenience functions wrapping the bindings: __init__.py, utils.py
            refactored serialization tools and protobuf stuff: dawn4py/serialization/...
    
        Add scripts/refactor/make_pybind11_sources.py and scripts/refactor/_dawn4py.cpp.in files to semi-automatize (only the bindings for Options object is generated dynamically) the generation of the Python bindings.
    
        Refactor the python examples in examples/python to use the new bindings

---
+++ dawn/src/dawn4py/serialization/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##===-----------------------------------------------------------------------------*- Python -*-===##
+##                          _
+##                         | |
+##                       __| | __ ___      ___ ___
+##                      / _` |/ _` \ \ /\ / / '_  |
+##                     | (_| | (_| |\ V  V /| | | |
+##                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+##
+##
+##  This file is distributed under the MIT License (MIT).
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+"""
+Python serialization tools for SIR and IIR.
+"""
+
+from . import SIR, IIR
+
+from .utils import *
+from .error import *
