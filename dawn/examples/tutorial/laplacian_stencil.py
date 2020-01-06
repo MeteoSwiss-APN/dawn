a244ad56ab0355daf1659e2a40532a1018f42206
Tobias Wicky <wicky.tob@gmail.com>
2020-01-06 14:07:10 +0100

    fix tutorial with new python (#556)
    Fixes the tutorial with the new python usage

--- dawn/examples/tutorial/laplacian_stencil.py
+++ dawn/examples/tutorial/laplacian_stencil.py
@@ -28,163 +28,100 @@ The program contains two parts:
 
 """
 
+
 import argparse
-import ctypes
-import os.path
-import sys
-import textwrap
-from ctypes import *
-from optparse import OptionParser
+import os
 
-from config import __dawn_install_module__, __dawn_install_dawnclib__
-from dawn import *
-from dawn import sir_printer
+import dawn4py
+from dawn4py.serialization import SIR
+from dawn4py.serialization import utils as sir_utils
 
-dawn = CDLL(__dawn_install_dawnclib__)
+OUTPUT_NAME = "laplacian_stencil"
+OUTPUT_FILE = f"{OUTPUT_NAME}_from_python.cpp"
+OUTPUT_PATH = os.path.join(os.path.dirname(__file__), OUTPUT_FILE)
 
 
-def create_vertical_region_stmt() -> VerticalRegionDeclStmt:
-    """ create a vertical region statement for the stencil
-    """
+def main(args: argparse.Namespace):
+    interval = sir_utils.make_interval(SIR.Interval.Start, SIR.Interval.End, 0, 0)
 
-    interval = make_interval(Interval.Start, Interval.End, 0, 0)
-
-    # create the out = in[i+1] statement
-    body_ast = make_ast(
-        [  
-        make_assignment_stmt(
-            make_field_access_expr("out", [0, 0, 0]),
-            make_binary_operator(
-              make_binary_operator(
-                make_binary_operator(
-                  make_field_access_expr("in", [0, 0, 0]),
-                  "*",
-                  make_literal_access_expr("-4.0", BuiltinType.Float)
-                ),
-                "+",
-                make_binary_operator(
-                    make_field_access_expr("in", [1, 0, 0]),
-                    "+",
-                    make_binary_operator(
-                        make_field_access_expr("in", [-1, 0, 0]),
+    # create the laplace statement
+    body_ast = sir_utils.make_ast(
+        [
+            sir_utils.make_assignment_stmt(
+                sir_utils.make_field_access_expr("out", [0, 0, 0]),
+                sir_utils.make_binary_operator(
+                    sir_utils.make_binary_operator(
+                        sir_utils.make_binary_operator(
+                            sir_utils.make_field_access_expr("in", [0, 0, 0]),
+                            "*",
+                            sir_utils.make_literal_access_expr("-4.0", sir_utils.BuiltinType.Float),
+                        ),
                         "+",
-                        make_binary_operator(
-                            make_field_access_expr("in", [0, 1, 0]),
+                        sir_utils.make_binary_operator(
+                            sir_utils.make_field_access_expr("in", [1, 0, 0]),
                             "+",
-                            make_field_access_expr("in", [0, -1, 0])
+                            sir_utils.make_binary_operator(
+                                sir_utils.make_field_access_expr("in", [-1, 0, 0]),
+                                "+",
+                                sir_utils.make_binary_operator(
+                                    sir_utils.make_field_access_expr("in", [0, 1, 0]),
+                                    "+",
+                                    sir_utils.make_field_access_expr("in", [0, -1, 0]),
+                                ),
                             ),
                         ),
                     ),
+                    "/",
+                    sir_utils.make_binary_operator(
+                        sir_utils.make_var_access_expr("dx", is_external=True),
+                        "*",
+                        sir_utils.make_var_access_expr("dx", is_external=True),
+                    ),
                 ),
-              "/",
-              make_binary_operator(
-                make_var_access_expr("dx", is_external=True),
-                "*",
-                make_var_access_expr("dx", is_external=True),
-              )
-            ),           
-            "="
-          )
+                "=",
+            ),
         ]
     )
 
-    vertical_region_stmt = make_vertical_region_decl_stmt(
-        body_ast, interval, VerticalRegion.Forward)
-    return vertical_region_stmt
+    vertical_region_stmt = sir_utils.make_vertical_region_decl_stmt(body_ast, interval, SIR.VerticalRegion.Forward)
 
+    stencils_globals = sir_utils.GlobalVariableMap()
+    stencils_globals.map["dx"].double_value = 0.0
 
-stencilsglobals  = GlobalVariableMap()
-myglobal = stencilsglobals.map['dx'].double_value = 0.;
-
-hir = make_sir("laplacian_stencil_from_python.cpp", stencils=[
-    make_stencil(
-        "laplacian_stencil",
-        make_ast([create_vertical_region_stmt()]),
-        [make_field("out",), make_field("in")]
+    sir = sir_utils.make_sir(
+        OUTPUT_FILE,
+        SIR.GridType.Value("Cartesian"),
+        [
+            sir_utils.make_stencil(
+                OUTPUT_NAME,
+                sir_utils.make_ast([vertical_region_stmt]),
+                [sir_utils.make_field("out"), sir_utils.make_field("in")],
+            )
+        ],
+        global_variables=stencils_globals,
     )
 
-],global_variables=stencilsglobals)
+    # print the SIR
+    if args.verbose:
+        sir_utils.pprint(sir)
 
-parser = OptionParser()
-parser.add_option("-v", "--verbose",
-                  action="store_true", dest="verbose", default=False,
-                  help="print the SIR")
+    # serialize the SIR to file
+    sir_file = open("./laplacian_stencil_from_python.sir", "wb")
+    sir_file.write(sir.SerializeToString())
+    sir_file.close()
 
-(options, args) = parser.parse_args()
+    # compile
+    code = dawn4py.compile(sir, backend="c++-naive")
 
-# Print the SIR to stdout 
-T = textwrap.TextWrapper(
-    initial_indent=' ' * 1, width=120, subsequent_indent=' ' * 1)
-des = sir_printer.SIRPrinter()
+    # write to file
+    print(f"Writing generated code to '{OUTPUT_PATH}'")
+    with open(OUTPUT_PATH, "w") as f:
+        f.write(code)
 
-des.visit_global_variables(stencilsglobals)
 
-for stencil in hir.stencils:
-    des.visit_stencil(stencil)
-
-# serialize the hir to pass it to the compiler
-hirstr = hir.SerializeToString()
-fhir = open("./laplacian_stencil_from_python.sir", "wb");
-fhir.write(hirstr)
-fhir.close
-
-# create the options to control the compiler
-dawn.dawnOptionsCreate.restype = c_void_p
-options = dawn.dawnOptionsCreate()
-
-# we set the backend of the compiler to cuda
-dawn.dawnOptionsEntryCreateString.restype = c_void_p
-dawn.dawnOptionsEntryCreateString.argtypes = [
-    ctypes.c_char_p
-]
-backend = dawn.dawnOptionsEntryCreateString("c++-naive".encode('utf-8'))
-
-dawn.dawnOptionsSet.argtypes = [
-    ctypes.c_void_p,
-    ctypes.c_char_p,
-    ctypes.c_void_p
-]
-dawn.dawnOptionsSet(options, "Backend".encode('utf-8'), backend)
-
-# call the compiler that generates a translation unit
-
-dawn.dawnCompile.restype = c_void_p
-dawn.dawnCompile.argtypes = [
-    ctypes.c_char_p,
-    ctypes.c_int,
-    ctypes.c_void_p
-]
-tu = dawn.dawnCompile(hirstr, len(hirstr), options)
-stencilname = "laplacian_stencil"
-b_stencilName = stencilname.encode('utf-8')
-# get the code of the translation unit for the given stencil
-dawn.dawnTranslationUnitGetStencil.restype = c_void_p
-dawn.dawnTranslationUnitGetStencil.argtypes = [
-    ctypes.c_void_p,
-    ctypes.c_char_p
-]
-
-dawn.dawnTranslationUnitGetPPDefines.restype = c_void_p
-dawn.dawnTranslationUnitGetPPDefines.argtypes = [
-    ctypes.c_void_p,
-    POINTER(POINTER(ctypes.c_char_p)),
-    POINTER(ctypes.c_int)
-]
-
-pp_defines = POINTER(ctypes.c_char_p)()
-num_pp_defines = ctypes.c_int()
-dawn.dawnTranslationUnitGetPPDefines(tu, byref(pp_defines), byref(num_pp_defines))
-num_pp_defines = num_pp_defines.value
-
-global_defines = dawn.dawnTranslationUnitGetGlobals(tu)
-code = dawn.dawnTranslationUnitGetStencil(tu, b_stencilName)
-
-# write to file
-f = open(os.path.dirname(os.path.realpath(__file__))
-         + "/laplacian_stencil_from_python.cpp", "w")
-for i in range(0,num_pp_defines):
-    f.write(ctypes.c_char_p(pp_defines[i]).value.decode("utf-8") + "\n")    
-f.write(ctypes.c_char_p(global_defines).value.decode("utf-8"))
-f.write(ctypes.c_char_p(code).value.decode("utf-8"))
-
-f.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a simple laplace stencil using Dawn compiler")
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", default=False, help="Print the generated SIR",
+    )
+    main(parser.parse_args())
