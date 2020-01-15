3d582251afdbae6e39114280fae17c2334cf168b
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-01-15 15:10:05 +0100

    Fix all update_references.sh scripts
    And update references of iir deserializer tests

--- dawn/test/integration-test/reference_iir/update_references.sh
+++ dawn/test/integration-test/reference_iir/update_references.sh
@@ -22,27 +22,27 @@ done
 script=$(readlink -f $0)
 script_path=$(dirname $script)
 repo_path=$(git rev-parse --show-toplevel)
+integration_test_dir="$repo_path/dawn/test/integration-test"
 
 # find DawnUpdateIIRReferences
 if [[ "$dawn_path" == "" ]]; then
-    if [[ -f "$repo_path/dawn/build/bin/integrationtest/DawnUpdateIIRReferences" ]]; then
-        update_path="$repo_path/dawn/build/bin/integrationtest/DawnUpdateIIRReferences"
-    elif [[ -f "$repo_path/dawn/bundle/build/dawn-prefix/src/dawn-build/bin/integrationtest/" ]]; then
-        update_path="$repo_path/dawn/bundle/build/dawn-prefix/src/dawn-build/bin/integrationtest/DawnUpdateIIRReferences"
+    if [[ -f "$repo_path/build/dawn/test/integration-test/DawnUpdateIIRReferences" ]]; then
+        update_path="$repo_path/build/dawn/test/integration-test/DawnUpdateIIRReferences"
     else
         echo "Cannot find DawnUpdateIIRReferences."
         exit 1
     fi
 else
-    if [[ -f "$dawn_path/bin/integrationtest/DawnUpdateIIRReferences" ]]; then
-        update_path=$(readlink -f "$dawn_path/bin/integrationtest/DawnUpdateIIRReferences")
+    if [[ -f "$dawn_path/test/integration-test/DawnUpdateIIRReferences" ]]; then
+        update_path=$(readlink -f "$dawn_path/test/integration-test/DawnUpdateIIRReferences")
+    elif [[ -f "$dawn_path/dawn/test/integration-test/DawnUpdateIIRReferences" ]]; then
+        update_path=$(readlink -f "$dawn_path/dawn/test/integration-test/DawnUpdateIIRReferences")
     else
         echo "Cannot find DawnUpdateIIRReferences."
         exit 1
     fi
 fi
-
 # run the script
-pushd $(dirname $update_path) > /dev/null
+pushd $integration_test_dir > /dev/null
   $update_path
 popd > /dev/null
