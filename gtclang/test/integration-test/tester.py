554f989bc8026be36894f7500008f922fa10f024
Johann Dahm <johannd@vulcan.com>
2020-01-18 17:12:25 -0800

    Add json comparison function to ignore patterns

--- gtclang/test/integration-test/tester.py
+++ gtclang/test/integration-test/tester.py
@@ -4,18 +4,71 @@ import re
 import subprocess
 import filecmp
 import argparse
+from json import loads as load_json
 from difflib import unified_diff
 
 
 def print_error(message):
-    print("FAILURE:", message, file=sys.stderr)
+    print("FAILURE: " + message, file=sys.stderr)
 
 
-def get_line_number(content, m):
-    return list(map(lambda x: m in x, content.split("\n"))).index(True) + 1
+def print_test(message):
+    print("TEST:", message)
 
 
-def run_test(content, gtclang_exec, filename, verbose=False):
+def compare_json_files(output, reference, ignore_keys=[]):
+    def read_file(filename):
+        if not os.path.exists(filename):
+            raise ValueError("Could not find file: {}".format(filename))
+        else:
+            with open(filename, mode="r") as f:
+                content = f.readlines()
+        return content
+
+    def compare_json_trees(t1, t2):
+        # If entering here, t1 and t2 could be lists or dicts (of values, lists, or dicts)
+        for v1, v2 in zip(t1, t2):
+            if isinstance(t1, list):
+                # Then v1 and v2 are _values_
+                if any(isinstance(v1, x) for x in (list, dict)):
+                    if not compare_json_trees(v1, v2):
+                        return False
+                if v1 != v2:
+                    return False
+            elif isinstance(t1, dict):
+                # Then v1 and v2 are _keys_
+                if any(isinstance(t1[v1], x) for x in (list, dict)):
+                    if not compare_json_trees(t1[v1], t2[v2]):
+                        return False
+                if v1 != v2 and v1 not in ignore_keys:
+                    return False
+                if t1[v1] != t2[v2] and v1 not in ignore_keys:
+                    return False
+        return True
+
+    output_lines = read_file(output)
+    output_json = load_json(" ".join(output_lines))
+    reference_lines = read_file(reference)
+    reference_json = load_json(" ".join(reference_lines))
+
+    if not compare_json_trees(reference_json, output_json):
+        # Compare files
+        sys.stdout.writelines(
+            unified_diff(
+                [l for l in output_lines if not any((p in l for p in ignore_keys))],
+                [l for l in reference_lines if not any((p in l for p in ignore_keys))],
+                fromfile=output,
+                tofile=reference,
+            )
+        )
+        return False
+    return True
+
+
+def run_test(content, gtclang_exec, filename, verbose=False, ignore_keys=[]):
+    def get_line_number(content, m):
+        return list(map(lambda x: m in x, content.split("\n"))).index(True) + 1
+
     dirname, basename = (
         os.path.dirname(args.source),
         os.path.splitext(os.path.basename(args.source))[0],
@@ -29,7 +82,7 @@ def run_test(content, gtclang_exec, filename, verbose=False):
         "EXPECTED_LINE": re.compile(r"//\s*EXPECTED\s*%line%:\s*(?P<output>[^\n]+)"),
         "EXPECTED": re.compile(r"//\s*EXPECTED:\s*(?!%line%)(?P<output>[^\n]+)"),
         "EXPECTED_FILE": re.compile(
-            r"//\s*EXPECTED_FILE:\s*OUTPUT:\s*(?P<output>[^\s]+)\s*REFERENCE:\s*(?P<reference>[^\s]+)"
+            r"//\s*EXPECTED_FILE:\s*OUTPUT:\s*(?P<output>[^\s]+)\s*REFERENCE:\s*(?P<reference>[^\s]+)(?P<remainder>[^\n]*)"
         ),
         "EXPECTED_ERROR": re.compile(r"//\s*EXPECTED_ERROR:\s*(?P<output>[^\n]+)"),
     }
@@ -70,6 +123,7 @@ def run_test(content, gtclang_exec, filename, verbose=False):
                 .replace("line", str(get_line_number(content, m)))
             )
             m = m.replace(line_match.group(), str(line))
+        print_test("EXPECTED_LINE: " + m)
         # Look for line in stdout
         if not re.search(m, proc.stdout):
             print_error("Could not match: {}".format(m))
@@ -80,6 +134,7 @@ def run_test(content, gtclang_exec, filename, verbose=False):
     for m in m_errors:
         # Replace all possible patterns with regex expressions
         m = m.strip(" ")
+        print_test("EXPECTED_ERROR: " + m)
         # Look for line in stdout
         if re.search(m, proc.stderr) is None:
             print_error("Could not find error in stdout: {}".format(m))
@@ -92,30 +147,37 @@ def run_test(content, gtclang_exec, filename, verbose=False):
     # EXPECTED_FILE
     m_expected_file = patterns["EXPECTED_FILE"].findall(content)
     for m in m_expected_file:
+        files = (m[0], m[1])
+        if len(m) > 2:
+            # test for IGNORE pattern
+            ignore_keys += re.findall(r"IGNORE:\s*(?P<pattern>[^ ]+)", m[2])
         # Replace %filename% in string expression and split on commas
-        tests = zip(*(x.replace(r"%filename%", basename).split(",") for x in m))
+        tests = zip(*(x.replace(r"%filename%", basename).split(",") for x in files))
         for output, reference in tests:
             # Add source directory path to reference
             reference = os.path.join(dirname, reference)
-
-            if not os.path.exists(output):
-                print_error("Could not find file: {}".format(output))
-                error_happened = True
-            if not os.path.exists(reference):
-                print_error("Could not find file: {}".format(reference))
-                error_happened = True
-
-            # Compare files
-            if not filecmp.cmp(output, reference, shallow=False):
-                print_error("Files {} and {} do not match".format(output, reference))
-                with open(output, mode="r") as f:
-                    s1 = f.readlines()
-                with open(reference, mode="r") as f:
-                    s2 = f.readlines()
-                sys.stdout.writelines(
-                    unified_diff(s1, s2, fromfile=output, tofile=reference)
+            print_test(
+                "EXPECTED_FILE: OUTPUT={} REFERENCE={}{}".format(
+                    output, reference, "".join(" IGNORE: " + k for k in ignore_keys)
                 )
-                error_happened = True
+            )
+
+            if all(
+                (
+                    os.path.splitext(f)[-1] in (".iir", ".sir", ".json")
+                    for f in (output, reference)
+                )
+            ):
+                # All these are json trees, so we can compare them
+                same_trees = compare_json_files(
+                    output, reference, ignore_keys=ignore_keys
+                )
+                if not same_trees:
+                    error_happened = True
+                    print_error("JSON trees do not match")
+
+            else:
+                raise ValueError("Not yet implemented.")
 
     # Ensure the compiler was successful
     successful_code = 0
@@ -144,6 +206,13 @@ if __name__ == "__main__":
     parser.add_argument("gtclang", type=str, help="GTClang executable")
     parser.add_argument("source", type=str, help="Source code to compile")
     parser.add_argument(
+        "-i",
+        "--ignore",
+        nargs="*",
+        help="Extra keys to ignore in comparison",
+        default=[],
+    )
+    parser.add_argument(
         "-v", "--verbose", help="modify output verbosity", action="store_true"
     )
     parser.add_argument("options", nargs="*")
@@ -152,5 +221,11 @@ if __name__ == "__main__":
     content = ""
     with open(args.source, mode="r") as f:
         content = f.read().rstrip("\n")
-    ret_val = run_test(content, args.gtclang, args.source, verbose=args.verbose)
+    ret_val = run_test(
+        content,
+        args.gtclang,
+        args.source,
+        verbose=args.verbose,
+        ignore_keys=args.ignore,
+    )
     sys.exit(ret_val)
