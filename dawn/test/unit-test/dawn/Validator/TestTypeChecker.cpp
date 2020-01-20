01c3a25ff77c7ca3defba6b93116aee488b50d8f
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-01-20 14:36:57 +0100

    Add more TypeChecker tests

--- dawn/test/unit-test/dawn/Validator/TestTypeChecker.cpp
+++ dawn/test/unit-test/dawn/Validator/TestTypeChecker.cpp
@@ -20,7 +20,57 @@ using namespace dawn;
 
 namespace {
 
-TEST(TypeCheckerTest, UnstructuredCheck01) {
+TEST(TypeCheckerTest, AssignmentCase0_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field1 =
+      b.field("sparse_field1", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto sparse_field2 = b.field("sparse_field2", {LocType::Edges, LocType::Cells, LocType::Cells});
+
+  EXPECT_DEATH(
+      b.build("fail", b.stencil(b.multistage(
+                          LoopOrderKind::Parallel,
+                          b.stage(b.doMethod(
+                              dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                              b.stmt(b.assignExpr(b.at(sparse_field1), b.at(sparse_field2)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, AssignmentCase0_1) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field1 = b.field("sparse_field1", {LocType::Edges, LocType::Cells});
+  auto sparse_field2 = b.field("sparse_field2", {LocType::Edges, LocType::Cells, LocType::Cells});
+
+  EXPECT_DEATH(
+      b.build("fail", b.stencil(b.multistage(
+                          LoopOrderKind::Parallel,
+                          b.stage(b.doMethod(
+                              dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                              b.stmt(b.assignExpr(b.at(sparse_field1), b.at(sparse_field2)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, AssignmentCase1_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field = b.field("sparse_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto dense_field = b.field("dense_field", LocType::Cells);
+
+  EXPECT_DEATH(
+      b.build(
+          "fail",
+          b.stencil(b.multistage(
+              LoopOrderKind::Parallel,
+              b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                                 b.stmt(b.assignExpr(b.at(sparse_field), b.at(dense_field)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, AssignmentCase2_0) {
   using namespace dawn::iir;
   using LocType = dawn::ast::LocationType;
 
@@ -35,7 +85,92 @@ TEST(TypeCheckerTest, UnstructuredCheck01) {
                                              b.stmt(b.assignExpr(b.at(cell_f), b.at(edge_f)))))))),
       ".*checkDimensionsConsistency.*");
 }
-TEST(TypeCheckerTest, UnstructuredCheck02) {
+TEST(TypeCheckerTest, AssignmentNoCase_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto dense_field = b.field("dense_field", LocType::Vertices);
+  auto sparse_field = b.field("sparse_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+
+  EXPECT_DEATH(
+      b.build(
+          "fail",
+          b.stencil(b.multistage(
+              LoopOrderKind::Parallel,
+              b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                                 b.stmt(b.assignExpr(b.at(dense_field), b.at(sparse_field)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, BinaryOpCase0_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field1 =
+      b.field("sparse_field1", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto sparse_field2 = b.field("sparse_field2", {LocType::Edges, LocType::Cells, LocType::Cells});
+
+  EXPECT_DEATH(
+      b.build("fail",
+              b.stencil(b.multistage(
+                  LoopOrderKind::Parallel,
+                  b.stage(b.doMethod(
+                      dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                      b.stmt(b.binaryExpr(b.at(sparse_field1), b.at(sparse_field2), Op::plus))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, BinaryOpCase0_1) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field1 = b.field("sparse_field1", {LocType::Edges, LocType::Cells});
+  auto sparse_field2 = b.field("sparse_field2", {LocType::Edges, LocType::Cells, LocType::Cells});
+
+  EXPECT_DEATH(
+      b.build("fail",
+              b.stencil(b.multistage(
+                  LoopOrderKind::Parallel,
+                  b.stage(b.doMethod(
+                      dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                      b.stmt(b.binaryExpr(b.at(sparse_field1), b.at(sparse_field2), Op::plus))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, BinaryOpCase1_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto dense_field = b.field("dense_field", LocType::Cells);
+  auto sparse_field = b.field("sparse_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+
+  EXPECT_DEATH(
+      b.build("fail",
+              b.stencil(b.multistage(
+                  LoopOrderKind::Parallel,
+                  b.stage(b.doMethod(
+                      dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                      b.stmt(b.binaryExpr(b.at(dense_field), b.at(sparse_field), Op::plus))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, BinaryOpCase2_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field = b.field("sparse_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto dense_field = b.field("dense_field", LocType::Cells);
+
+  EXPECT_DEATH(
+      b.build("fail", b.stencil(b.multistage(
+                          LoopOrderKind::Parallel,
+                          b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                                             b.stmt(b.binaryExpr(b.at(sparse_field),
+                                                                 b.at(dense_field), Op::plus))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, BinaryOpCase3_0) {
   using namespace dawn::iir;
   using LocType = dawn::ast::LocationType;
 
@@ -52,7 +187,7 @@ TEST(TypeCheckerTest, UnstructuredCheck02) {
                                  b.stmt(b.binaryExpr(b.at(cell_f), b.at(edge_f), Op::plus))))))),
       ".*checkDimensionsConsistency.*");
 }
-TEST(TypeCheckerTest, UnstructuredCheck03) {
+TEST(TypeCheckerTest, ReduceDense_0) {
   using namespace dawn::iir;
   using LocType = dawn::ast::LocationType;
 
@@ -72,7 +207,7 @@ TEST(TypeCheckerTest, UnstructuredCheck03) {
                                              b.lit(0.), LocType::Edges, LocType::Cells)))))))),
       ".*checkDimensionsConsistency.*");
 }
-TEST(TypeCheckerTest, UnstructuredCheck04) {
+TEST(TypeCheckerTest, ReduceDense_1) {
   using namespace dawn::iir;
   using LocType = dawn::ast::LocationType;
 
@@ -90,6 +225,48 @@ TEST(TypeCheckerTest, UnstructuredCheck04) {
                                          b.reduceOverNeighborExpr(
                                              Op::plus, b.at(cell_field, HOffsetType::withOffset, 0),
                                              b.lit(0.), LocType::Cells, LocType::Edges)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, ReduceSparse_0) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field = b.field("edge_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto cell_field = b.field("cell_field", LocType::Cells);
+
+  EXPECT_DEATH(
+      b.build(
+          "fail",
+          b.stencil(b.multistage(
+              LoopOrderKind::Parallel,
+              b.stage(b.doMethod(
+                  dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                  b.stmt(b.assignExpr(b.at(cell_field),
+                                      b.reduceOverNeighborExpr(
+                                          Op::plus, b.at(sparse_field, HOffsetType::withOffset, 0),
+                                          b.lit(0.), LocType::Cells, LocType::Vertices)))))))),
+      ".*checkDimensionsConsistency.*");
+}
+TEST(TypeCheckerTest, ReduceSparse_1) {
+  using namespace dawn::iir;
+  using LocType = dawn::ast::LocationType;
+
+  UnstructuredIIRBuilder b;
+  auto sparse_field = b.field("edge_field", {LocType::Edges, LocType::Cells, LocType::Vertices});
+  auto edge_field = b.field("cell_field", LocType::Edges);
+
+  EXPECT_DEATH(
+      b.build(
+          "fail",
+          b.stencil(b.multistage(
+              LoopOrderKind::Parallel,
+              b.stage(b.doMethod(
+                  dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                  b.stmt(b.assignExpr(b.at(edge_field),
+                                      b.reduceOverNeighborExpr(
+                                          Op::plus, b.at(sparse_field, HOffsetType::withOffset, 0),
+                                          b.lit(0.), LocType::Edges, LocType::Cells)))))))),
       ".*checkDimensionsConsistency.*");
 }
 } // namespace
