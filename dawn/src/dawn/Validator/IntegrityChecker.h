da7dd8758c8354984ed41e7351aa4c1d0fff060d
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-01-21 16:03:48 -0800

    Globals opt away (#611)
    This PR adds an AST integrity check pass to the end of the Dawn compiler passes. The current version contains a single check, that assignment statements do not attempt to assign to constant expressions (literals). A semantic error is thrown when this pattern is found in the AST, such as an attempt to reassign a global variable that has been optimized away.

---
+++ dawn/src/dawn/Validator/IntegrityChecker.h
@@ -0,0 +1,74 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_OPTIMIZER_INTEGRITYCHECKER_H
+#define DAWN_OPTIMIZER_INTEGRITYCHECKER_H
+
+#include "dawn/IIR/ASTExpr.h"
+#include "dawn/IIR/ASTUtil.h"
+#include "dawn/IIR/StencilInstantiation.h"
+#include "dawn/Support/Exception.h"
+
+namespace dawn {
+
+//===------------------------------------------------------------------------------------------===//
+//     IntegrityChecker
+//===------------------------------------------------------------------------------------------===//
+/// @brief Perform basic integrity checks on the AST.
+class IntegrityChecker : public ast::ASTVisitorForwarding {
+  iir::StencilInstantiation* instantiation_;
+  iir::StencilMetaInformation& metadata_;
+
+public:
+  IntegrityChecker(iir::StencilInstantiation* instantiation);
+
+  void run();
+
+  void visit(const std::shared_ptr<iir::BlockStmt>& stmt) override;
+
+  void visit(const std::shared_ptr<iir::ExprStmt>& stmt) override;
+
+  void visit(const std::shared_ptr<iir::ReturnStmt>& stmt) override;
+
+  void visit(const std::shared_ptr<iir::IfStmt>& stmt) override;
+
+  void visit(const std::shared_ptr<iir::VarDeclStmt>& stmt) override;
+
+  void visit(const std::shared_ptr<iir::AssignmentExpr>& expr) override;
+
+  void visit(const std::shared_ptr<iir::UnaryOperator>& expr) override;
+
+  void visit(const std::shared_ptr<iir::ReductionOverNeighborExpr>& expr) override;
+
+  void visit(const std::shared_ptr<iir::BinaryOperator>& expr) override;
+
+  void visit(const std::shared_ptr<iir::TernaryOperator>& expr) override;
+
+  void visit(const std::shared_ptr<iir::FunCallExpr>& expr) override;
+
+private:
+  void iterate(iir::StencilInstantiation* instantiation);
+
+  void iterate(const std::unique_ptr<iir::Stencil>& stencil);
+
+  void iterate(const std::unique_ptr<iir::MultiStage>& multiStage);
+
+  void iterate(const std::unique_ptr<iir::Stage>& stage);
+
+  void iterate(const std::unique_ptr<iir::DoMethod>& doMethod);
+};
+
+} // namespace dawn
+
+#endif // DAWN_OPTIMIZER_INTEGRITYCHECKER_H
