436a1c7e115a32e6f78e44d3533e0f36104a1a30
Johann Dahm <johannd@vulcan.com>
2020-01-23 08:24:01 -0800

    Add iterator adaptors: reverse and enumerate

---
+++ dawn/src/dawn/Support/IteratorAdaptors.h
@@ -0,0 +1,61 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_SUPPORT_ITERATORADAPTORS_H
+#define DAWN_SUPPORT_ITERATORADAPTORS_H
+
+#include <tuple>
+
+template <typename T, typename TIter = decltype(std::begin(std::declval<T>())),
+          typename = decltype(std::end(std::declval<T>()))>
+constexpr auto enumerate(T&& iterable) {
+  struct iterator {
+    size_t i;
+    TIter iter;
+    bool operator!=(const iterator& other) const { return iter != other.iter; }
+    void operator++() {
+      ++i;
+      ++iter;
+    }
+    auto operator*() const { return std::tie(i, *iter); }
+  };
+  struct iterable_wrapper {
+    T iterable;
+    auto begin() { return iterator{0, std::begin(iterable)}; }
+    auto end() { return iterator{0, std::end(iterable)}; }
+  };
+  return iterable_wrapper{std::forward<T>(iterable)};
+}
+
+template <typename T>
+struct reversion_wrapper {
+  T& iterable;
+};
+
+template <typename T>
+auto begin(reversion_wrapper<T> w) {
+  return std::rbegin(w.iterable);
+}
+
+template <typename T>
+auto end(reversion_wrapper<T> w) {
+  return std::rend(w.iterable);
+}
+
+template <typename T>
+reversion_wrapper<T> reverse(T&& iterable) {
+  return {iterable};
+}
+
+#endif // DAWN_SUPPORT_ITERATORADAPTORS_H
