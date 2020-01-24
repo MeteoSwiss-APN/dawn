2a2abb8e4f73c83bfa3911f179a2919db6a1480e
Eddie Davis <eddied@vulcan.com>
2020-01-24 09:09:45 -0800

    Ensure at least 1 valid stencil in PreprocessingTest.Include

--- gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
+++ gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
@@ -29,17 +29,19 @@ namespace {
 TEST(PreprocessingTest, Include) {
   auto flags = UnittestEnvironment::getSingleton().getFlagManager().getDefaultFlags();
 
-  std::string filename = "test_stencil_w_include.cpp";
+  std::string filename = "input/test_stencil_w_include.cpp";
   dawn::UIDGenerator::getInstance()->reset();
   auto pair1 = GTClang::run({filename, "-fno-codegen"}, flags);
   ASSERT_TRUE(pair1.first);
   dawn::SIR* sir1 = pair1.second.get();
+  ASSERT_TRUE(sir1->Stencils.size() > 0);
 
-  filename = "test_stencil_no_include.cpp";
+  filename = "input/test_stencil_no_include.cpp";
   dawn::UIDGenerator::getInstance()->reset();
   auto pair2 = GTClang::run({filename, "-fno-codegen"}, flags);
   ASSERT_TRUE(pair2.first);
   dawn::SIR* sir2 = pair2.second.get();
+  ASSERT_TRUE(sir2->Stencils.size() > 0);
 
   sir2->Filename = sir1->Filename;
   std::string sirStr1 = dawn::SIRSerializer::serializeToString(sir1);
