3b1d6dffc2ff36f94e353982639ea8f791179da9
Johann Dahm <johannd@vulcan.com>
2020-01-24 14:26:42 -0800

    Add adapters

---
+++ dawn/test/unit-test/dawn/Support/TestIteratorAdapters.cpp
@@ -0,0 +1,64 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "dawn/Support/IteratorAdaptors.h"
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace dawn;
+
+namespace {
+
+TEST(TestIteratorAdaptor, reverse) {
+  const std::vector<int> vec{0, 1, 2};
+
+  int i = 0;
+  for(auto e : reverse(vec)) {
+    switch(i) {
+    case 0:
+      ASSERT_EQ(e, 2);
+      break;
+    case 1:
+      ASSERT_EQ(e, 1);
+      break;
+    case 2:
+      ASSERT_EQ(e, 0);
+      break;
+    }
+    ++i;
+  }
+}
+
+TEST(TestIteratorAdaptor, enumerate) {
+  const std::vector<int> vec{0, 1, 2};
+
+  for(auto [i, e] : enumerate(vec)) {
+    switch(i) {
+    case 0:
+      ASSERT_EQ(e, 0);
+      ASSERT_EQ(i, 0);
+      break;
+    case 1:
+      ASSERT_EQ(e, 1);
+      ASSERT_EQ(i, 1);
+      break;
+    case 2:
+      ASSERT_EQ(e, 2);
+      ASSERT_EQ(i, 2);
+      break;
+    }
+  }
+}
+
+} // anonymous namespace
