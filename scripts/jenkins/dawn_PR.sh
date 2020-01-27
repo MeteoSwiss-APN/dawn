c21a503de6854251a9c070bbed2def8e209585dd
Tobias Wicky <tobiasw@vulcan.com>
2020-01-27 16:09:22 +0100

    somewhere else

--- scripts/jenkins/dawn_PR.sh
+++ scripts/jenkins/dawn_PR.sh
@@ -20,7 +20,7 @@ repo_root=${BASEPATH_SCRIPT}/../..
 echo "Compiling on $(hostname)"
 
 base_dir=`pwd`
-workdir=//scratch/snx3000/jenkins/workspace/dawn_gtclang_clang-gridtools_PR/temp
+workdir=`pwd`/../temp
 rm -rf ${workdir}
 echo "Copying repository to ${workdir}"
 cp -r ${repo_root} ${workdir}
