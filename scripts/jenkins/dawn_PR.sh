163b148d32634d44e6a2e682ea439c306235af91
Tobias Wicky <tobiasw@vulcan.com>
2020-01-27 15:20:36 +0100

    reenable

--- scripts/jenkins/dawn_PR.sh
+++ scripts/jenkins/dawn_PR.sh
@@ -20,11 +20,11 @@ repo_root=${BASEPATH_SCRIPT}/../..
 echo "Compiling on $(hostname)"
 
 base_dir=`pwd`
-workdir=$base_dir
-# rm -rf ${workdir}
-# echo "Copying repository to ${workdir}"
-# cp -r ${repo_root} ${workdir}
-# echo "done"
+workdir=/dev/shm/tmp_dawn
+rm -rf ${workdir}
+echo "Copying repository to ${workdir}"
+cp -r ${repo_root} ${workdir}
+echo "done"
 
 cd ${workdir}
 
@@ -64,6 +64,6 @@ for t in $clang_gridtools_tests; do
   i=$((i+1))
 done
 
-# rm -rf ${workdir}
+rm -rf ${workdir}
 
 exit $ret
