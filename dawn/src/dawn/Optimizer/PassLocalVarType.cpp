eaf552244593f749907d4cce0fd3629423ddf133
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-02-04 15:10:06 +0100

    Change implementation and add tests

--- dawn/src/dawn/Optimizer/PassLocalVarType.cpp
+++ dawn/src/dawn/Optimizer/PassLocalVarType.cpp
@@ -33,105 +33,109 @@ void resetVarTypes(iir::StencilMetaInformation& metadata) {
 class VarTypeFinder : public ast::ASTVisitorForwarding {
   iir::StencilMetaInformation& metadata_;
 
-  // Currently determined variable type.
+  // AccessID of variable being processed.
   // Unset if visitor is not inside a `VarDeclStmt` or an `AssignmentExpr` to a local variable.
-  std::optional<iir::LocalVariableType> curVarType_;
+  std::optional<int> curVarID_;
+  // When assigning to or declaring variable A, each other variable that appears on the rhs is a
+  // dependence of A. This map is from a variable B to all variables that depend on B. A variable
+  // cannot depend on itself.
+  std::unordered_map<int, std::vector<int>> dependencyMap_;
 
-  void updateVariableType(int accessID, SourceLocation sourceLocation) {
+  void updateVariableType(iir::LocalVariableType newType, SourceLocation sourceLocation) {
+    DAWN_ASSERT(curVarID_.has_value());
 
     // Set the variable type (could still be changed when visiting another assignment)
-    iir::LocalVariableData& data = metadata_.getLocalVariableDataFromAccessID(accessID);
+    iir::LocalVariableData& data = metadata_.getLocalVariableDataFromAccessID(*curVarID_);
 
     if(data.isTypeSet()) {
       iir::LocalVariableType previouslyComputedType = data.getType();
 
       if(previouslyComputedType == iir::LocalVariableType::Scalar) {
-        data.setType(*curVarType_);
-      } else if(previouslyComputedType != *curVarType_) {
+        data.setType(newType);
+      } else if(previouslyComputedType != newType) {
 
         throw std::runtime_error(
             dawn::format("Invalid assignment to variable at line %d: rhs differs in location type",
                          sourceLocation.Line));
       }
     } else {
-      data.setType(*curVarType_);
+      data.setType(newType);
     }
-  }
-
-  void updateCurrentType(iir::LocalVariableType newType, SourceLocation sourceLocation) {
-    if(curVarType_.has_value() && *curVarType_ != iir::LocalVariableType::Scalar &&
-       *curVarType_ != newType) {
-
-      throw std::runtime_error(
-          dawn::format("Invalid assignment to variable at line %d: rhs differs in location type",
-                       sourceLocation.Line));
-    }
-    curVarType_ = newType;
   }
 
 public:
   void visit(const std::shared_ptr<iir::FieldAccessExpr>& expr) override {
-    if(curVarType_.has_value()) { // We are inside an assignment to variable / variable declaration
+    if(curVarID_.has_value()) { // We are inside an assignment to variable / variable declaration
       iir::LocalVariableType newType;
-      switch(metadata_.getDenseLocationTypeFromAccessID(iir::getAccessID(expr))) {
-      case ast::LocationType::Cells:
-        newType = iir::LocalVariableType::OnCells;
-        break;
-      case ast::LocationType::Edges:
-        newType = iir::LocalVariableType::OnEdges;
-        break;
-      case ast::LocationType::Vertices:
-        newType = iir::LocalVariableType::OnVertices;
-        break;
+
+      if(sir::dimension_isa<sir::CartesianFieldDimension>(
+             metadata_.getFieldDimensions(iir::getAccessID(expr)).getHorizontalFieldDimension())) {
+        // Cartesian case
+        newType = iir::LocalVariableType::OnIJ;
+      } else {
+        // Unstructured case
+        switch(metadata_.getDenseLocationTypeFromAccessID(iir::getAccessID(expr))) {
+        case ast::LocationType::Cells:
+          newType = iir::LocalVariableType::OnCells;
+          break;
+        case ast::LocationType::Edges:
+          newType = iir::LocalVariableType::OnEdges;
+          break;
+        case ast::LocationType::Vertices:
+          newType = iir::LocalVariableType::OnVertices;
+          break;
+        }
       }
 
-      updateCurrentType(newType, expr->getSourceLocation());
+      updateVariableType(newType, expr->getSourceLocation());
     }
   }
   void visit(const std::shared_ptr<iir::VarAccessExpr>& expr) override {
-    if(curVarType_.has_value()) { // We are inside an assignment / variable declaration
+    if(curVarID_.has_value()) { // We are inside an assignment / variable declaration
+
+      // Need to map dependency between lhs variable and accessed variable
+      // TODO...
+
+      // Retrieve the type of the accessed variable
       const iir::LocalVariableData& data =
           metadata_.getLocalVariableDataFromAccessID(iir::getAccessID(expr));
       DAWN_ASSERT_MSG(data.isTypeSet(), "Variable accessed before being declared.");
       iir::LocalVariableType newType = data.getType();
 
-      updateCurrentType(newType, expr->getSourceLocation());
+      updateVariableType(newType, expr->getSourceLocation());
     }
   }
   void visit(const std::shared_ptr<iir::AssignmentExpr>& expr) override {
     if(expr->getLeft()->getKind() == ast::Expr::Kind::VarAccessExpr) {
 
-      if(curVarType_
-             .has_value()) { // Variable assignment inside variable assignment, not supported.
+      if(curVarID_.has_value()) { // Variable assignment inside variable assignment, not supported.
         throw std::runtime_error(dawn::format(
             "Variable assignment inside rhs of variable assignment is not supported. Line %d.",
             expr->getSourceLocation().Line));
       }
 
       // Assume scalar if nothing proves the opposite during the visit
-      curVarType_ = iir::LocalVariableType::Scalar;
+      curVarID_ = iir::getAccessID(expr->getLeft());
+      updateVariableType(iir::LocalVariableType::Scalar, expr->getSourceLocation());
 
       // Visit rhs
       expr->getRight()->accept(*this);
 
-      updateVariableType(iir::getAccessID(expr->getLeft()), expr->getSourceLocation());
-
-      // Unset varType_ as we are exiting the AssignmentExpr's visit
-      curVarType_ = std::nullopt;
+      // Unset curVarID_ as we are exiting the AssignmentExpr's visit
+      curVarID_ = std::nullopt;
     }
   }
   void visit(const std::shared_ptr<iir::VarDeclStmt>& stmt) override {
 
     // Assume scalar if nothing proves the opposite during the visit
-    curVarType_ = iir::LocalVariableType::Scalar;
+    curVarID_ = iir::getAccessID(stmt);
+    updateVariableType(iir::LocalVariableType::Scalar, stmt->getSourceLocation());
 
     // Visit rhs
     ast::ASTVisitorForwarding::visit(stmt);
 
-    updateVariableType(iir::getAccessID(stmt), stmt->getSourceLocation());
-
-    // Unset varType_ as we are exiting the VarDeclStmt's visit
-    curVarType_ = std::nullopt;
+    // Unset curVarID_ as we are exiting the VarDeclStmt's visit
+    curVarID_ = std::nullopt;
   }
   VarTypeFinder(iir::StencilMetaInformation& metadata) : metadata_(metadata) {}
 };
--- dawn/test/unit-test/dawn/Optimizer/Passes/TestPassLocalVarType.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestPassLocalVarType.cpp
@@ -51,9 +51,94 @@ TEST(TestLocalVarType, test_cartesian_01) {
   PassLocalVarType passLocalVarType(optimizer);
   passLocalVarType.run(stencil);
 
-  // Need to check that varA has been flagged as scalar
   int varAID = stencil->getMetaData().getAccessIDFromName("varA");
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).isTypeSet());
+  // Need to check that varA has been flagged as scalar
   ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).isScalar());
+}
+
+TEST(TestLocalVarType, test_cartesian_02) {
+  using namespace dawn::iir;
+
+  CartesianIIRBuilder b;
+  auto fIJ = b.field("f_ij", FieldType::ij);
+  auto varA = b.localvar("varA", dawn::BuiltinTypeID::Double, {b.lit(3.0)});
+  auto varB = b.localvar("varB", dawn::BuiltinTypeID::Double, {b.at(varA)});
+
+  /// storage_ij f_ij;
+  /// double varA = 3.0;
+  /// double varB = varA;
+  /// f_ij = varB;
+
+  auto stencilInstantiationContext = b.build(
+      "generated", b.stencil(b.multistage(
+                       dawn::iir::LoopOrderKind::Forward,
+                       b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                                          b.declareVar(varA), b.declareVar(varB),
+                                          b.stmt(b.assignExpr(b.at(fIJ), b.at(varB))))))));
+  auto stencil = stencilInstantiationContext.at("generated");
+
+  OptimizerContext::OptimizerContextOptions optimizerOptions;
+
+  DawnCompiler compiler;
+  OptimizerContext optimizer(compiler.getDiagnostics(), optimizerOptions,
+                             std::make_shared<dawn::SIR>(ast::GridType::Cartesian));
+
+  // run single pass (PassLocalVarType)
+  PassLocalVarType passLocalVarType(optimizer);
+  passLocalVarType.run(stencil);
+
+  int varAID = stencil->getMetaData().getAccessIDFromName("varA");
+  int varBID = stencil->getMetaData().getAccessIDFromName("varB");
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).isTypeSet());
+  // Need to check that varA has been flagged as scalar
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).isScalar());
+  // Need to check that varB has been flagged as scalar
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varBID).isScalar());
+}
+
+TEST(TestLocalVarType, test_cartesian_03) {
+  using namespace dawn::iir;
+
+  CartesianIIRBuilder b;
+  auto fIJ = b.field("f_ij", FieldType::ij);
+  auto varA = b.localvar("varA", dawn::BuiltinTypeID::Double, {b.lit(3.0)});
+  auto varB = b.localvar("varB", dawn::BuiltinTypeID::Double, {b.at(varA)});
+
+  /// storage_ij f_ij;
+  /// double varA = 3.0;
+  /// double varB = varA;
+  /// varA = f_ij;
+  /// f_ij = varB;
+
+  auto stencilInstantiationContext = b.build(
+      "generated", b.stencil(b.multistage(
+                       dawn::iir::LoopOrderKind::Forward,
+                       b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::End,
+                                          b.declareVar(varA), b.declareVar(varB),
+                                          b.stmt(b.assignExpr(b.at(varA), b.at(fIJ))),
+                                          b.stmt(b.assignExpr(b.at(fIJ), b.at(varB))))))));
+  auto stencil = stencilInstantiationContext.at("generated");
+
+  OptimizerContext::OptimizerContextOptions optimizerOptions;
+
+  DawnCompiler compiler;
+  OptimizerContext optimizer(compiler.getDiagnostics(), optimizerOptions,
+                             std::make_shared<dawn::SIR>(ast::GridType::Cartesian));
+
+  // run single pass (PassLocalVarType)
+  PassLocalVarType passLocalVarType(optimizer);
+  passLocalVarType.run(stencil);
+
+  int varAID = stencil->getMetaData().getAccessIDFromName("varA");
+  int varBID = stencil->getMetaData().getAccessIDFromName("varB");
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).isTypeSet());
+  // Need to check that varA has been flagged as IJ
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varAID).getType() ==
+              iir::LocalVariableType::OnIJ);
+  // Need to check that varB has been flagged as IJ
+  ASSERT_TRUE(stencil->getMetaData().getLocalVariableDataFromAccessID(varBID).getType() ==
+              iir::LocalVariableType::OnIJ);
 }
 
 } // namespace
