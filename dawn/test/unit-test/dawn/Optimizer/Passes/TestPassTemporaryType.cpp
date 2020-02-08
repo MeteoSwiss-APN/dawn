ac1a92b68d0677570d2fc0a6925e1e4179852a99
Eddie Davis <eddied@vulcan.com>
2020-02-07 17:20:03 -0800

    Update TestPassTemporaryType::runTest to use AST matcher

--- dawn/test/unit-test/dawn/Optimizer/Passes/TestPassTemporaryType.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestPassTemporaryType.cpp
@@ -34,9 +34,7 @@ protected:
   dawn::OptimizerContext::OptimizerContextOptions options_;
   std::unique_ptr<OptimizerContext> context_;
 
-  //virtual void SetUp() { options_.MergeTemporaries = true; }
-
-  void runTest(const std::string& filename, const std::vector<std::string>& mergedFields) {
+  void runTest(const std::string& filename, const std::vector<std::string>& demotedFields) {
     dawn::UIDGenerator::getInstance()->reset();
     std::shared_ptr<iir::StencilInstantiation> instantiation =
         CompilerUtil::load(filename, options_, context_, TestEnvironment::path_);
@@ -44,14 +42,34 @@ protected:
     // Run prerequisite tests
     ASSERT_TRUE(CompilerUtil::runPasses(5, context_, instantiation));
 
+    //CompilerUtil::write(context_, 0, 1, "DemoteTest01");
+
     // Expect pass to succeed...
     ASSERT_TRUE(CompilerUtil::runPass<dawn::PassTemporaryType>(context_, instantiation));
 
-    int stop = 1;
+    //CompilerUtil::write(context_, 1, 1, "DemoteTest01");
+
+    if(demotedFields.size() > 0) {
+      // Apply AST matcher to find all field access expressions
+      dawn::ASTMatcher matcher(instantiation.get());
+      std::vector<std::shared_ptr<ast::Expr>>& accessExprs =
+          matcher.match(ast::Expr::Kind::FieldAccessExpr);
+
+      std::unordered_set<std::string> fieldNames;
+      for(const auto& access : accessExprs) {
+        const auto& field = std::dynamic_pointer_cast<ast::FieldAccessExpr>(access);
+        fieldNames.insert(field->getName());
+      }
+
+      // Assert that merged fields are no longer accessed
+      for(const auto& demotedField : demotedFields) {
+        ASSERT_TRUE(fieldNames.find(demotedField) == fieldNames.end());
+      }
+    }
   }
 };
 
-TEST_F(TestPassTemporaryType, DemoteTest1) { runTest("DemoteTest01.sir", {}); }
+TEST_F(TestPassTemporaryType, DemoteTest1) { runTest("DemoteTest01.sir", {"tmp"}); }
 
 //TEST_F(TestPassTemporaryType, PromoteTest1) { runTest("PromoteTest01.sir", {}); }
 //
