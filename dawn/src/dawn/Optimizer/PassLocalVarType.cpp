beeb7480c561e135df127ba916c928e442badcc2
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-02-12 11:43:39 +0100

    Address review

--- dawn/src/dawn/Optimizer/PassLocalVarType.cpp
+++ dawn/src/dawn/Optimizer/PassLocalVarType.cpp
@@ -72,30 +72,25 @@ class VarTypeFinder : public ast::ASTVisitorForwarding {
     // Define a recursive lambda function to propagate the type through the dependency map
     std::function<void(int)> recursivePropagate;
     recursivePropagate = [&](int dependee) {
-      // Flag the variable as visited
       visitedSet.insert(dependee);
-      // If no other variable depends on the dependee, return
+      // If no other variable depends on the dependee, no propagation.
       if(!dependencyMap_.count(dependee)) {
         return;
       }
       // Collect the type to propagate
       const iir::LocalVariableData& dependeeData =
           metadata_.getLocalVariableDataFromAccessID(dependee);
-      // If the dependee's type is not set yet, it has been accessed before being declared.
       DAWN_ASSERT_MSG(dependeeData.isTypeSet(), "Variable accessed before being declared.");
-      // Loop through variables (dependers) which depend on the dependee
       for(int depender : dependencyMap_.at(dependee)) {
-        // Make sure depender hasn't be already visited within the current call to
+        // Make sure depender hasn't been already visited within the current call to
         // `propagateVariableType()`
         if(!visitedSet.count(depender)) {
-          // Update the type of depender with the dependee's type
           updateVariableType(depender, dependeeData.getType(), sourceLocation);
-          // Call the recursive procedure on depender
           recursivePropagate(depender);
         }
       }
     };
-    // Call the recursive function
+
     recursivePropagate(accessID);
   }
 
@@ -113,18 +108,15 @@ class VarTypeFinder : public ast::ASTVisitorForwarding {
   // Construct a LocalVariableType from a field accessed within an assignment to / declaration of a
   // local variable.
   iir::LocalVariableType inferLocalVarTypeFromField(int fieldAccessID) {
-    iir::LocalVariableType type;
-
     if(sir::dimension_isa<sir::CartesianFieldDimension>(
            metadata_.getFieldDimensions(fieldAccessID).getHorizontalFieldDimension())) {
       // Cartesian case
-      type = iir::LocalVariableType::OnIJ;
+      return iir::LocalVariableType::OnIJ;
     } else {
       // Unstructured case, convert from location type
-      type =
-          localVarTypeFromLocationType(metadata_.getDenseLocationTypeFromAccessID(fieldAccessID));
+      return localVarTypeFromLocationType(
+          metadata_.getDenseLocationTypeFromAccessID(fieldAccessID));
     }
-    return type;
   }
   // Records that to write into `destinationVariable` we need to access `accessedVariable`.
   // `sourceLocation` is where this happens.
@@ -133,12 +125,9 @@ class VarTypeFinder : public ast::ASTVisitorForwarding {
     // No self-dependencies
     if(accessedVariable != destinationVariable) {
 
-      // If the set of dependers for this variable is not there, it means the variable hasn't been
-      // declared
       DAWN_ASSERT_MSG(dependencyMap_.count(accessedVariable),
                       "Variable accessed before being declared.");
-      // Need to register a dependency. Pair to be added is from dependency (variable accessed in
-      // the rhs) to dependent (lhs).
+
       dependencyMap_.at(accessedVariable).insert(destinationVariable);
 
       // Retrieve the type of the accessed variable
@@ -147,23 +136,29 @@ class VarTypeFinder : public ast::ASTVisitorForwarding {
       DAWN_ASSERT_MSG(data.isTypeSet(), "Variable accessed before being declared.");
       iir::LocalVariableType newType = data.getType();
 
-      // Update the type of the current variable with `newType`
       updateVariableType(destinationVariable, newType, sourceLocation);
     }
+  }
+
+  void initializeLocalVarType(SourceLocation sourceLocation) {
+    // If we are inside an if-statement and the if condition accesses a field, we already have a
+    // variable type inferred. Otherwise, assume scalar as starting type.
+    const iir::LocalVariableType startingType =
+        conditionalType_.has_value() ? *conditionalType_ : iir::LocalVariableType::Scalar;
+    updateVariableType(*curVarID_, startingType, sourceLocation);
   }
 
 public:
   void visit(const std::shared_ptr<iir::FieldAccessExpr>& expr) override {
     if(curVarID_.has_value()) { // We are inside an assignment to variable / variable declaration
       iir::LocalVariableType newType = inferLocalVarTypeFromField(iir::getAccessID(expr));
-      // Update the type of the current variable with `newType`
       updateVariableType(*curVarID_, newType, expr->getSourceLocation());
     }
   }
   void visit(const std::shared_ptr<iir::VarAccessExpr>& expr) override {
-    // We are inside an assignment / variable declaration and the current variable accessed is not a
-    // global variable
-    if(curVarID_.has_value() && expr->isLocal()) {
+    if(curVarID_.has_value() && expr->isLocal()) { // We are inside an assignment / variable
+                                                   // declaration and the current variable accessed
+                                                   // is not a global variable
       int accessedVariableID = iir::getAccessID(expr);
       recordVariablePair(accessedVariableID, *curVarID_, expr->getSourceLocation());
     }
@@ -171,10 +166,9 @@ public:
   void visit(const std::shared_ptr<iir::IfStmt>& ifStmt) override {
     // If in the conditional expression we are accessing a field, it means that every statement in
     // the then and else blocks will have a read-dependency on such field.
-
     for(const auto& readAccess :
         ifStmt->getCondStmt()->getData<iir::IIRStmtData>().CallerAccesses->getReadAccesses()) {
-      int accessID = readAccess.first;
+      const int accessID = readAccess.first;
       if(metadata_.isAccessType(iir::FieldAccessType::Field, accessID)) {
         if(conditionalType_.has_value()) {
           if(*conditionalType_ != inferLocalVarTypeFromField(accessID)) {
@@ -192,7 +186,7 @@ public:
     // If we encounter an assignment to a local variable inside the then and else blocks, we should
     // impose the recorded type.
     ast::ASTVisitorForwarding::visit(ifStmt);
-    // Unset conditionalType_ as we are exiting the visit of the if statement
+    // Reset as we are exiting the visit of the if statement
     conditionalType_ = std::nullopt;
     variablesAccessedInConditional_ = {};
   }
@@ -202,21 +196,13 @@ public:
        std::dynamic_pointer_cast<iir::VarAccessExpr>(expr->getLeft())->isLocal()) {
 
       if(curVarID_.has_value()) {
-        // Variable assignment inside variable assignment, not supported.
         throw std::runtime_error(dawn::format("Variable assignment inside rhs of variable "
                                               "assignment is not supported. Line %d.",
                                               expr->getSourceLocation().Line));
       }
       // Set curVarID_ to current variable being processed
       curVarID_ = iir::getAccessID(expr->getLeft());
-      // If we are inside an if-statement and the if condition accesses a field, we already have a
-      // variable type inferred. Otherwise, assume scalar as starting type. In any case we need
-      // to do the visit.
-      const iir::LocalVariableType startingType =
-          conditionalType_.has_value() ? *conditionalType_ : iir::LocalVariableType::Scalar;
-      updateVariableType(*curVarID_, startingType, expr->getSourceLocation());
-      // If the set of dependers for this variable is not there, it means the variable hasn't been
-      // declared
+      initializeLocalVarType(expr->getSourceLocation());
       DAWN_ASSERT_MSG(dependencyMap_.count(*curVarID_), "Variable accessed before being declared.");
       // If we are inside an if statement, for each variable accessed in the conditional of the if,
       // we need to record its access as if it happened in the rhs of `expr`.
@@ -225,15 +211,14 @@ public:
       }
       // Visit rhs
       expr->getRight()->accept(*this);
-      // Propagate the type information to variables that depend on this one
+
       propagateVariableType(*curVarID_, expr->getSourceLocation());
-      // Unset curVarID_ as we are exiting the AssignmentExpr's visit
+      // Reset as we are exiting the AssignmentExpr's visit
       curVarID_ = std::nullopt;
     }
   }
   void visit(const std::shared_ptr<iir::ReductionOverNeighborExpr>& expr) override {
-    // We are inside an assignment to variable / variable declaration
-    if(curVarID_.has_value()) {
+    if(curVarID_.has_value()) { // We are inside an assignment to variable / variable declaration
       // Inferred variable type is the lhs location type of the reduction
       updateVariableType(*curVarID_, localVarTypeFromLocationType(expr->getLhsLocation()),
                          expr->getSourceLocation());
@@ -243,13 +228,8 @@ public:
   void visit(const std::shared_ptr<iir::VarDeclStmt>& stmt) override {
     // Set curVarID_ to current variable being processed
     curVarID_ = iir::getAccessID(stmt);
-    // If we are inside an if-statement and the if condition accesses a field, we already have a
-    // variable type inferred. Otherwise, assume scalar as starting type. In any case we need
-    // to do the visit.
-    const iir::LocalVariableType startingType =
-        conditionalType_.has_value() ? *conditionalType_ : iir::LocalVariableType::Scalar;
-    updateVariableType(*curVarID_, startingType, stmt->getSourceLocation());
-    // Setup empty set of dependers for this variable
+    initializeLocalVarType(stmt->getSourceLocation());
+    // Setup empty set of dependers for this variable (it's the first time we encounter it)
     dependencyMap_.emplace(*curVarID_, std::set<int>{});
     // If we are inside an if statement, for each variable accessed in the conditional of the if,
     // we need to record its access as if it happened in the rhs of `stmt`.
@@ -258,7 +238,7 @@ public:
     }
     // Visit rhs
     ast::ASTVisitorForwarding::visit(stmt);
-    // Unset curVarID_ as we are exiting the VarDeclStmt's visit
+    // Reset as we are exiting the VarDeclStmt's visit
     curVarID_ = std::nullopt;
   }
   VarTypeFinder(iir::StencilMetaInformation& metadata) : metadata_(metadata) {}
@@ -266,11 +246,10 @@ public:
 } // namespace
 
 bool PassLocalVarType::run(const std::shared_ptr<iir::StencilInstantiation>& stencilInstantiation) {
-  // Loop over all the statements of each stencil
   for(const auto& stencilPtr : stencilInstantiation->getStencils()) {
 
     // As this might not be the first run of this pass, need to clear the variables' types in order
-    // to recompute them
+    // to recompute them.
     stencilInstantiation->getMetaData().resetLocalVarTypes();
 
     for(const auto& doMethod : iterateIIROver<iir::DoMethod>(*stencilPtr)) {
