9a33af7fad6fd2a85891c85f9b74f1ca97dec040
Johann Dahm <johannd@vulcan.com>
2020-02-20 10:58:40 -0800

    Cleanup PreprocessingTest.Include

--- gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
+++ gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
@@ -29,24 +29,28 @@ namespace {
 TEST(PreprocessingTest, Include) {
   auto flags = UnittestEnvironment::getSingleton().getFlagManager().getDefaultFlags();
 
-  std::string filename = "input/test_stencil_w_include.cpp";
-  dawn::UIDGenerator::getInstance()->reset();
-  auto pair1 = GTClang::run({filename, "-fno-codegen"}, flags);
-  ASSERT_TRUE(pair1.first);
-  dawn::SIR* sir1 = pair1.second.get();
-  ASSERT_TRUE(sir1->Stencils.size() > 0);
+  std::string sirString1, sirString2;
+  {
+    const std::string filename = "input/test_stencil_w_include.cpp";
+    dawn::UIDGenerator::getInstance()->reset();
+    auto [passed, sir] = GTClang::run({filename, "-fno-codegen"}, flags);
+    ASSERT_TRUE(passed);
+    ASSERT_TRUE(!sir->Stencils.empty());
+    sir->Filename = "";
+    sirString1 = dawn::SIRSerializer::serializeToString(sir);
+  }
 
-  filename = "input/test_stencil_no_include.cpp";
-  dawn::UIDGenerator::getInstance()->reset();
-  auto pair2 = GTClang::run({filename, "-fno-codegen"}, flags);
-  ASSERT_TRUE(pair2.first);
-  dawn::SIR* sir2 = pair2.second.get();
-  ASSERT_TRUE(sir2->Stencils.size() > 0);
+  {
+    const std::string filename = "input/test_stencil_no_include.cpp";
+    dawn::UIDGenerator::getInstance()->reset();
+    auto [passed, sir] = GTClang::run({filename, "-fno-codegen"}, flags);
+    ASSERT_TRUE(passed);
+    ASSERT_TRUE(!sir->Stencils.empty());
+    sir->Filename = "";
+    sirString2 = dawn::SIRSerializer::serializeToString(sir);
+  }
 
-  sir2->Filename = sir1->Filename;
-  std::string sirStr1 = dawn::SIRSerializer::serializeToString(sir1);
-  std::string sirStr2 = dawn::SIRSerializer::serializeToString(sir2);
-  ASSERT_EQ(sirStr1, sirStr2);
+  ASSERT_EQ(sirString1, sirString2);
 }
 
 } // anonymous namespace
