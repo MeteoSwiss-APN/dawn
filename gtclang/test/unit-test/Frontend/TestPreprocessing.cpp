b37e3a886b7e01d4d7939f404e9015754565d017
Johann Dahm <johannd@vulcan.com>
2020-02-20 20:46:11 -0800

    Fix iteration_space parse and add test

--- gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
+++ gtclang/test/unit-test/Frontend/TestPreprocessing.cpp
@@ -14,7 +14,10 @@
 //
 //===------------------------------------------------------------------------------------------===//
 
+// #include "dawn/AST/ASTVisitor.h"
+#include "dawn/SIR/SIR.h"
 #include "dawn/Serialization/SIRSerializer.h"
+#include "dawn/Support/Casting.h"
 #include "dawn/Support/UIDGenerator.h"
 #include "gtclang/Unittest/GTClang.h"
 #include "gtclang/Unittest/UnittestEnvironment.h"
@@ -37,7 +40,7 @@ TEST(PreprocessingTest, Include) {
     ASSERT_TRUE(passed);
     ASSERT_TRUE(!sir->Stencils.empty());
     sir->Filename = "";
-    sirString1 = dawn::SIRSerializer::serializeToString(sir);
+    sirString1 = dawn::SIRSerializer::serializeToString(sir.get());
   }
 
   {
@@ -47,10 +50,68 @@ TEST(PreprocessingTest, Include) {
     ASSERT_TRUE(passed);
     ASSERT_TRUE(!sir->Stencils.empty());
     sir->Filename = "";
-    sirString2 = dawn::SIRSerializer::serializeToString(sir);
+    sirString2 = dawn::SIRSerializer::serializeToString(sir.get());
   }
 
   ASSERT_EQ(sirString1, sirString2);
+}
+
+using LevelKind = dawn::sir::Interval::LevelKind;
+using Interval = dawn::sir::Interval;
+
+void checkVerticalRegion(const dawn::sir::VerticalRegion& vr, const Interval& VerticalInterval,
+                         const std::optional<Interval>& Interval0,
+                         const std::optional<Interval>& Interval1) {
+  ASSERT_EQ(*vr.VerticalInterval, VerticalInterval);
+  ASSERT_EQ(vr.IterationSpace[0], Interval0);
+  ASSERT_EQ(vr.IterationSpace[1], Interval1);
+}
+
+TEST(PreprocessingTest, IterationSpaceToken) {
+  auto flags = UnittestEnvironment::getSingleton().getFlagManager().getDefaultFlags();
+  const std::string filename = "input/test_iteration_space.cpp";
+  dawn::UIDGenerator::getInstance()->reset();
+  auto [passed, sir] = GTClang::run({filename, "-fno-codegen", "-backend=c++-naive"}, flags);
+  ASSERT_TRUE(!sir->Stencils.empty());
+
+  const auto& stencil = sir->Stencils[0];
+  const auto& statements = stencil->StencilDescAst->getRoot()->getStatements();
+
+  {
+    // iteration_space(i_start, i_end - 1)
+    dawn::ast::VerticalRegionDeclStmt* vrds =
+        dawn::dyn_cast<dawn::sir::VerticalRegionDeclStmt>(statements[0].get());
+    ASSERT_NE(vrds, nullptr);
+    checkVerticalRegion(*vrds->getVerticalRegion(), Interval(LevelKind::Start, LevelKind::End),
+                        std::optional<Interval>(Interval(LevelKind::Start, LevelKind::End, 0, -1)),
+                        std::optional<Interval>());
+  }
+
+  std::cout << "2" << std::endl;
+
+  {
+    // iteration_space(j_start, j_end - 2)
+    dawn::ast::VerticalRegionDeclStmt* vrds =
+        dawn::dyn_cast<dawn::sir::VerticalRegionDeclStmt>(statements[1].get());
+    ASSERT_NE(vrds, nullptr);
+    checkVerticalRegion(*vrds->getVerticalRegion(), Interval(LevelKind::Start, LevelKind::End),
+                        std::optional<Interval>(),
+                        std::optional<Interval>(Interval(LevelKind::Start, LevelKind::End, 0, -2)));
+  }
+
+  std::cout << "3" << std::endl;
+
+  {
+    // iteration_space(i_start, i_start + 1, j_start, j_start + 1, k_start,
+    // k_end - 1)
+    dawn::ast::VerticalRegionDeclStmt* vrds =
+        dawn::dyn_cast<dawn::sir::VerticalRegionDeclStmt>(statements[2].get());
+    ASSERT_NE(vrds, nullptr);
+    checkVerticalRegion(
+        *vrds->getVerticalRegion(), Interval(LevelKind::Start, LevelKind::End, 0, -1),
+        std::optional<Interval>(Interval(LevelKind::Start, LevelKind::Start, 0, 1)),
+        std::optional<Interval>(Interval(LevelKind::Start, LevelKind::Start, 0, 1)));
+  }
 }
 
 } // anonymous namespace
