49a383f21419e53bee81bd35d9778a0f2d63fd09
Tobias Wicky <tobiasw@vulcan.com>
2020-02-21 12:33:56 -0800

    move files

---
+++ dawn/test/integration-test/dawnOpt/TestsFromSIR/samples/test_compute_read_access_interval.cpp
@@ -0,0 +1,40 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "gtclang_dsl_defs/gtclang_dsl.hpp"
+
+using namespace gtclang::dsl;
+
+stencil stencil {
+  storage b, a;
+  var tmp;
+
+  Do {
+    vertical_region(k_start + 2, k_end - 1) { tmp = tmp[k - 2]; }
+    vertical_region(k_end, k_end) { tmp = tmp[k - 1] + a; }
+    /*
+        vertical_region(k_start, k_start) {
+          b = tmp[i-2];
+        }
+        vertical_region(k_start+1, k_end-4) {
+          b = tmp[k-1,i+1];
+        }
+        vertical_region(k_end-3, k_end) {
+          b = tmp[k-1,i+1]+a;
+        }
+    */
+  }
+};
---
+++ dawn/test/integration-test/dawnOpt/TestsFromSIR/samples/test_compute_read_access_interval_04.cpp
@@ -0,0 +1,49 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "gtclang_dsl_defs/gtclang_dsl.hpp"
+
+using namespace gtclang::dsl;
+
+stencil stencil {
+  storage in, out;
+  storage a1, a2, b1, b2, c1, c2;
+  var tmp;
+
+  Do {
+    vertical_region(k_start, k_end) {
+      // --- MS0 ---
+      tmp = in;
+
+      b1 = a1;
+      // --- MS1 ---
+      c1 = b1(k + 1);
+      c1 = b1(k - 1);
+
+      out = tmp;
+      tmp = in;
+
+      b2 = a2;
+      // --- MS2 ---
+      c2 = b2(k + 1);
+      c2 = b2(k - 1);
+
+      out = tmp;
+    }
+  }
+};
+
+int main() {}
