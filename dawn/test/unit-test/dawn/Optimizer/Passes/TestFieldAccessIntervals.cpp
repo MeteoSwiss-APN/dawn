7f6d811a62b1b35c924867eceb2012ef4aa72e9b
Tobias Wicky <tobiasw@vulcan.com>
2020-02-21 12:06:30 -0800

    change field access interval tests

--- dawn/test/unit-test/dawn/Optimizer/Passes/TestFieldAccessIntervals.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestFieldAccessIntervals.cpp
@@ -12,158 +12,62 @@
 //
 //===------------------------------------------------------------------------------------------===//
 
-#include "dawn/Compiler/DawnCompiler.h"
-#include "dawn/Compiler/Options.h"
+#include "dawn/IIR/IIR.h"
 #include "dawn/IIR/StencilInstantiation.h"
-#include "dawn/SIR/SIR.h"
 #include "dawn/Serialization/IIRSerializer.h"
-#include "dawn/Serialization/SIRSerializer.h"
 #include "test/unit-test/dawn/Optimizer/TestEnvironment.h"
 #include <fstream>
 #include <gtest/gtest.h>
-#include <optional>
-#include <streambuf>
 
 using namespace dawn;
 
 namespace {
 
 TEST(TestFieldAccessIntervals, test_field_access_interval_01) {
+  /*
+    interval k_flat = k_start + 12;
+    vertical_region(k_start, k_flat) { in = 12; }
+  */
   auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_01.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
-  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
-  EXPECT_EQ(stencil->getNumStages(), 2);
-  EXPECT_EQ(stencil->getStage(0)->getExtents(), (iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0)));
-  EXPECT_EQ(stencil->getStage(1)->getExtents(), (iir::Extents(ast::cartesian, 0, 0, 0, 0, 0, 0)));
-
-  for(auto fieldPair : (*stencil->childrenBegin())->getFields()) {
-    iir::Field& field = fieldPair.second;
-    int AccessID = fieldPair.first;
-
-    if(AccessID == metadata.getAccessIDFromName("lap")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 11, 0}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("out") ||
-       AccessID == metadata.getAccessIDFromName("u")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 0, 0}));
-    }
-  }
+  int inID = metadata.getAccessIDFromName("in");
+  auto fieldToIDMap = stencils[0]->getFields();
+  auto field = fieldToIDMap.at(inID).field;
+  EXPECT_EQ(field.getInterval(),
+            (iir::Interval{sir::Interval::Start, sir::Interval::Start, 0, 12}));
 }
 
 TEST(TestFieldAccessIntervals, test_field_access_interval_02) {
+  /*
+    vertical_region(k_start, k_start) { in = 12; }
+    vertical_region(k_end, k_end) { in = 10; }
+  */
   auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_02.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
-  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
-  EXPECT_EQ(stencil->getNumStages(), 2);
-  EXPECT_EQ(stencil->getStage(0)->getExtents(),
-            ((iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0))));
-  EXPECT_EQ(stencil->getStage(1)->getExtents(), ((iir::Extents(ast::cartesian, 0, 0, 0, 0, 0, 0))));
-
-  for(auto fieldPair : (*stencil->childrenBegin())->getFields()) {
-    iir::Field& field = fieldPair.second;
-    int AccessID = fieldPair.first;
-    if(AccessID == metadata.getAccessIDFromName("lap")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start + 11, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("out") ||
-       AccessID == metadata.getAccessIDFromName("u")) {
-      EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("coeff")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 11, 0}));
-      EXPECT_EQ(field.computeAccessedInterval(), (iir::Interval{12, sir::Interval::End + 1}));
-    }
-  }
+  int inID = metadata.getAccessIDFromName("in");
+  auto fieldToIDMap = stencils[0]->getFields();
+  auto field = fieldToIDMap.at(inID).field;
+  EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::End, 0, 0}));
 }
-
 TEST(TestFieldAccessIntervals, test_field_access_interval_03) {
+  /*
+    vertical_region(k_start, k_start + 2) { in = 12; }
+  */
   auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_03.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
-  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
-  EXPECT_EQ(stencil->getNumStages(), 3);
-  EXPECT_EQ(stencil->getStage(0)->getExtents(), (iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0)));
-  EXPECT_EQ(stencil->getStage(1)->getExtents(), (iir::Extents(ast::cartesian, 0, 0, 0, 0, 0, 0)));
-  EXPECT_EQ(stencil->getStage(2)->getExtents(), (iir::Extents(ast::cartesian, 0, 0, 0, 0, 0, 0)));
-
-  for(auto fieldPair : (*stencil->childrenBegin())->getFields()) {
-    iir::Field& field = fieldPair.second;
-    int AccessID = fieldPair.first;
-    if(AccessID == metadata.getAccessIDFromName("lap")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start + 11, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("out") ||
-       AccessID == metadata.getAccessIDFromName("u")) {
-      EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("coeff")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 4, 0}));
-      EXPECT_EQ(field.computeAccessedInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 2, 1}));
-    }
-  }
-}
-
-TEST(TestFieldAccessIntervals, test_field_access_interval_04) {
-  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_04.iir");
-  const auto& stencils = stencilInstantiation->getStencils();
-  EXPECT_EQ(stencils.size(), 1);
-  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
-
-  EXPECT_EQ(stencil->getNumStages(), 3);
-
-  iir::MultiStage& multiStage = **(stencil->childrenBegin());
-
-  std::optional<iir::Interval> enclosingInterval =
-      multiStage.getEnclosingAccessIntervalTemporaries();
-  ASSERT_TRUE(enclosingInterval.has_value());
-  EXPECT_EQ((*enclosingInterval), (iir::Interval{2, 14}));
-}
-
-TEST(TestFieldAccessIntervals, test_field_access_interval_05) {
-  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_05.iir");
-  const auto& metadata = stencilInstantiation->getMetaData();
-  const auto& stencils = stencilInstantiation->getStencils();
-  EXPECT_EQ(stencils.size(), 1);
-  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
-
-  EXPECT_EQ(stencil->getNumStages(), 2);
-  EXPECT_EQ(stencil->getStage(0)->getExtents(), (iir::Extents(ast::cartesian, -1, 1, -1, 1, 0, 0)));
-  EXPECT_EQ(stencil->getStage(1)->getExtents(), (iir::Extents(ast::cartesian, 0, 0, 0, 0, 0, 0)));
-
-  for(auto fieldPair : (*stencil->childrenBegin())->getFields()) {
-    iir::Field& field = fieldPair.second;
-    int AccessID = fieldPair.first;
-    if(AccessID == metadata.getAccessIDFromName("lap")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start + 11, sir::Interval::End}));
-      EXPECT_EQ(field.computeAccessedInterval(),
-                (iir::Interval{sir::Interval::Start + 10, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("out") ||
-       AccessID == metadata.getAccessIDFromName("u")) {
-      EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::End}));
-    }
-    if(AccessID == metadata.getAccessIDFromName("coeff")) {
-      EXPECT_EQ(field.getInterval(),
-                (iir::Interval{sir::Interval::Start, sir::Interval::End, 11, 0}));
-      EXPECT_EQ(field.computeAccessedInterval(), (iir::Interval{12, sir::Interval::End + 1}));
-    }
-  }
+  int inID = metadata.getAccessIDFromName("in");
+  auto fieldToIDMap = stencils[0]->getFields();
+  auto field = fieldToIDMap.at(inID).field;
+  EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::Start, 0, 2}));
 }
 
 } // anonymous namespace
--- dawn/test/unit-test/dawn/Optimizer/Passes/samples/test_field_access_interval_03.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/samples/test_field_access_interval_03.cpp
@@ -1,32 +1,9 @@
-//===--------------------------------------------------------------------------------*- C++ -*-===//
-//                         _       _
-//                        | |     | |
-//                    __ _| |_ ___| | __ _ _ __   __ _
-//                   / _` | __/ __| |/ _` | '_ \ / _` |
-//                  | (_| | || (__| | (_| | | | | (_| |
-//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
-//                    __/ |                       __/ |
-//                   |___/                       |___/
-//
-//
-//  This file is distributed under the MIT License (MIT).
-//  See LICENSE.txt for details.
-//
-//===------------------------------------------------------------------------------------------===//
-
 #include "gtclang_dsl_defs/gtclang_dsl.hpp"
-
 using namespace gtclang::dsl;
 
-stencil compute_extent_test_stencil {
-  storage u, out, coeff, lap, out2;
-
-  Do {
-    vertical_region(k_start, k_start + 10) out = u;
-    vertical_region(k_start + 4, k_start + 14) out2 = u + coeff(k - 2);
-    vertical_region(k_start + 11, k_end) {
-      lap = u(i + 1) + u(i - 1) + u(j + 1) + u(j - 1) - 4.0 * u + coeff(k + 1);
-      out = lap(i + 1) + lap(i - 1) + lap(j + 1) + lap(j - 1) - 4.0 * lap;
-    }
+stencil Test {
+  storage in;
+  void Do() {
+    vertical_region(k_start, k_start + 2) { in = 12; }
   }
 };
