e25435c4d6b272b6ca55604292be3e286cdb28a0
Tobias Wicky <tobiasw@vulcan.com>
2020-02-21 10:05:55 -0800

    change to iir

--- dawn/test/unit-test/dawn/Optimizer/Passes/TestFieldAccessIntervals.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestFieldAccessIntervals.cpp
@@ -16,6 +16,7 @@
 #include "dawn/Compiler/Options.h"
 #include "dawn/IIR/StencilInstantiation.h"
 #include "dawn/SIR/SIR.h"
+#include "dawn/Serialization/IIRSerializer.h"
 #include "dawn/Serialization/SIRSerializer.h"
 #include "test/unit-test/dawn/Optimizer/TestEnvironment.h"
 #include <fstream>
@@ -27,42 +28,8 @@ using namespace dawn;
 
 namespace {
 
-class TestFieldAccessIntervals : public ::testing::Test {
-  dawn::DawnCompiler compiler_;
-
-protected:
-  virtual void SetUp() {}
-
-  std::shared_ptr<iir::StencilInstantiation> loadTest(std::string sirFilename) {
-
-    std::string filename = sirFilename;
-    if(!TestEnvironment::path_.empty())
-      filename = TestEnvironment::path_ + "/" + filename;
-    std::ifstream file(filename);
-    DAWN_ASSERT_MSG((file.good()), std::string("File " + filename + " does not exists").c_str());
-
-    std::string jsonstr((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
-    std::shared_ptr<SIR> sir =
-        SIRSerializer::deserializeFromString(jsonstr, SIRSerializer::Format::Json);
-
-    auto stencilInstantiationMap = compiler_.optimize(compiler_.lowerToIIR(sir));
-    // Report diganostics
-    if(compiler_.getDiagnostics().hasDiags()) {
-      for(const auto& diag : compiler_.getDiagnostics().getQueue())
-        std::cerr << "Compilation Error " << diag->getMessage() << std::endl;
-      throw std::runtime_error("compilation failed");
-    }
-
-    DAWN_ASSERT_MSG(stencilInstantiationMap.count("compute_extent_test_stencil"),
-                    "compute_extent_test_stencil not found in sir");
-
-    return stencilInstantiationMap["compute_extent_test_stencil"];
-  }
-};
-
-TEST_F(TestFieldAccessIntervals, test_field_access_interval_01) {
-  auto stencilInstantiation = loadTest("input/test_field_access_interval_01.sir");
+TEST(TestFieldAccessIntervals, test_field_access_interval_01) {
+  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_01.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
@@ -88,8 +55,8 @@ TEST_F(TestFieldAccessIntervals, test_field_access_interval_01) {
   }
 }
 
-TEST_F(TestFieldAccessIntervals, test_field_access_interval_02) {
-  auto stencilInstantiation = loadTest("input/test_field_access_interval_02.sir");
+TEST(TestFieldAccessIntervals, test_field_access_interval_02) {
+  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_02.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
@@ -119,8 +86,8 @@ TEST_F(TestFieldAccessIntervals, test_field_access_interval_02) {
   }
 }
 
-TEST_F(TestFieldAccessIntervals, test_field_access_interval_03) {
-  auto stencilInstantiation = loadTest("input/test_field_access_interval_03.sir");
+TEST(TestFieldAccessIntervals, test_field_access_interval_03) {
+  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_03.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
@@ -151,8 +118,8 @@ TEST_F(TestFieldAccessIntervals, test_field_access_interval_03) {
   }
 }
 
-TEST_F(TestFieldAccessIntervals, test_field_access_interval_04) {
-  auto stencilInstantiation = loadTest("input/test_field_access_interval_04.sir");
+TEST(TestFieldAccessIntervals, test_field_access_interval_04) {
+  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_04.iir");
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
@@ -167,8 +134,8 @@ TEST_F(TestFieldAccessIntervals, test_field_access_interval_04) {
   EXPECT_EQ((*enclosingInterval), (iir::Interval{2, 14}));
 }
 
-TEST_F(TestFieldAccessIntervals, test_field_access_interval_05) {
-  auto stencilInstantiation = loadTest("input/test_field_access_interval_05.sir");
+TEST(TestFieldAccessIntervals, test_field_access_interval_05) {
+  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_05.iir");
   const auto& metadata = stencilInstantiation->getMetaData();
   const auto& stencils = stencilInstantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
