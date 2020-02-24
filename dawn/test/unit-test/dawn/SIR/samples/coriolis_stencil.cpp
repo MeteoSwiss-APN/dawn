e23756605c155316c4aafb88231f265ad76e14f3
Eddie Davis <eddied@vulcan.com>
2020-02-24 14:59:43 -0800

    Add dawn/SIR/samples DSL files

---
+++ dawn/test/unit-test/dawn/SIR/samples/coriolis_stencil.cpp
@@ -0,0 +1,39 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#include "gtclang_dsl_defs/gtclang_dsl.hpp"
+
+using namespace gtclang::dsl;
+
+stencil coriolis_stencil
+{
+  storage u_nnow, v_nnow, fc, u_tens, v_tens;
+  var z_fv_north, z_fv_south, z_fu_east, z_fu_west;
+
+  Do
+  {
+    vertical_region(k_start, k_end)
+    {
+      z_fv_north = fc * (v_nnow + v_nnow(i + 1));
+      z_fv_south = fc(j - 1) * (v_nnow(j - 1) + v_nnow(i + 1, j - 1));
+      u_tens = u_tens + 0.25 * (z_fv_north + z_fv_south);
+
+      z_fu_east = fc * (u_nnow + u_nnow(j + 1));
+      z_fu_west = fc(i - 1) * (u_nnow(i - 1) + u_nnow(i - 1, j + 1));
+      v_tens = v_tens - 0.25 * (z_fu_east + z_fu_west);
+    }
+  }
+};
