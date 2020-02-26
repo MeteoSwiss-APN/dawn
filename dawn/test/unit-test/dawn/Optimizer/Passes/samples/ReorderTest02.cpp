6a1e0c90443f22e378e3166253c6ace9f05a4b5c
Eddie C Davis <eddiedavis@u.boisestate.edu>
2020-02-25 20:11:47 -0800

    Add braces

--- dawn/test/unit-test/dawn/Optimizer/Passes/samples/ReorderTest02.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/samples/ReorderTest02.cpp
@@ -6,12 +6,17 @@ stencil Test {
   storage field_b0, field_b1, field_b2;
 
   Do {
-    vertical_region(k_end, k_start) field_b1 = field_b0;
-
-    vertical_region(k_start, k_end) field_a1 = field_a0;
-
-    vertical_region(k_end, k_start) field_b2 = field_b1;
-
-    vertical_region(k_start, k_end) field_a2 = field_a1;
+    vertical_region(k_end, k_start) {
+      field_b1 = field_b0;
+    }
+    vertical_region(k_start, k_end) {
+      field_a1 = field_a0;
+    }
+    vertical_region(k_end, k_start) {
+      field_b2 = field_b1;
+    }
+    vertical_region(k_start, k_end) {
+      field_a2 = field_a1;
+    }
   }
 };
