47748fe2943d121aadd4d5cd1fe111d6991d88d8
Johann Dahm <johannd@vulcan.com>
2020-02-27 06:00:56 -0800

    Add CodeGen options to OptimizerContextOptions

--- dawn/src/dawn/Optimizer/OptimizerContext.h
+++ dawn/src/dawn/Optimizer/OptimizerContext.h
@@ -48,6 +48,7 @@ public:
   struct OptimizerContextOptions {
 #define OPT(TYPE, NAME, DEFAULT_VALUE, OPTION, OPTION_SHORT, HELP, VALUE_NAME, HAS_VALUE, F_GROUP) \
   TYPE NAME = DEFAULT_VALUE;
+#include "dawn/CodeGen/Options.inc"
 #include "dawn/Optimizer/Options.inc"
 #undef OPT
   };
