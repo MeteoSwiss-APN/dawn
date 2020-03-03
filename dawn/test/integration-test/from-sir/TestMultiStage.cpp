611a0f261f22ec678dc22c661bb8faa340ccf390
mroeth <matthias.roethlin@meteoswiss.ch>
2020-03-03 16:34:11 +0100

    moved tests, running minimal number of passes

--- dawn/test/integration-test/from-sir/TestMultiStage.cpp
+++ dawn/test/integration-test/from-sir/TestMultiStage.cpp
@@ -17,6 +17,7 @@
 #include "dawn/IIR/StencilInstantiation.h"
 #include "dawn/SIR/SIR.h"
 #include "dawn/Serialization/SIRSerializer.h"
+#include "dawn/Unittest/CompilerUtil.h"
 #include "test/unit-test/dawn/Optimizer/TestEnvironment.h"
 #include <fstream>
 #include <gtest/gtest.h>
@@ -28,9 +29,16 @@ using namespace dawn;
 namespace {
 
 class MultiStageTest : public ::testing::Test {
+  dawn::OptimizerContext::OptimizerContextOptions options_;
+  DiagnosticsEngine diagnostics_;
   dawn::DawnCompiler compiler_;
+  std::unique_ptr<OptimizerContext> context_;
 
 protected:
+  MultiStageTest() {
+    context_ = std::make_unique<dawn::OptimizerContext>(diagnostics_, options_, nullptr);
+  }
+
   virtual void SetUp() {}
 
   std::shared_ptr<iir::StencilInstantiation> loadTest(std::string sirFilename,
@@ -45,7 +53,14 @@ protected:
     std::shared_ptr<SIR> sir =
         SIRSerializer::deserializeFromString(jsonstr, SIRSerializer::Format::Json);
 
-    auto stencilInstantiationMap = compiler_.optimize(compiler_.lowerToIIR(sir));
+    auto stencilInstantiationMap = compiler_.lowerToIIR(sir);
+    DAWN_ASSERT_MSG(stencilInstantiationMap.size() == 1, "unexpected number of stencils");
+    CompilerUtil::runGroup(PassGroup::StageReordering, context_,
+                           stencilInstantiationMap.begin()->second);
+    // stage merger segfaults if stage reordering is not run beforehand
+    CompilerUtil::runGroup(PassGroup::StageMerger, context_,
+                           stencilInstantiationMap.begin()->second);
+
     // Report diagnostics
     if(compiler_.getDiagnostics().hasDiags()) {
       for(const auto& diag : compiler_.getDiagnostics().getQueue())
