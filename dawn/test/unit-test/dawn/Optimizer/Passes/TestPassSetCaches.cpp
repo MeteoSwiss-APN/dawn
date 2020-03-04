1ee51f1da015412aa11c1e87a3093afd4a2301d0
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-03-04 12:59:40 -0800

    Refactor set cache tests to start from SIR and add missing tests (#795)

--- dawn/test/unit-test/dawn/Optimizer/Passes/TestPassSetCaches.cpp
+++ dawn/test/unit-test/dawn/Optimizer/Passes/TestPassSetCaches.cpp
@@ -17,91 +17,230 @@
 #include "dawn/IIR/IIR.h"
 #include "dawn/IIR/StencilInstantiation.h"
 #include "dawn/Optimizer/PassSetCaches.h"
+#include "dawn/Optimizer/PassStageSplitter.h"
 #include "dawn/Serialization/IIRSerializer.h"
-#include "dawn/Unittest/CompilerUtil.h"
+#include "dawn/Support/Iterator.h"
 #include "test/unit-test/dawn/Optimizer/TestEnvironment.h"
 
 #include <fstream>
 #include <gtest/gtest.h>
 
-using namespace dawn;
-
 namespace {
+
+using namespace dawn;
+using namespace dawn::iir;
 
 class TestPassSetCaches : public ::testing::Test {
 protected:
-  dawn::OptimizerContext::OptimizerContextOptions options_;
-  std::unique_ptr<OptimizerContext> context_;
+  OptimizerContext::OptimizerContextOptions options_;
+  std::unique_ptr<dawn::OptimizerContext> context_;
+  DiagnosticsEngine diag_;
 
-  void runTest(const std::string& filename, int nStencils, int nMultiStages,
+  explicit TestPassSetCaches() {
+    std::shared_ptr<SIR> sir = std::make_shared<SIR>(ast::GridType::Cartesian);
+    context_ = std::make_unique<OptimizerContext>(diag_, options_, sir);
+    dawn::UIDGenerator::getInstance()->reset();
+  }
+
+  void runTest(const std::string& filename, int nMultiStages,
                const std::vector<std::vector<std::string>>& fieldNames,
-               const std::vector<std::vector<iir::Cache::CacheType>>& cacheTypes,
-               const std::vector<std::vector<iir::Cache::IOPolicy>>& ioPolicies) {
-    if(nStencils < 1)
-      nStencils = 1;
+               const std::vector<std::vector<Cache::CacheType>>& cacheTypes,
+               const std::vector<std::vector<Cache::IOPolicy>>& ioPolicies) {
+    std::string filepath = filename;
+    if(!TestEnvironment::path_.empty() && filepath.find('/') != 0)
+      filepath = TestEnvironment::path_ + "/" + filepath;
+    auto instantiation = IIRSerializer::deserialize(filepath);
 
-    std::shared_ptr<iir::StencilInstantiation> instantiation =
-        CompilerUtil::load(filename, options_, context_, TestEnvironment::path_);
-
-    ASSERT_TRUE(CompilerUtil::runGroup(PassGroup::Parallel, context_, instantiation));
-    ASSERT_TRUE(CompilerUtil::runGroup(PassGroup::StageReordering, context_, instantiation));
+    // Run stage splitter pass
+    PassStageSplitter stageSplitPass(*context_);
+    EXPECT_TRUE(stageSplitPass.run(instantiation));
 
     // Expect pass to succeed...
-    ASSERT_TRUE(CompilerUtil::runPass<dawn::PassSetCaches>(context_, instantiation));
+    PassSetCaches setCachesPass(*context_);
+    EXPECT_TRUE(setCachesPass.run(instantiation));
 
     auto& stencils = instantiation->getStencils();
-    ASSERT_EQ(stencils.size(), nStencils);
+    ASSERT_GT(stencils.size(), 0);
 
-    for(int i = 0; i < nStencils; ++i) {
-      auto& multiStages = stencils[i]->getChildren();
-      ASSERT_EQ(multiStages.size(), nMultiStages);
+    auto& stencil = stencils[0];
+    auto& multiStages = stencil->getChildren();
+    ASSERT_EQ(multiStages.size(), nMultiStages);
 
-      int j = 0;
-      for(const auto& multiStage : multiStages) {
-        unsigned nFields = fieldNames[j].size();
-        for(int k = 0; k < nFields; ++k) {
-          int accessID = stencils[i]->getMetadata().getAccessIDFromName(fieldNames[j][k]);
-          ASSERT_TRUE(multiStage->isCached(accessID));
-          ASSERT_TRUE(multiStage->getCache(accessID).getType() == cacheTypes[j][k]);
-          ASSERT_TRUE(multiStage->getCache(accessID).getIOPolicy() == ioPolicies[j][k]);
-        }
-        j += 1;
+    for(auto [i, multiStage] : enumerate(multiStages)) {
+      for(auto [j, fieldName] : enumerate(fieldNames[i])) {
+        int accessID = stencil->getMetadata().getAccessIDFromName(fieldName);
+        ASSERT_TRUE(multiStage->isCached(accessID));
+        ASSERT_TRUE(multiStage->getCache(accessID).getType() == cacheTypes[i][j]);
+        ASSERT_TRUE(multiStage->getCache(accessID).getIOPolicy() == ioPolicies[i][j]);
       }
     }
   }
 };
 
 TEST_F(TestPassSetCaches, IJCacheTest1) {
-  runTest("input/IJCacheTest01.sir", 1, 1, {{"tmp"}}, {{iir::Cache::CacheType::IJ}},
-          {{iir::Cache::IOPolicy::local}});
+  /*
+    vertical_region(k_start, k_end) {
+      tmp = in;
+      out = tmp(i + 1);
+    } */
+  runTest("input/IJCacheTest01.iir", 1, {{"tmp"}}, {{Cache::CacheType::IJ}},
+          {{Cache::IOPolicy::local}});
 }
 
 TEST_F(TestPassSetCaches, IJCacheTest2) {
-  runTest("input/IJCacheTest02.sir", 1, 1, {{"tmp"}}, {{iir::Cache::CacheType::IJ}},
-          {{iir::Cache::IOPolicy::local}});
+  /*
+    vertical_region(k_start, k_end) {
+      tmp = in;
+    }
+    vertical_region(k_start, k_end) {
+      out = tmp(i + 1);
+    } */
+  runTest("input/IJCacheTest02.iir", 1, {{"tmp"}}, {{Cache::CacheType::IJ}},
+          {{Cache::IOPolicy::local}});
 }
 
 TEST_F(TestPassSetCaches, KCacheTest1) {
-  runTest("input/KCacheTest01.sir", 1, 1, {{"tmp"}}, {{iir::Cache::CacheType::K}},
-          {{iir::Cache::IOPolicy::fill}});
+  /*
+    vertical_region(k_end, k_end) {
+      tmp = in;
+    }
+    vertical_region(k_end - 1, k_start) {
+      out = tmp(k + 1);
+    } */
+  runTest("input/KCacheTest01.iir", 1, {{"tmp"}}, {{Cache::CacheType::K}},
+          {{Cache::IOPolicy::fill}});
 }
 
 TEST_F(TestPassSetCaches, KCacheTest1b) {
-  runTest("input/KCacheTest01b.sir", 1, 1, {{"tmp"}}, {{iir::Cache::CacheType::K}},
-          {{iir::Cache::IOPolicy::local}});
+  /*
+    vertical_region(k_end, k_end) {
+      tmp = in;
+    }
+    vertical_region(k_end - 1, k_start) {
+      tmp = in * 2;
+      out = tmp(k + 1);
+    } */
+  runTest("input/KCacheTest01b.iir", 1, {{"tmp"}}, {{Cache::CacheType::K}},
+          {{Cache::IOPolicy::local}});
 }
 
 TEST_F(TestPassSetCaches, KCacheTest2) {
-  runTest("input/KCacheTest02.sir", 1, 2, {{"tmp"}, {"tmp"}},
-          {{iir::Cache::CacheType::K}, {iir::Cache::CacheType::K}},
-          {{iir::Cache::IOPolicy::fill_and_flush}, {iir::Cache::IOPolicy::fill}});
+  /*
+    vertical_region(k_start, k_start) {
+      tmp = a;
+    }
+    vertical_region(k_start + 1, k_end) {
+      b = tmp(k - 1);
+    }
+    vertical_region(k_end, k_end) {
+      tmp = (b(k - 1) + b) * tmp;
+    }
+    vertical_region(k_end - 1, k_start) {
+      c = tmp(k + 1);
+    } */
+  runTest("input/KCacheTest02.iir", 2, {{"tmp"}, {"tmp"}},
+          {{Cache::CacheType::K}, {Cache::CacheType::K}},
+          {{Cache::IOPolicy::fill_and_flush}, {Cache::IOPolicy::fill}});
 }
 
 TEST_F(TestPassSetCaches, KCacheTest2b) {
-  runTest("input/KCacheTest02b.sir", 1, 2, {{"tmp"}, {"b", "tmp"}},
-          {{iir::Cache::CacheType::K}, {iir::Cache::CacheType::K, iir::Cache::CacheType::K}},
-          {{iir::Cache::IOPolicy::fill_and_flush},
-           {iir::Cache::IOPolicy::fill, iir::Cache::IOPolicy::bpfill}});
+  /*
+    vertical_region(k_start, k_start) {
+      tmp = a;
+    }
+    vertical_region(k_start + 1, k_end) {
+      b = tmp(k - 1);
+    }
+    vertical_region(k_end, k_end) {
+      tmp = (b(k - 1) + b) * tmp;
+    }
+    vertical_region(k_end - 1, k_start) {
+      tmp = 2 * b;
+      c = tmp(k + 1);
+    } */
+  runTest("input/KCacheTest02b.iir", 2, {{"tmp"}, {"b", "tmp"}},
+          {{Cache::CacheType::K}, {Cache::CacheType::K, Cache::CacheType::K}},
+          {{Cache::IOPolicy::fill_and_flush}, {Cache::IOPolicy::fill, Cache::IOPolicy::bpfill}});
+}
+
+TEST_F(TestPassSetCaches, KCacheTest3) {
+  /*
+    vertical_region(k_start, k_end) {
+      tmp = in;
+      b = a;
+      c = b(k + 1);
+      c = b(k - 1);
+      out = tmp;
+    } */
+  runTest("input/KCacheTest03.iir", 2, {{"tmp"}, {"b", "tmp"}},
+          {{Cache::CacheType::K}, {Cache::CacheType::K, Cache::CacheType::K}},
+          {{Cache::IOPolicy::flush}, {Cache::IOPolicy::fill, Cache::IOPolicy::fill}});
+}
+
+TEST_F(TestPassSetCaches, KCacheTest4) {
+  /*
+    vertical_region(k_start, k_end) {
+      tmp = in;
+      b1 = a1;
+      c1 = b1(k + 1);
+      c1 = b1(k - 1);
+      out = tmp;
+      tmp = in;
+      b2 = a2;
+      c2 = b2(k + 1);
+      c2 = b2(k - 1);
+      out = tmp;
+    } */
+  auto kCacheType = Cache::CacheType::K;
+  auto fillPolicy = Cache::IOPolicy::fill;
+  auto flushPolicy = Cache::IOPolicy::flush;
+  auto fillAndFlush = Cache::IOPolicy::fill_and_flush;
+
+  runTest("input/KCacheTest04.iir", 3, {{"tmp"}, {"b1", "tmp"}, {"b2", "tmp"}},
+          {{kCacheType}, {kCacheType, kCacheType}, {kCacheType, kCacheType}},
+          {{flushPolicy}, {fillPolicy, fillAndFlush}, {fillPolicy, fillPolicy}});
+}
+
+TEST_F(TestPassSetCaches, KCacheTest5) {
+  /*
+    stencil Test1 {
+      vertical_region(k_start, k_end) {
+        out = in + in(k + 1);
+      }
+    }
+    stencil Test2 {
+      vertical_region(k_start, k_end) {
+        out += in + in(k + 1);
+      }
+    } */
+  auto kCacheType = Cache::CacheType::K;
+  auto fillPolicy = Cache::IOPolicy::fill;
+  auto fillAndFlush = Cache::IOPolicy::fill_and_flush;
+
+  runTest("input/KCacheTest05_Test1.iir", 1, {{"in"}}, {{kCacheType}}, {{fillPolicy}});
+  runTest("input/KCacheTest05_Test2.iir", 1, {{"in", "out"}}, {{kCacheType, kCacheType}},
+          {{fillPolicy, fillAndFlush}});
+}
+
+TEST_F(TestPassSetCaches, KCacheTest6) {
+  /*
+    vertical_region(k_start, k_start) {
+      tmp = a;
+    }
+    vertical_region(k_start + 1, k_end) {
+      tmp = a * 2;
+      b = tmp(k - 1);
+    }
+    vertical_region(k_end - 3, k_end - 3) {
+      c = tmp[k + 3] + tmp[k + 2] + tmp[k + 1];
+    }
+    vertical_region(k_end - 4, k_start) {
+      tmp = b;
+      c = tmp[k + 1];
+    } */
+  auto kCacheType = Cache::CacheType::K;
+  runTest("input/KCacheTest06.iir", 2, {{"tmp"}, {"tmp"}}, {{kCacheType}, {kCacheType}},
+          {{Cache::IOPolicy::epflush}, {Cache::IOPolicy::bpfill}});
 }
 
 } // anonymous namespace
---
+++ dawn/test/unit-test/dawn/Optimizer/Passes/samples/KCacheTest01.cpp
@@ -0,0 +1,16 @@
+#include "gtclang_dsl_defs/gtclang_dsl.hpp"
+using namespace gtclang::dsl;
+
+stencil Test {
+    storage in, out;
+    var tmp;
+
+    Do {
+      vertical_region(k_end, k_end) {
+        tmp = in;
+      }
+      vertical_region(k_end - 1, k_start) {
+        out = tmp(k + 1);
+      }
+    }
+};
