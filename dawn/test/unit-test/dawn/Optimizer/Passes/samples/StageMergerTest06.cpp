ed190e5e19f10c2a6a733df6a6beaff548284906
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-03-03 19:35:51 -0800

    Refactor stage merger pass tests to start from IIR (#794)
    Refactors stage merger pass tests to start from IIR, adds serialized IIR files, corresponding DSL files, and removes SIR files.

---
+++ dawn/test/unit-test/dawn/Optimizer/Passes/samples/StageMergerTest06.cpp
@@ -0,0 +1,15 @@
+#include "gtclang_dsl_defs/gtclang_dsl.hpp"
+using namespace gtclang::dsl;
+
+stencil Test {
+    storage field_a0, field_a1, field_a2;
+
+    Do {
+      vertical_region(k_start, k_start) {
+        field_a1 = field_a0;
+      }
+      vertical_region(k_start + 1, k_end) {
+        field_a2 = field_a1(i + 1);
+      }
+    }
+};
