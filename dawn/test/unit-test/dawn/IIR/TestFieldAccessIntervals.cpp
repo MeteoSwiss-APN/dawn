d15d4c3c5cb00ab1d9dd482ac141011f72fb7b50
Eddie Davis <eddied@vulcan.com>
2020-03-06 08:55:34 -0800

    Modify TestFieldAccessIntervalTests to use IIRBuilder and move to IIR module

--- dawn/test/unit-test/dawn/IIR/TestFieldAccessIntervals.cpp
+++ dawn/test/unit-test/dawn/IIR/TestFieldAccessIntervals.cpp
@@ -15,6 +15,7 @@
 #include "dawn/IIR/IIR.h"
 #include "dawn/IIR/StencilInstantiation.h"
 #include "dawn/Serialization/IIRSerializer.h"
+#include "dawn/Unittest/IIRBuilder.h"
 
 #include <gtest/gtest.h>
 
@@ -24,12 +25,19 @@ namespace {
 
 TEST(TestFieldAccessIntervals, test_field_access_interval_01) {
   /*
-    interval k_flat = k_start + 12;
-    vertical_region(k_start, k_flat) { in = 12; }
+    vertical_region(k_start, k_start + 12) { in = 12; }
   */
-  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_01.iir");
-  const auto& metadata = stencilInstantiation->getMetaData();
-  const auto& stencils = stencilInstantiation->getStencils();
+  iir::CartesianIIRBuilder b;
+  auto in_f = b.field("in", iir::FieldType::ijk);
+
+  auto instantiation = b.build(
+      "Test", b.stencil(b.multistage(
+                  iir::LoopOrderKind::Forward,
+                  b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::Start, 0, 12,
+                                     b.block(b.stmt(b.assignExpr(b.at(in_f), b.lit(12.)))))))));
+
+  const auto& metadata = instantiation->getMetaData();
+  const auto& stencils = instantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
 
   int inID = metadata.getAccessIDFromName("in");
@@ -44,9 +52,21 @@ TEST(TestFieldAccessIntervals, test_field_access_interval_02) {
     vertical_region(k_start, k_start) { in = 12; }
     vertical_region(k_end, k_end) { in = 10; }
   */
-  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_02.iir");
-  const auto& metadata = stencilInstantiation->getMetaData();
-  const auto& stencils = stencilInstantiation->getStencils();
+  iir::CartesianIIRBuilder b;
+  auto in_f = b.field("in", iir::FieldType::ijk);
+  auto instantiation = b.build(
+      "Test",
+      b.stencil(
+          b.multistage(iir::LoopOrderKind::Forward,
+                       b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::Start,
+                                          b.block(b.stmt(b.assignExpr(b.at(in_f), b.lit(12.))))))),
+          b.multistage(
+              iir::LoopOrderKind::Forward,
+              b.stage(b.doMethod(dawn::sir::Interval::End, dawn::sir::Interval::End,
+                                 b.block(b.stmt(b.assignExpr(b.at(in_f), b.lit(10.)))))))));
+
+  const auto& metadata = instantiation->getMetaData();
+  const auto& stencils = instantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
 
   int inID = metadata.getAccessIDFromName("in");
@@ -54,13 +74,22 @@ TEST(TestFieldAccessIntervals, test_field_access_interval_02) {
   auto field = fieldToIDMap.at(inID).field;
   EXPECT_EQ(field.getInterval(), (iir::Interval{sir::Interval::Start, sir::Interval::End, 0, 0}));
 }
+
 TEST(TestFieldAccessIntervals, test_field_access_interval_03) {
   /*
     vertical_region(k_start, k_start + 2) { in = 12; }
   */
-  auto stencilInstantiation = IIRSerializer::deserialize("input/test_field_access_interval_03.iir");
-  const auto& metadata = stencilInstantiation->getMetaData();
-  const auto& stencils = stencilInstantiation->getStencils();
+  iir::CartesianIIRBuilder b;
+  auto in_f = b.field("in", iir::FieldType::ijk);
+
+  auto instantiation = b.build(
+      "Test", b.stencil(b.multistage(
+                  iir::LoopOrderKind::Forward,
+                  b.stage(b.doMethod(dawn::sir::Interval::Start, dawn::sir::Interval::Start, 0, 2,
+                                     b.block(b.stmt(b.assignExpr(b.at(in_f), b.lit(12.)))))))));
+
+  const auto& metadata = instantiation->getMetaData();
+  const auto& stencils = instantiation->getStencils();
   EXPECT_EQ(stencils.size(), 1);
 
   int inID = metadata.getAccessIDFromName("in");
--- dawn/test/unit-test/dawn/Optimizer/samples/test_field_access_interval_03.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtclang_dsl_defs/gtclang_dsl.hpp"
-using namespace gtclang::dsl;
-
-stencil Test {
-  storage in;
-  void Do() {
-    vertical_region(k_start, k_start + 2) { in = 12; }
-  }
-};
