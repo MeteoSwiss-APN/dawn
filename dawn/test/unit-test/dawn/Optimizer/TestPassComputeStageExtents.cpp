9da8ef69ea944be4b505afc71dcf1154bd50f2e4
Eddie Davis <eddied@vulcan.com>
2020-03-06 08:17:27 -0800

    Merge TestComputeMaxExtent with TestPassComputeStageExtents

--- dawn/test/unit-test/dawn/Optimizer/TestPassComputeStageExtents.cpp
+++ dawn/test/unit-test/dawn/Optimizer/TestPassComputeStageExtents.cpp
@@ -31,9 +31,16 @@ TEST(TestComputeStageExtents, test_stencil_01) {
   vertical_region(k_start, k_end) { out = in[i - 1]; }
   */
   auto instantiation = IIRSerializer::deserialize("input/compute_extent_test_stencil_01.iir");
-  const auto& stencils = instantiation->getIIR()->getChildren();
+  const std::unique_ptr<iir::IIR>& IIR = instantiation->getIIR();
+  const auto& metadata = instantiation->getMetaData();
+  const auto& stencils = IIR->getChildren();
   ASSERT_TRUE((stencils.size() == 1));
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
+
+  auto fields = stencil->getFields();
+  int inID = metadata.getAccessIDFromName("in");
+  EXPECT_EQ(fields.at(inID).field.getExtentsRB(),
+            (iir::Extents(dawn::ast::cartesian, -1, -1, 0, 0, 0, 0)));
 
   std::unique_ptr<OptimizerContext> context;
   PassComputeStageExtents pass(*context);
@@ -47,11 +54,31 @@ TEST(TestComputeStageExtents, test_stencil_02) {
   /*
   vertical_region(k_start, k_end) {
       mid = in[i - 1];
+      out = in[j + 1];
+    } */
+  auto instantiation = IIRSerializer::deserialize("input/compute_extent_test_stencil_02.iir");
+  const auto& metadata = instantiation->getMetaData();
+  const std::unique_ptr<iir::IIR>& IIR = instantiation->getIIR();
+  const auto& stencils = IIR->getChildren();
+  ASSERT_TRUE((stencils.size() == 1));
+  const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
+  auto fields = stencil->getFields();
+  int inID = metadata.getAccessIDFromName("in");
+
+  EXPECT_EQ(fields.at(inID).field.getExtentsRB(),
+            (iir::Extents(dawn::ast::cartesian, -1, 0, 0, 1, 0, 0)));
+}
+
+TEST(TestComputeStageExtents, test_stencil_03) {
+  /*
+    vertical_region(k_start, k_end) {
+      mid = in[i - 1];
       out = mid[i - 1];
-    }
-  */
+    } */
   auto instantiation = IIRSerializer::deserialize("input/compute_extent_test_stencil_03.iir");
-  const auto& stencils = instantiation->getIIR()->getChildren();
+  const auto& metadata = instantiation->getMetaData();
+  const std::unique_ptr<iir::IIR>& IIR = instantiation->getIIR();
+  const auto& stencils = IIR->getChildren();
   ASSERT_TRUE((stencils.size() == 1));
   const std::unique_ptr<iir::Stencil>& stencil = stencils[0];
 
@@ -62,16 +89,21 @@ TEST(TestComputeStageExtents, test_stencil_02) {
   EXPECT_EQ(stencil->getNumStages(), 2);
   EXPECT_EQ(stencil->getStage(0)->getExtents(), iir::Extents(ast::cartesian, -1, 0, 0, 0, 0, 0));
   EXPECT_EQ(stencil->getStage(1)->getExtents(), iir::Extents(ast::cartesian));
+
+  auto fields = stencil->getFields();
+  int inID = metadata.getAccessIDFromName("in");
+
+  EXPECT_EQ(fields.at(inID).field.getExtentsRB(),
+            (iir::Extents(dawn::ast::cartesian, -2, -1, 0, 0, 0, 0)));
 }
 
-TEST(TestComputeStageExtents, test_stencil_03) {
+TEST(TestComputeStageExtents, test_stencil_04) {
   /*
-      vertical_region(k_start, k_end) {
+    vertical_region(k_start, k_end) {
       mid = in;
       mid2 = mid[i + 1];
       out = mid2[j - 1];
-    }
-  */
+    } */
   auto instantiation = IIRSerializer::deserialize("input/compute_extent_test_stencil_04.iir");
   const auto& stencils = instantiation->getIIR()->getChildren();
   ASSERT_TRUE((stencils.size() == 1));
