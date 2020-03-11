b41633fcca0152608296b77aaf90d99f743ff3d1
Johann Dahm <johannd@vulcan.com>
2020-03-10 17:23:44 -0700

    Try single script

--- scripts/jenkins/srun_dawn_PR.sh
+++ scripts/jenkins/srun_dawn_PR.sh
@@ -9,10 +9,7 @@ image=gtclang/dawn-env-ubuntu19.10
 module load daint-gpu
 module load sarus
 
-sarus images
-sarus pull $image
-#sarus rmi gtclang/dawn-env-ubuntu19.04
-sarus images
+# sarus pull $image
 
 srun --job-name=dawn_PR \
      --time=00:45:00 \
@@ -25,16 +22,18 @@ srun --job-name=dawn_PR \
      --account=c14 \
      sarus run \
      --mount=type=bind,source=$rootdir,destination=/usr/src/dawn \
+     -workdir /usr/src/dawn \
      $image \
-     cmake -S /usr/src/dawn -B /usr/src/dawn/build \
+     bash -c " \
+     cmake -S . -B build \
     -DBUILD_TESTING=ON \
     -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 \
     -DCMAKE_INSTALL_PREFIX=/usr/local \
     -DGridTools_DIR=/usr/local/lib/cmake \
     -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages \
     -GNinja \
-    && cmake --build /usr/src/dawn/build --parallel 24 --target install \
+    && cmake --build build --parallel 24 --target install \
     && python -m pip install -e /usr/src/dawn/dawn \
-    && cd /usr/src/dawn/build && ctest --progress --output-on-failure
+    && cd /usr/src/dawn/build && ctest --progress --output-on-failure"
 
 #     $BASEPATH_SCRIPT/dawn_PR.sh "$@"
