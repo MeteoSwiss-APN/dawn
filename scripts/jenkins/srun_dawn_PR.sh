6e38c521c12b3660de3ae51a50108eec17d4e7aa
Johann Dahm <johannd@vulcan.com>
2020-03-10 21:06:34 -0700

    Add clang-gridtools

--- scripts/jenkins/srun_dawn_PR.sh
+++ scripts/jenkins/srun_dawn_PR.sh
@@ -9,7 +9,15 @@ image=gtclang/dawn-env-ubuntu19.10
 module load daint-gpu
 module load sarus
 
-# sarus pull $image
+sarus pull $image
+
+if [ -z ${CLANG_GRIDTOOLS_REPOSITORY+x} ]; then
+  # ssh port is blocked on compute nodes, go via https port
+  export CLANG_GRIDTOOLS_REPOSITORY=ssh://git@ssh.github.com:443/MeteoSwiss-APN/clang-gridtools.git
+fi
+if [ -z ${CLANG_GRIDTOOLS_BRANCH+x} ]; then
+  export CLANG_GRIDTOOLS_BRANCH=master
+fi
 
 srun --job-name=dawn_PR \
      --time=00:45:00 \
@@ -23,6 +31,14 @@ srun --job-name=dawn_PR \
      sarus run \
      --mount=type=bind,source=$rootdir,destination=/usr/src/dawn \
      $image \
-     bash -c "cmake -S /usr/src/dawn -B /usr/src/dawn/build -DBUILD_TESTING=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 -DCMAKE_INSTALL_PREFIX=/usr/local -DGridTools_DIR=/usr/local/lib/cmake -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages -GNinja && cmake --build /usr/src/dawn/build --parallel 24 --target install && python -m pip install -e /usr/src/dawn/dawn && (cd /usr/src/dawn/build && ctest --progress --output-on-failure)"
-
-#     $BASEPATH_SCRIPT/dawn_PR.sh "$@"
+     bash -c " \
+     cmake -S /usr/src/dawn -B /usr/src/dawn/build \
+          -DBUILD_TESTING=ON -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 \
+          -DCMAKE_INSTALL_PREFIX=/usr/local \
+          -DGridTools_DIR=/usr/local/lib/cmake \
+          -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages \
+          -GNinja && \
+     cmake --build /usr/src/dawn/build --parallel 24 --target install && \
+     python -m pip install -e /usr/src/dawn/dawn && \
+     (cd /usr/src/dawn/build && ctest --progress --output-on-failure) &&
+     /usr/src/dawn/scripts/jenkins/build_clang_gridtools.sh -r $CLANG_GRIDTOOLS_REPOSITORY -b $CLANG_GRIDTOOLS_BRANCH"
