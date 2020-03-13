b84f395c3aa44f894c3072f44c347531c0380baa
Johann Dahm <johannd@vulcan.com>
2020-03-12 21:27:10 -0700

    Clone clang-gridtools outside container

--- scripts/jenkins/srun_dawn_PR.sh
+++ scripts/jenkins/srun_dawn_PR.sh
@@ -7,6 +7,17 @@ image=gtclang/dawn-env-ubuntu19.10
 module load daint-gpu
 module load sarus
 
+# Clone clang-gridtools here because the image does not have the ssh key
+if [ -z ${CLANG_GRIDTOOLS_REPOSITORY+x} ]; then
+  # ssh port is blocked on compute nodes, go via https port
+  export CLANG_GRIDTOOLS_REPOSITORY=ssh://git@ssh.github.com:443/MeteoSwiss-APN/clang-gridtools.git
+fi
+if [ -z ${CLANG_GRIDTOOLS_BRANCH+x} ]; then
+  export CLANG_GRIDTOOLS_BRANCH=master
+fi
+
+git clone --depth 1 -b $CLANG_GRIDTOOLS_BRANCH $CLANG_GRIDTOOLS_REPOSITORY clang-gridtools
+
 sarus pull $image
 
 srun --job-name=dawn_PR \
@@ -18,8 +29,10 @@ srun --job-name=dawn_PR \
     --partition=cscsci \
     --constraint=gpu \
     --account=c14 \
-    sarus run --mount=type=bind,source=$rootdir,destination=/usr/src/dawn $image \
-        /usr/src/dawn/scripts/build-and-test /usr/src/dawn /usr/src/dawn-build /usr/local \
+    sarus run --mount=type=bind,source=$rootdir,destination=/usr/src/dawn \
+        --mount=type=bind,source=clang-gridtools,destination=/usr/src/clang-gridtools \
+        $image \
+        /usr/src/dawn/scripts/build-and-test /usr/src/dawn /usr/src/dawn-build /usr/local clang-gridtools \
             -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 \
             -DGridTools_DIR=/usr/local/lib/cmake \
             -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages \
