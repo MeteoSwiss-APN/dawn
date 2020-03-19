c6fbbe7d1463a1252f76f0524cd6a6e8979671a5
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-03-19 17:31:23 +0100

    Fixed dawn4py

--- dawn/examples/python/unstructured_stencil.py
+++ dawn/examples/python/unstructured_stencil.py
@@ -45,8 +45,7 @@ def main(args: argparse.Namespace):
                     "+",
                     sir_utils.make_literal_access_expr("1.0", SIR.BuiltinType.Float),
                     sir_utils.make_field_access_expr("in"),
-                    lhs_location=SIR.LocationType.Value("Edge"),
-                    rhs_location=SIR.LocationType.Value("Cell"),
+                    chain=[SIR.LocationType.Value("Edge"), SIR.LocationType.Value("Cell")],
                 ),
                 "=",
             )
