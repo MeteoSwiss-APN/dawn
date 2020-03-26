4da9315218bd81fd00c3f8780c7224f3790e1a1f
Eddie Davis <eddied@vulcan.com>
2020-03-26 12:58:40 -0700

    Rename ReoderStrategyPartitioning -> ReorderStrategyPartitioning

--- dawn/src/dawn/Optimizer/ReorderStrategyPartitioning.h
+++ dawn/src/dawn/Optimizer/ReorderStrategyPartitioning.h
@@ -22,7 +22,7 @@ namespace dawn {
 /// @brief Reordering strategy which uses S-cut graph partitioning to reorder the stages and
 /// statements
 /// @ingroup optimizer
-class ReoderStrategyPartitioning : public ReorderStrategy {
+class ReorderStrategyPartitioning : public ReorderStrategy {
 public:
   /// @brief Apply the reordering strategy and return the stencil
   virtual std::unique_ptr<iir::Stencil> reorder(iir::StencilInstantiation* instantiation,
