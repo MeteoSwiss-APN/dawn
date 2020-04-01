cf3e289a2f556b3a488006e2447d5bfeb4378793
Johann Dahm <johannd@vulcan.com>
2020-04-01 10:04:30 -0700

    Driver methods (#877)
    Add drivers for each gtclang and dawn component and uses these in the executables. This does add code, but that is mostly in separating options, and more code will be eliminated once only these drivers are used (follow-up PR).

---
+++ dawn/src/dawn/CodeGen/Options.h
@@ -0,0 +1,37 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                          _
+//                         | |
+//                       __| | __ ___      ___ ___
+//                      / _` |/ _` \ \ /\ / / '_  |
+//                     | (_| | (_| |\ V  V /| | | |
+//                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+#ifndef DAWN_CODEGEN_OPTIONS_H
+#define DAWN_CODEGEN_OPTIONS_H
+
+namespace dawn {
+namespace codegen {
+
+/// @brief CodeGen backends
+enum class Backend { GridTools, CXXNaive, CXXNaiveIco, CUDA, CXXOpt };
+
+/// @brief Options for all codegen backends combined.
+///
+/// These have to be disjoint from the options for other dawn and gtc-parse components.
+struct Options {
+#define OPT(TYPE, NAME, DEFAULT_VALUE, OPTION, OPTION_SHORT, HELP, VALUE_NAME, HAS_VALUE, F_GROUP) \
+  TYPE NAME = DEFAULT_VALUE;
+#include "dawn/CodeGen/Options.inc"
+#undef OPT
+};
+
+} // namespace codegen
+} // namespace dawn
+
+#endif
