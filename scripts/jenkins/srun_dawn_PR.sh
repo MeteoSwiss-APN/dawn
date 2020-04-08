0e01e8a7f9be0a359a39aa60c7b95d9f6f2e83b2
Johann Dahm <johannd@vulcan.com>
2020-04-08 08:30:16 -0700

    Revert "Try docker instead of sarus"
    This reverts commit 0d39abfb998c7274175fde8bcb16073f2d3703a9.

--- scripts/jenkins/srun_dawn_PR.sh
+++ scripts/jenkins/srun_dawn_PR.sh
@@ -19,7 +19,7 @@ fi
 
 git clone --depth 1 -b $CLANG_GRIDTOOLS_BRANCH $CLANG_GRIDTOOLS_REPOSITORY $(pwd)/clang-gridtools
 
-docker pull $image
+sarus pull $image
 
 srun --job-name=dawn_PR \
     --time=00:45:00 \
@@ -30,8 +30,8 @@ srun --job-name=dawn_PR \
     --partition=cscsci \
     --constraint=gpu \
     --account=c14 \
-    docker run -v $root_dir:/usr/src/dawn \
-    $(pwd)/clang-gridtools:/usr/src/clang-gridtools \
+    sarus run --mount=type=bind,source=$root_dir,destination=/usr/src/dawn \
+    --mount=type=bind,source=$(pwd)/clang-gridtools,destination=/usr/src/clang-gridtools \
     $image \
     /usr/src/dawn/scripts/build-and-test \
     --dawn-build-dir /usr/src/dawn-build \
@@ -48,33 +48,3 @@ srun --job-name=dawn_PR \
     -Datlas_DIR=/usr/local/atlas/lib/cmake/atlas \
     -Deckit_DIR=/usr/local/eckit/lib/cmake/eckit \
     -GNinja
-
-# sarus pull $image
-
-# srun --job-name=dawn_PR \
-#     --time=00:45:00 \
-#     --nodes=1 \
-#     --ntasks-per-node=1 \
-#     --ntasks-per-core=2 \
-#     --cpus-per-task=$build_jobs \
-#     --partition=cscsci \
-#     --constraint=gpu \
-#     --account=c14 \
-#     sarus run --mount=type=bind,source=$root_dir,destination=/usr/src/dawn \
-#     --mount=type=bind,source=$(pwd)/clang-gridtools,destination=/usr/src/clang-gridtools \
-#     $image \
-#     /usr/src/dawn/scripts/build-and-test \
-#     --dawn-build-dir /usr/src/dawn-build \
-#     --dawn-install-dir /usr/local/dawn \
-#     --clang-gridtools-source-dir /usr/src/clang-gridtools \
-#     --clang-gridtools-build-dir /usr/src/clang-gridtools-build \
-#     --parallel $build_jobs \
-#     --config $build_type \
-#     -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc \
-#     -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 \
-#     -DProtobuf_DIR=/usr/local/protobuf/lib/cmake/protobuf \
-#     -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages \
-#     -DGridTools_DIR=/usr/local/gridtools/lib/cmake \
-#     -Datlas_DIR=/usr/local/atlas/lib/cmake/atlas \
-#     -Deckit_DIR=/usr/local/eckit/lib/cmake/eckit \
-#     -GNinja
