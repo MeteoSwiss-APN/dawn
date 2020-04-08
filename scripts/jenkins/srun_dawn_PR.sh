fb6831c9156d49b95f71e76cc3754f549fc075a3
Johann Dahm <johannd@vulcan.com>
2020-04-08 09:35:58 -0700

    Use build-and-test without sarus

--- scripts/jenkins/srun_dawn_PR.sh
+++ scripts/jenkins/srun_dawn_PR.sh
@@ -6,8 +6,10 @@ root_dir=$script_path/../../
 image=gtclang/dawn-env-cuda10.1-ubuntu18.04
 build_jobs=24
 
-module load daint-gpu
-module load sarus
+# Fill myhost variable value
+source $script_path/machine_env.sh
+# Get machine-specific dependency locations
+source $script_path/env_${myhost}.sh
 
 # Clone clang-gridtools here because the image does not have the ssh key
 if [ -z ${CLANG_GRIDTOOLS_REPOSITORY+x} ]; then
@@ -17,34 +19,20 @@ if [ -z ${CLANG_GRIDTOOLS_BRANCH+x} ]; then
     CLANG_GRIDTOOLS_BRANCH=master
 fi
 
-git clone --depth 1 -b $CLANG_GRIDTOOLS_BRANCH $CLANG_GRIDTOOLS_REPOSITORY $(pwd)/clang-gridtools
+git clone --depth 1 -b $CLANG_GRIDTOOLS_BRANCH $CLANG_GRIDTOOLS_REPOSITORY $root_dir/clang-gridtools
 
-sarus pull $image
+python -m venv dawn_venv
 
-srun --job-name=dawn_PR \
-    --time=00:45:00 \
-    --nodes=1 \
-    --ntasks-per-node=1 \
-    --ntasks-per-core=2 \
-    --cpus-per-task=$build_jobs \
-    --partition=cscsci \
-    --constraint=gpu \
-    --account=c14 \
-    sarus run --mount=type=bind,source=$root_dir,destination=/usr/src/dawn \
-    --mount=type=bind,source=$(pwd)/clang-gridtools,destination=/usr/src/clang-gridtools \
-    $image \
-    /usr/src/dawn/scripts/build-and-test \
-    --dawn-build-dir /usr/src/dawn-build \
-    --dawn-install-dir /usr/local/dawn \
-    --clang-gridtools-source-dir /usr/src/clang-gridtools \
-    --clang-gridtools-build-dir /usr/src/clang-gridtools-build \
+$root_dir/scripts/build-and-test \
+    --dawn-build-dir $root_dir/build \
+    --dawn-install-dir $root_dir/install \
+    --clang-gridtools-source-dir $root_dir/clang-gridtools \
+    --clang-gridtools-build-dir $root_dir/clang-gridtools-build \
     --parallel $build_jobs \
     --config $build_type \
-    -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc \
-    -DCMAKE_PREFIX_PATH=/usr/lib/llvm-9 \
-    -DProtobuf_DIR=/usr/local/protobuf/lib/cmake/protobuf \
-    -DPROTOBUF_PYTHON_DIR=/usr/local/lib/python3.7/dist-packages \
-    -DGridTools_DIR=/usr/local/gridtools/lib/cmake \
-    -Datlas_DIR=/usr/local/atlas/lib/cmake/atlas \
-    -Deckit_DIR=/usr/local/eckit/lib/cmake/eckit \
-    -GNinja
+    --python-venv dawn_venv \
+    -DProtobuf_DIR=$PROTOBUFDIR \
+    -DPROTOBUF_PYTHON_MODULE_DIR=$PROTOBUFDIR/../../../python \
+    -DDAWN_REQUIRE_PYTHON=ON \
+    -Datlas_DIR=${ATLAS_DIR}/lib/cmake/atlas \
+    -Deckit_DIR=${ECKIT_DIR}/lib/cmake/eckit
