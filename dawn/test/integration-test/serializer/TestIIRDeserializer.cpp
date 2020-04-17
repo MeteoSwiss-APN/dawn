52fc3d0352917d165b884bf44099c6e6ce45ad86
Eddie Davis <eddied@vulcan.com>
2020-04-17 16:02:48 -0700

    Add UnstructuredMixedCopies test

--- dawn/test/integration-test/serializer/TestIIRDeserializer.cpp
+++ dawn/test/integration-test/serializer/TestIIRDeserializer.cpp
@@ -46,10 +46,10 @@ namespace dawn {
 TEST(TestIIRDeserializer, CopyStencil) {
   // generate IIR in memory
   UIDGenerator::getInstance()->reset();
-  auto copy_stencil_memory = IIRSerializer::deserialize("reference_iir/copy_stencil.iir");
-  EXPECT_EQ(copy_stencil_memory->getStencils().size(), 1);
+  auto instantiation = IIRSerializer::deserialize("reference_iir/copy_stencil.iir");
+  EXPECT_EQ(instantiation->getStencils().size(), 1);
 
-  const auto& stencil = copy_stencil_memory->getStencils()[0];
+  const auto& stencil = instantiation->getStencils()[0];
   EXPECT_EQ(stencil->getChildren().size(), 1);
 
   auto const& mss = *(stencil->childrenBegin());
@@ -63,7 +63,78 @@ TEST(TestIIRDeserializer, CopyStencil) {
   auto interval = iir::Interval{0, sir::Interval::End};
   EXPECT_EQ(doMethod->getInterval(), interval);
 
-  // Assert something about AST...
+  auto const& ast = doMethod->getAST();
+  EXPECT_EQ(ast.getStatements().size(), 1);
+}
+
+TEST(TestIIRDeserializer, LapStencil) {
+  // generate IIR in memory
+  UIDGenerator::getInstance()->reset();
+  auto instantiation = IIRSerializer::deserialize("reference_iir/lap_stencil.iir");
+  EXPECT_EQ(instantiation->getStencils().size(), 1);
+
+  const auto& stencil = instantiation->getStencils()[0];
+  EXPECT_EQ(stencil->getChildren().size(), 1);
+
+  auto const& mss = *(stencil->childrenBegin());
+  EXPECT_EQ(mss->getChildren().size(), 2);
+  EXPECT_EQ(mss->getLoopOrder(), iir::LoopOrderKind::Parallel);
+
+  auto const& stage_iter = mss->getChildren().begin();
+  auto const& stage1 = *(stage_iter);
+  EXPECT_EQ(stage1->getChildren().size(), 1);
+
+  auto const& doMethod = *(stage1->childrenBegin());
+  auto interval = iir::Interval{0, sir::Interval::End};
+  EXPECT_EQ(doMethod->getInterval(), interval);
+
+  auto const& ast = doMethod->getAST();
+  EXPECT_EQ(ast.getStatements().size(), 1);
+
+  auto const& stage2 = *(std::next(stage_iter));
+  EXPECT_EQ(stage2->getChildren().size(), 1);
+
+  auto const& doMethod2 = *(stage2->childrenBegin());
+  interval = iir::Interval{0, sir::Interval::End};
+  EXPECT_EQ(doMethod2->getInterval(), interval);
+
+  auto const& ast2 = doMethod2->getAST();
+  EXPECT_EQ(ast2.getStatements().size(), 1);
+}
+
+TEST(TestIIRDeserializer, UnstructuredMixedCopies) {
+  // generate IIR in memory
+  UIDGenerator::getInstance()->reset();
+  auto instantiation = IIRSerializer::deserialize("reference_iir/unstructured_mixed_copies.iir");
+  EXPECT_EQ(instantiation->getStencils().size(), 1);
+
+  const auto& stencil = instantiation->getStencils()[0];
+  EXPECT_EQ(stencil->getChildren().size(), 1);
+
+  auto const& mss = *(stencil->childrenBegin());
+  EXPECT_EQ(mss->getChildren().size(), 2);
+  EXPECT_EQ(mss->getLoopOrder(), iir::LoopOrderKind::Forward);
+
+  auto const& stage_iter = mss->getChildren().begin();
+  auto const& stage1 = *(stage_iter);
+  EXPECT_EQ(stage1->getChildren().size(), 1);
+
+  auto const& doMethod = *(stage1->childrenBegin());
+  auto interval = iir::Interval{0, sir::Interval::End};
+  EXPECT_EQ(doMethod->getInterval(), interval);
+
+  auto const& ast = doMethod->getAST();
+  EXPECT_EQ(ast.getStatements().size(), 1);
+
+  auto const& stage2 = *(std::next(stage_iter));
+  EXPECT_EQ(stage2->getChildren().size(), 1);
+
+  auto const& doMethod2 = *(stage2->childrenBegin());
+  interval = iir::Interval{0, sir::Interval::End};
+  EXPECT_EQ(doMethod2->getInterval(), interval);
+
+  auto const& ast2 = doMethod2->getAST();
+  EXPECT_EQ(ast2.getStatements().size(), 1);
 }
 
 } // anonymous namespace
