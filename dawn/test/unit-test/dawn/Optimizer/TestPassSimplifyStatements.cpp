5ea58e9b84829051d01906530101bc861f5d3c1e
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-04-22 11:01:30 +0200

    Use new pattern for tests

--- dawn/test/unit-test/dawn/Optimizer/TestPassSimplifyStatements.cpp
+++ dawn/test/unit-test/dawn/Optimizer/TestPassSimplifyStatements.cpp
@@ -13,17 +13,14 @@
 //===------------------------------------------------------------------------------------------===//
 
 #include "dawn/AST/ASTStringifier.h"
-#include "dawn/Compiler/DawnCompiler.h"
-#include "dawn/Compiler/Options.h"
 #include "dawn/IIR/ASTFwd.h"
 #include "dawn/IIR/IIR.h"
 #include "dawn/IIR/StencilInstantiation.h"
+#include "dawn/Optimizer/OptimizerContext.h"
 #include "dawn/Optimizer/PassSimplifyStatements.h"
 #include "dawn/Serialization/IIRSerializer.h"
 #include "dawn/Unittest/ASTConstructionAliases.h"
-#include "dawn/Unittest/CompilerUtil.h"
 #include "dawn/Unittest/UnittestUtils.h"
-#include "test/unit-test/dawn/Optimizer/TestEnvironment.h"
 
 #include <fstream>
 #include <gtest/gtest.h>
@@ -33,87 +30,87 @@ using namespace dawn;
 using namespace astgen;
 
 namespace {
-class TestPassSimplifyStatements : public ::testing::Test {
-protected:
-  std::unique_ptr<OptimizerContext> context_;
-  std::shared_ptr<iir::StencilInstantiation> instantiation_;
 
-  void runPass(const std::string& filename) {
-    dawn::UIDGenerator::getInstance()->reset();
-    instantiation_ = IIRSerializer::deserialize(filename);
+std::shared_ptr<iir::StencilInstantiation> initializeInstantiation(const std::string& filename) {
+  UIDGenerator::getInstance()->reset();
+  auto instantiation = IIRSerializer::deserialize(filename);
+  DiagnosticsEngine diag;
+  OptimizerContext context(diag, {}, {{instantiation->getName(), instantiation}});
 
-    PassSimplifyStatements pass(*context_);
-    ASSERT_TRUE(pass.run(instantiation_));
-  }
-};
+  PassSimplifyStatements pass(context);
+  pass.run(instantiation);
+  EXPECT_TRUE(!diag.hasErrors());
 
-TEST_F(TestPassSimplifyStatements, CompoundStatement) {
+  return instantiation;
+}
+
+TEST(TestPassSimplifyStatements, CompoundStatement) {
   // b += a;
   // d -= c;
-  runPass("input/test_simplify_statements_compound_statement.iir");
-  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation_), 0);
+  auto instantiation = initializeInstantiation("input/test_simplify_statements_compound_statement.iir");
+  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation), 0);
   ASSERT_TRUE(firstStmt->equals(expr(assign(field("b"), binop(field("b"), "+", field("a")))).get(),
                                 /*compareData = */ false));
-  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation_), 1);
+  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation), 1);
   ASSERT_TRUE(secondStmt->equals(expr(assign(field("d"), binop(field("d"), "-", field("c")))).get(),
                                  /*compareData = */ false));
 }
 
-TEST_F(TestPassSimplifyStatements, IncrementDecrement) {
+TEST(TestPassSimplifyStatements, IncrementDecrement) {
   // int b = d;
   // int c = d;
   // --b;
   // ++c;
   // a = c + b;
-  runPass("input/test_simplify_statements_increment_decrement.iir");
-  ASSERT_EQ(5, getFirstDoMethod(instantiation_).getAST().getStatements().size());
-  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation_), 2);
+  auto instantiation = initializeInstantiation("input/test_simplify_statements_increment_decrement.iir");
+  ASSERT_EQ(5, getFirstDoMethod(instantiation).getAST().getStatements().size());
+  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation), 2);
   ASSERT_TRUE(firstStmt->equals(expr(assign(var("b"), binop(var("b"), "-", lit(1)))).get(),
                                 /*compareData = */ false));
-  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation_), 3);
+  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation), 3);
   ASSERT_TRUE(secondStmt->equals(expr(assign(var("c"), binop(var("c"), "+", lit(1)))).get(),
                                  /*compareData = */ false));
 }
 
-TEST_F(TestPassSimplifyStatements, IncrementNested) {
+TEST(TestPassSimplifyStatements, IncrementNested) {
   // int b = c;
   // a = (++b) + 1;
-  runPass("input/test_simplify_statements_increment_nested.iir");
-  ASSERT_EQ(3, getFirstDoMethod(instantiation_).getAST().getStatements().size());
-  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation_), 1);
+  auto instantiation = initializeInstantiation("input/test_simplify_statements_increment_nested.iir");
+  ASSERT_EQ(3, getFirstDoMethod(instantiation).getAST().getStatements().size());
+  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation), 1);
   ASSERT_TRUE(firstStmt->equals(expr(assign(var("b"), binop(var("b"), "+", lit(1)))).get(),
                                 /*compareData = */ false));
-  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation_), 2);
+  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation), 2);
   ASSERT_TRUE(secondStmt->equals(expr(assign(field("a"), binop(var("b"), "+", lit(1)))).get(),
                                  /*compareData = */ false));
 }
 
-TEST_F(TestPassSimplifyStatements, MixMultipleNested) {
+TEST(TestPassSimplifyStatements, MixMultipleNested) {
   // int b = d;
   // int c = d;
   // a += ++b + (1 + --c);
   // a *= ++c * --b;
-  runPass("input/test_simplify_statements_mix_multiple_nested.iir");
-  ASSERT_EQ(8, getFirstDoMethod(instantiation_).getAST().getStatements().size());
-  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation_), 2);
+  auto instantiation = initializeInstantiation("input/test_simplify_statements_mix_multiple_nested.iir");
+  ASSERT_EQ(8, getFirstDoMethod(instantiation).getAST().getStatements().size());
+  auto const& firstStmt = getNthStmt(getFirstDoMethod(instantiation), 2);
   ASSERT_TRUE(firstStmt->equals(expr(assign(var("b"), binop(var("b"), "+", lit(1)))).get(),
                                 /*compareData = */ false));
-  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation_), 3);
+  auto const& secondStmt = getNthStmt(getFirstDoMethod(instantiation), 3);
   ASSERT_TRUE(secondStmt->equals(expr(assign(var("c"), binop(var("c"), "-", lit(1)))).get(),
                                  /*compareData = */ false));
-  auto const& thirdStmt = getNthStmt(getFirstDoMethod(instantiation_), 4);
+  auto const& thirdStmt = getNthStmt(getFirstDoMethod(instantiation), 4);
   ASSERT_TRUE(thirdStmt->equals(
       expr(assign(field("a"),
                   binop(field("a"), "+", binop(var("b"), "+", binop(lit(1), "+", var("c"))))))
           .get(),
       /*compareData = */ false));
-  auto const& fourthStmt = getNthStmt(getFirstDoMethod(instantiation_), 5);
+  auto const& fourthStmt = getNthStmt(getFirstDoMethod(instantiation), 5);
   ASSERT_TRUE(fourthStmt->equals(expr(assign(var("c"), binop(var("c"), "+", lit(1)))).get(),
                                  /*compareData = */ false));
-  auto const& fifthStmt = getNthStmt(getFirstDoMethod(instantiation_), 6);
+  auto const& fifthStmt = getNthStmt(getFirstDoMethod(instantiation), 6);
   ASSERT_TRUE(fifthStmt->equals(expr(assign(var("b"), binop(var("b"), "-", lit(1)))).get(),
                                 /*compareData = */ false));
-  auto const& sixthStmt = getNthStmt(getFirstDoMethod(instantiation_), 7);
+  auto const& sixthStmt = getNthStmt(getFirstDoMethod(instantiation), 7);
   ASSERT_TRUE(sixthStmt->equals(
       expr(assign(field("a"), binop(field("a"), "*", binop(var("c"), "*", var("b"))))).get(),
       /*compareData = */ false));
