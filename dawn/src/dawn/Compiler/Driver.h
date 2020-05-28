be9cdc4e239ec2ff8bfdea33b2bfe998ea4b01aa
Johann Dahm <johannd@vulcan.com>
2020-05-28 15:00:45 -0700

    Always output json from python

--- dawn/src/dawn/Compiler/Driver.h
+++ dawn/src/dawn/Compiler/Driver.h
@@ -38,6 +38,10 @@ run(const std::shared_ptr<SIR>& stencilIR, const std::list<PassGroup>& groups,
     const Options& options = {});
 
 /// @brief Lower to IIR and run groups. Use strings in place of C++ structures.
+///
+/// NOTE: This method always returns the stencil instantiations as json string objects, not
+/// bytes, as this greatly simplifies the conversion.
+/// See https://pybind11.readthedocs.io/en/stable/advanced/cast/strings.html for more details.
 std::map<std::string, std::string> run(const std::string& sir, dawn::SIRSerializer::Format format,
                                        const std::list<dawn::PassGroup>& groups,
                                        const dawn::Options& options = {});
@@ -49,6 +53,10 @@ run(const std::map<std::string, std::shared_ptr<iir::StencilInstantiation>>&
     const std::list<PassGroup>& groups, const Options& options = {});
 
 /// @brief Run groups. Use strings in place of C++ structures.
+///
+/// NOTE: This method always returns the stencil instantiations as json string objects, not bytes,
+/// as this greatly simplifies the conversion.
+/// See https://pybind11.readthedocs.io/en/stable/advanced/cast/strings.html for more details.
 std::map<std::string, std::string>
 run(const std::map<std::string, std::string>& stencilInstantiationMap, IIRSerializer::Format format,
     const std::list<PassGroup>& groups, const Options& options = {});
