13648490d7a7975e23e8a5d58e670f6f9eaba3f9
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-07-21 12:03:55 +0200

    added tests

---
+++ dawn/test/integration-test/dawn4py-tests/generate_empty_stage.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+##===-----------------------------------------------------------------------------*- Python -*-===##
+##                          _
+##                         | |
+##                       __| | __ ___      ___ ___
+##                      / _` |/ _` \ \ /\ / / '_  |
+##                     | (_| | (_| |\ V  V /| | | |
+##                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+##
+##
+##  This file is distributed under the MIT License (MIT).
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+"""
+this integration test generates a SIR which will contain an empty stage after the PassRemoveScalars
+has run, and thus ensures that such emtpied stages are handled correctly
+"""
+
+import argparse
+import os
+
+import dawn4py
+from dawn4py.serialization import SIR
+from dawn4py.serialization import utils as sir_utils
+
+OUTPUT_NAME = "empty_stage_stencil"
+OUTPUT_FILE = f"{OUTPUT_NAME}.cpp"
+OUTPUT_PATH = f"{OUTPUT_NAME}.cpp"
+
+
+def main(args: argparse.Namespace):
+    interval = sir_utils.make_interval(
+        SIR.Interval.Start, SIR.Interval.End, 0, 0)
+
+    body_ast = sir_utils.make_ast(
+        [
+            sir_utils.make_assignment_stmt(
+                sir_utils.make_field_access_expr("x"),
+                sir_utils.make_literal_access_expr(
+                    "1",  SIR.BuiltinType.Double),
+                "=",
+            )
+        ]
+    )
+
+    vertical_region_stmt = sir_utils.make_vertical_region_decl_stmt(
+        body_ast, interval, SIR.VerticalRegion.Forward
+    )
+
+    sir = sir_utils.make_sir(
+        OUTPUT_FILE,
+        SIR.GridType.Value("Unstructured"),
+        [
+            sir_utils.make_stencil(
+                OUTPUT_NAME,
+                sir_utils.make_ast([vertical_region_stmt]),
+                [
+                    sir_utils.make_field(
+                        "in",
+                        sir_utils.make_field_dimensions_unstructured(
+                            [SIR.LocationType.Value("Edge")], 1
+                        ),
+                    ),
+                    sir_utils.make_field(
+                        "out",
+                        sir_utils.make_field_dimensions_unstructured(
+                            [SIR.LocationType.Value("Edge")], 1
+                        ),
+                    ),
+                    sir_utils.make_field(
+                        "x",
+                        sir_utils.make_field_dimensions_unstructured(
+                            [SIR.LocationType.Value("Edge")], 1
+                        ),
+                        True
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    # print the SIR
+    if args.verbose:
+        sir_utils.pprint(sir)
+
+    # compile
+    code = dawn4py.compile(sir, backend=dawn4py.CodeGenBackend.CXXNaiveIco)
+
+    # write to file
+    print(f"Writing generated code to '{OUTPUT_PATH}'")
+    with open(OUTPUT_PATH, "w") as f:
+        f.write(code)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate a stencil that degenerates to an empty stage"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Print the generated SIR",
+    )
+    main(parser.parse_args())
