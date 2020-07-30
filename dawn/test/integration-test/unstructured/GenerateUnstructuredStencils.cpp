a9f8f6fa31b9ae2981d6326ca128c325e89c82b3
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-07-30 17:05:57 +0200

    signal handler to dbg jenkins problem

--- dawn/test/integration-test/unstructured/GenerateUnstructuredStencils.cpp
+++ dawn/test/integration-test/unstructured/GenerateUnstructuredStencils.cpp
@@ -23,10 +23,29 @@
 #include "dawn/Unittest/IIRBuilder.h"
 
 #include <cstring>
+#include <execinfo.h>
 #include <fstream>
 #include <optional>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handler(int sig) {
+  void* array[100];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 100);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
 
 int main() {
+  signal(SIGSEGV, handler); // install our handler
 
   {
     using namespace dawn::iir;
