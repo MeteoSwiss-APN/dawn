6b6c827c1068b6b06e538fd14efbff226a9a2603
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-08-04 09:00:14 +0200

    trying to get a reasonable diff for this pr

--- dawn/cmake/HasUnstructuredDeps.cmake
+++ dawn/cmake/HasUnstructuredDeps.cmake
@@ -11,7 +11,9 @@
 ##  See LICENSE.txt for details.
 ##
 ##===------------------------------------------------------------------------------------------===##
-if (NOT DEFINED HAS_UNSTRUCTURED_DEPS) 
+
+if (NOT DEFINED HAS_UNSTRUCTURED_DEPS)
+
   set(HAS_UNSTRUCTURED_DEPS OFF CACHE BOOL "True if Dawn has detected necessary dependencies for supporting unstructured grids.")
   mark_as_advanced(HAS_UNSTRUCTURED_DEPS)
 
