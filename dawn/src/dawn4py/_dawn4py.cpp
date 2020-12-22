daec832a90f0619be42d8b1bbc6197fb916f7502
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-12-22 12:57:15 +0100

    ran pybind

--- dawn/src/dawn4py/_dawn4py.cpp
+++ dawn/src/dawn4py/_dawn4py.cpp
@@ -182,16 +182,20 @@ PYBIND11_MODULE(_dawn4py, m) {
   py::class_<dawn::codegen::Options>(m, "CodeGenOptions")
       .def(
           py::init([](int MaxHaloSize, bool UseParallelEP, bool RunWithSync, int MaxBlocksPerSM,
-                      int nsms, int DomainSizeI, int DomainSizeJ, int DomainSizeK,
-                      const std::string& OutputCHeader, const std::string& OutputFortranInterface) {
+                      int nsms, int DomainSizeI, int DomainSizeJ, int DomainSizeK, int paddingCells,
+                      int paddingEdges, int paddingVertices, const std::string& OutputCHeader,
+                      const std::string& OutputFortranInterface) {
             return dawn::codegen::Options{
-                MaxHaloSize, UseParallelEP, RunWithSync, MaxBlocksPerSM, nsms,
-                DomainSizeI, DomainSizeJ,   DomainSizeK, OutputCHeader,  OutputFortranInterface};
+                MaxHaloSize,     UseParallelEP, RunWithSync,           MaxBlocksPerSM, nsms,
+                DomainSizeI,     DomainSizeJ,   DomainSizeK,           paddingCells,   paddingEdges,
+                paddingVertices, OutputCHeader, OutputFortranInterface};
           }),
           py::arg("max_halo_size") = 3, py::arg("use_parallel_ep") = false,
           py::arg("run_with_sync") = true, py::arg("max_blocks_per_sm") = 0, py::arg("nsms") = 0,
           py::arg("domain_size_i") = 0, py::arg("domain_size_j") = 0, py::arg("domain_size_k") = 0,
-          py::arg("output_c_header") = "", py::arg("output_fortran_interface") = "")
+          py::arg("padding_cells") = 0, py::arg("padding_edges") = 0,
+          py::arg("padding_vertices") = 0, py::arg("output_c_header") = "",
+          py::arg("output_fortran_interface") = "")
       .def_readwrite("max_halo_size", &dawn::codegen::Options::MaxHaloSize)
       .def_readwrite("use_parallel_ep", &dawn::codegen::Options::UseParallelEP)
       .def_readwrite("run_with_sync", &dawn::codegen::Options::RunWithSync)
@@ -200,6 +204,9 @@ PYBIND11_MODULE(_dawn4py, m) {
       .def_readwrite("domain_size_i", &dawn::codegen::Options::DomainSizeI)
       .def_readwrite("domain_size_j", &dawn::codegen::Options::DomainSizeJ)
       .def_readwrite("domain_size_k", &dawn::codegen::Options::DomainSizeK)
+      .def_readwrite("padding_cells", &dawn::codegen::Options::paddingCells)
+      .def_readwrite("padding_edges", &dawn::codegen::Options::paddingEdges)
+      .def_readwrite("padding_vertices", &dawn::codegen::Options::paddingVertices)
       .def_readwrite("output_c_header", &dawn::codegen::Options::OutputCHeader)
       .def_readwrite("output_fortran_interface", &dawn::codegen::Options::OutputFortranInterface)
       .def("__repr__", [](const dawn::codegen::Options& self) {
@@ -212,6 +219,9 @@ PYBIND11_MODULE(_dawn4py, m) {
            << "domain_size_i=" << self.DomainSizeI << ",\n    "
            << "domain_size_j=" << self.DomainSizeJ << ",\n    "
            << "domain_size_k=" << self.DomainSizeK << ",\n    "
+           << "padding_cells=" << self.paddingCells << ",\n    "
+           << "padding_edges=" << self.paddingEdges << ",\n    "
+           << "padding_vertices=" << self.paddingVertices << ",\n    "
            << "output_c_header="
            << "\"" << self.OutputCHeader << "\""
            << ",\n    "
