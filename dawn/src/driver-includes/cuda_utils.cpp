e1dd76fc0946d39a6b39990495a68c6db9c3c2fb
mroethlin <53755555+mroethlin@users.noreply.github.com>
2021-04-08 09:54:26 +0200

    divied upper and lower index for subdomains (#1126)
    ## Technical Description
    
    There is not necessarily a relation between the upper and lower indices as retrieved from the `get_indices_(e|c|v)` call. I.e. for
    
    ```
    CALL get_indices_e(p_patch(jg), 1, 1, 1, edge_start_idx_a, edge_end_idx_b, a, b)
    CALL get_indices_e(p_patch(jg), 1, 1, 1, edge_start_idx_b_prime, edge_end_idx_c, b, c)
    ```
    
    it neither holds that `edge_end_idx_b == edge_end_idx_b_prime` nor |edge_end_idx_b - edge_end_idx_b_prime| == 1`. 
    
    Consequently, we need to also distinguish between upper and lower indices in dawn to avoid awkward workarounds if a index is used as lower and upper index by two different stencils. This PR splits the single subdomain structure into a upper and lower one, both holding a map for subdomain markers.
    
    ### Testing
    
    Tested in `ICON-DSL`

--- dawn/src/driver-includes/cuda_utils.cpp
+++ dawn/src/driver-includes/cuda_utils.cpp
@@ -1,9 +1,14 @@
 #include "cuda_utils.hpp"
 
 extern "C" {
-void set_splitter_index(dawn::GlobalGpuTriMesh* globalTriMesh, int loc, int space, int offset,
+void set_splitter_index_lower(dawn::GlobalGpuTriMesh* globalTriMesh, int loc, int space, int offset,
                         int index) {
-  globalTriMesh->set_splitter_index(dawn::LocationType(loc), dawn::UnstructuredSubdomain(space),
+  globalTriMesh->set_splitter_index_lower(dawn::LocationType(loc), dawn::UnstructuredSubdomain(space),
+                                    offset, index);
+}
+void set_splitter_index_upper(dawn::GlobalGpuTriMesh* globalTriMesh, int loc, int space, int offset,
+                        int index) {
+  globalTriMesh->set_splitter_index_upper(dawn::LocationType(loc), dawn::UnstructuredSubdomain(space),
                                     offset, index);
 }
 }
