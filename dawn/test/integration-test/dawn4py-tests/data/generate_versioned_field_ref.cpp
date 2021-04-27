199959f54cbad814b823e74ca80f97d1f8d9d088
cosunae <carlos.osuna@env.ethz.ch>
2021-04-27 15:04:35 +0200

    fix references for bug in loop bounds

--- dawn/test/integration-test/dawn4py-tests/data/generate_versioned_field_ref.cpp
+++ dawn/test/integration-test/dawn4py-tests/data/generate_versioned_field_ref.cpp
@@ -45,14 +45,14 @@ private:
     void run() {
       using ::dawn::deref;
       {
-        for(int k = 0 + 0; k <= (m_k_size == 0 ? 0 : (m_k_size - 1)) + 0 + 0; ++k) {
+        for(int k = 0 + 0; k <= (m_k_size == 0 ? 0 : (m_k_size)) + 0 - 1 + 0; ++k) {
           for(auto const& loc : getEdges(LibTag{}, m_mesh)) {
             m_c_0(deref(LibTag{}, loc), (k + 0)) = m_c(deref(LibTag{}, loc), (k + 0));
           }
         }
       }
       {
-        for(int k = 0 + 0; k <= (m_k_size == 0 ? 0 : (m_k_size - 1)) + 0 + 0; ++k) {
+        for(int k = 0 + 0; k <= (m_k_size == 0 ? 0 : (m_k_size)) + 0 - 1 + 0; ++k) {
           for(auto const& loc : getEdges(LibTag{}, m_mesh)) {
             m_a(deref(LibTag{}, loc), (k + 0)) =
                 ((m_b(deref(LibTag{}, loc), (k + 0)) / m_c_0(deref(LibTag{}, loc), (k + 0))) +
