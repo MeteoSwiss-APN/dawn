0e919eb59c853ea722760252862f89e9865e46fd
samkellerhals <kellerhalssamuel@gmail.com>
2022-08-08 16:05:06 +0200

    Format files

--- dawn/src/driver-includes/to_json.cpp
+++ dawn/src/driver-includes/to_json.cpp
@@ -2,76 +2,68 @@
 
 using namespace dawn;
 
-MetricsSerialiser::MetricsSerialiser(VerificationMetrics metricsStruct, std::string metricsPath, std::string stencilName,
-                                     std::string fieldIdentifier) {
-        fieldId = fieldIdentifier;
-        path = metricsPath;
-        stencil = stencilName;
-        newJsonMetrics = generateJsonFromStruct(metricsStruct);
-    }
+MetricsSerialiser::MetricsSerialiser(VerificationMetrics metricsStruct, std::string metricsPath,
+                                     std::string stencilName, std::string fieldIdentifier) {
+  fieldId = fieldIdentifier;
+  path = metricsPath;
+  stencil = stencilName;
+  newJsonMetrics = generateJsonFromStruct(metricsStruct);
+}
 
 void MetricsSerialiser::writeJson(int iteration) {
-        if (is_empty(path)) {
-            dumpJson(newJsonMetrics);
+  if(is_empty(path)) {
+    dumpJson(newJsonMetrics);
+  } else {
+    json oldJsonMetrics = json::parse(std::ifstream(path, std::ios_base::app));
+    json newMetrics = newJsonMetrics[stencil][0];
+    bool stencilFound = false;
+
+    for(auto& [stencilName, metricsArr] : oldJsonMetrics.items()) {
+      if(stencilName == stencil) {
+        // handle case where iteration does not yet exist for a stencil
+        if(metricsArr.size() < iteration + 1) {
+          metricsArr.insert(metricsArr.end(), newMetrics);
         } else {
-            json oldJsonMetrics = json::parse(std::ifstream(path, std::ios_base::app));
-            json newMetrics = newJsonMetrics[stencil][0];
-            bool stencilFound = false;
-
-            for (auto &[stencilName, metricsArr]: oldJsonMetrics.items()) {
-                if (stencilName == stencil) {
-                    // handle case where iteration does not yet exist for a stencil
-                    if (metricsArr.size() < iteration + 1) {
-                        metricsArr.insert(metricsArr.end(), newMetrics);
-                    } else {
-                        metricsArr[iteration].insert(newMetrics.begin(), newMetrics.end());
-                    }
-                    stencilFound = true;
-                }
-            }
-
-            if (!stencilFound) {
-                // add new stencil metrics object
-                oldJsonMetrics.insert(newJsonMetrics.begin(), newJsonMetrics.end());
-            }
-            dumpJson(oldJsonMetrics);
+          metricsArr[iteration].insert(newMetrics.begin(), newMetrics.end());
         }
+        stencilFound = true;
+      }
     }
 
+    if(!stencilFound) {
+      // add new stencil metrics object
+      oldJsonMetrics.insert(newJsonMetrics.begin(), newJsonMetrics.end());
+    }
+    dumpJson(oldJsonMetrics);
+  }
+}
+
 void MetricsSerialiser::dumpJson(json j) {
-    std::ofstream out(path, std::ios_base::out);
-    out << j.dump(4) << std::endl;
+  std::ofstream out(path, std::ios_base::out);
+  out << j.dump(4) << std::endl;
 }
 
 bool MetricsSerialiser::is_empty(std::string) {
-    std::ifstream f(path);
-    return f.peek() == std::ifstream::traits_type::eof();
+  std::ifstream f(path);
+  return f.peek() == std::ifstream::traits_type::eof();
 }
 
 json MetricsSerialiser::generateJsonFromStruct(VerificationMetrics metrics) {
-    json j =
-            {
-                    {stencil, {
-                            {
+  json j = {{stencil,
+             {
+                 {
 
-                                    {
-                                        fieldId, {
-                                            {"max_relative_error", metrics.maxRelErr},
-                                            {"min_relative_error", metrics.minRelErr},
-                                            {"max_absolute_error", metrics.maxAbsErr},
-                                            {"min_absolute_error", metrics.minAbsErr},
-                                            {"field_is_valid", metrics.isValid}
-                                        }
-                                    }
-                            },
-                        }
-                    }
-            };
-    return j;
+                     {fieldId,
+                      {{"max_relative_error", metrics.maxRelErr},
+                       {"min_relative_error", metrics.minRelErr},
+                       {"max_absolute_error", metrics.maxAbsErr},
+                       {"min_absolute_error", metrics.minAbsErr},
+                       {"field_is_valid", metrics.isValid}}}},
+             }}};
+  return j;
 }
 
-std::string getEnvVar( std::string const & key )
-{
-    char * val = getenv( key.c_str() );
-    return val == NULL ? std::string("") : std::string(val);
+std::string getEnvVar(std::string const& key) {
+  char* val = getenv(key.c_str());
+  return val == NULL ? std::string("") : std::string(val);
 }
--- dawn/src/driver-includes/to_json.hpp
+++ dawn/src/driver-includes/to_json.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
-#include <fstream>
-#include <nlohmann/json.hpp>
-#include <iostream>
 #include "verification_metrics.hpp"
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
 namespace dawn {
-    class MetricsSerialiser {
-    public:
-        std::string fieldId;
-        std::string path;
-        std::string stencil;
-        json newJsonMetrics;
-        MetricsSerialiser(VerificationMetrics metricsStruct, std::string metricsPath, std::string stencilName,
-                          std::string fieldIdentifier);
-        void writeJson(int iteration);
+class MetricsSerialiser {
+public:
+  std::string fieldId;
+  std::string path;
+  std::string stencil;
+  json newJsonMetrics;
+  MetricsSerialiser(VerificationMetrics metricsStruct, std::string metricsPath,
+                    std::string stencilName, std::string fieldIdentifier);
+  void writeJson(int iteration);
 
-    private:
-        void dumpJson(json j);
-        bool is_empty(std::string);
-        json generateJsonFromStruct(VerificationMetrics metrics);
-    };
+private:
+  void dumpJson(json j);
+  bool is_empty(std::string);
+  json generateJsonFromStruct(VerificationMetrics metrics);
+};
 
-    std::string getEnvVar( std::string const & key );
+std::string getEnvVar(std::string const& key);
 
-}
+} // namespace dawn
